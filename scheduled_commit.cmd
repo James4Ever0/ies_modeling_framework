3071dc0dc9f92d3cf2513739c1df81d598a64ab7
YuPan <example@example.com>
2023-02-16 11:09:02 +0800

    'update'

--- scheduled_commit.cmd
+++ scheduled_commit.cmd
@@ -1,5 +1,6 @@
 CONSOLESTATE /Hide
 @REM http://www.robvanderwoude.com
+@REM https://www.robvanderwoude.com/battech_hideconsole.php
 D:
 cd D:\project\xianxing
 @REM not to output too many things. just look at the most recent one.
