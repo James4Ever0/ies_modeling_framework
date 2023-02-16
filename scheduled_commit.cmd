f05a902f5d52229db84304c12d3c52167c9bf3bf
YuPan <example@example.com>
2023-02-16 11:04:21 +0800

    'update'

--- scheduled_commit.cmd
+++ scheduled_commit.cmd
@@ -1,5 +1,6 @@
-
+CONSOLESTATE /Hide
+@REM http://www.robvanderwoude.com
 D:
 cd D:\project\xianxing
-REM not to output too many things. just look at the most recent one.
+@REM not to output too many things. just look at the most recent one.
 commit.cmd > run.log
