e7a342627ca8f1a6c18f95f35cd6bdc94ef4d533
YuPan <example@example.com>
2023-02-16 11:29:02 +0800

    'update'

--- EnergyCircuitTheory-EnergyFlowCalculation/check_different_sheet_in_same_excel_file.py
+++ EnergyCircuitTheory-EnergyFlowCalculation/check_different_sheet_in_same_excel_file.py
@@ -1,8 +1,10 @@
-import pandas
+import pandas as pd
 
-excel_name = "6节点热网动态潮流.py"
+excel_name = "6节点热网动态data.xls"
 
-sheet_names = []
+sheet_names = ['Node','Branch','Device','Dynamic']
 
 for sheet_name in sheet_names:
-    ...
+    df=pd.read_excel(excel_name, sheet_name=sheet_name)
+    print(df.head())
+    print()
