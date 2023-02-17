9699a358687230413ca457e937c4f6a23293f7aa
YuPan <example@example.com>
2023-02-17 15:29:03 +0800

    'update'

--- serve_pydoc.ps1
+++ serve_pydoc.ps1
@@ -1,3 +1,4 @@
 ipconfig | rg 192.168
 # python -m pydoc -n 0.0.0.0 -p 8001 .\cpExample.py
-pdoc --http 0.0.0.0:8001 .\cpExample.py
+# pdoc --http 0.0.0.0:8001 .\cpExample.py
+pdoc --http 0.0.0.0:8001 .\example_docstring.py
