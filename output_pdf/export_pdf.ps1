22d03872dd220a4535bf59715b03c9780e9a6e38
YuPan <example@example.com>
2023-02-17 17:54:02 +0800

    'update'

--- output_pdf/export_pdf.ps1
+++ output_pdf/export_pdf.ps1
@@ -1,3 +1,5 @@
-pandoc --metadata=title:"MyProject Documentation"  --from=markdown+abbreviations+tex_math_single_backslash  --pdf-engine=xelatex --variable=mainfont:"DejaVu Sans"   --toc --toc-depth=4 --output=example_docstring.pdf  example_docstring.md
+$env:PATH+=$env:PATH;C:/Users/ss/scoop/apps/miktex/current/texmfs/install/miktex/bin/x64/
+
+pandoc --metadata=title:"MyProject Documentation"  --from=markdown+abbreviations+tex_math_single_backslash  --pdf-engine=xelatex --variable=mainfont:"DejaVu Sans"   --toc --toc-depth=4 --output=example_docstring.pdf  example_docstring_utf8.md
 
 # pandoc -s -o example_docstring.docx example_docstring.md
