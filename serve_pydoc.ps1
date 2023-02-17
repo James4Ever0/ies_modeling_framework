e9984fcf8208863d438bc3cc40f43b12537fc867
YuPan <example@example.com>
2023-02-17 16:34:02 +0800

    'update'

--- serve_pydoc.ps1
+++ serve_pydoc.ps1
@@ -2,11 +2,11 @@ ipconfig | rg 192.168
 # python -m pydoc -n 0.0.0.0 -p 8001 .\cpExample.py
 
 # pdoc and pdoc3 are different.
-# i use pdoc.
+# i use pdoc3.
 
 # pdoc3 syntax
 # pdoc --http 0.0.0.0:8001 .\cpExample.py
-# pdoc --http 0.0.0.0:8001 .\example_docstring.py
+pdoc --http 0.0.0.0:8001 .\example_docstring.py
 
 # pdoc syntax
-pdoc --port 8001 --search .\example_docstring.py
+# pdoc --port 8001 --search .\example_docstring.py
