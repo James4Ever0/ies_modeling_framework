cff63105417366b25eb8e67c6f429244566b73fd
YuPan <example@example.com>
2023-02-22 14:49:06 +0800

    'update'

--- generate_document.cmd
+++ generate_document.cmd
@@ -1,1 +1,4 @@
-pdoc --http 0.0.0.0:8021 integratedEnergySystemPrototypes.py demo_utils.py cpExample.py
+@REM pdoc --http 0.0.0.0:8021 integratedEnergySystemPrototypes.py demo_utils.py cpExample.py
+pdoc --html integratedEnergySystemPrototypes.py demo_utils.py cpExample.py config.py
+
+7z a ies_planning.7z integratedEnergySystemPrototypes.py demo_utils.py cpExample.py config.py html
