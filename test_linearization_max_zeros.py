596bec7b71b523327c00f573faea4f0a3309746e
YuPan <example@example.com>
2023-02-23 15:34:08 +0800

    'update'

--- test_linearization_max_zeros.py
+++ test_linearization_max_zeros.py
@@ -7,11 +7,14 @@ model = Model(model_name)
 
 num_hours = 24
 
-b = model.continuous_var(lb=-100,ub=100,name="b")
-x = model.continuous_var_list(lb=-100,ub=100,keys=[],name="x_n")
+b = model.continuous_var(lb=-100, ub=100, name="b")
+x = model.continuous_var_list(lb=-100, ub=100, keys=list(range(num_hours+1)), name="x_n")
+y = model.continuous_var_list(lb=-100, ub=100, keys=list(range(num_hours+1)), name="y_n")
+import math
 
+model.add_constraints(x[i] == b + math.sin(i * math.pi / 12) for i in range(num_hours=1))
 
 linearization = Linearization()
 linearization.max_zeros(num_hour=24, model=model, x=..., y=...)
 
-objective = ...
+objective = model.sum(x[i]+y[i] for i in range())
