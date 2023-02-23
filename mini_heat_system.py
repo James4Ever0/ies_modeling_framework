ac1dd37bb5abd18b809e3519f48eb1209683c84e
YuPan <example@example.com>
2023-02-23 13:54:04 +0800

    'update'

--- mini_heat_system.py
+++ mini_heat_system.py
@@ -33,13 +33,10 @@ resource = ResourceGet()
 gas_price0 = resource.get_gas_price(num_hour0)
 municipalSteam_price0 = resource.get_municipalSteam_price(num_hour0)
 electricity_price0 = resource.get_electricity_price(num_hour0)
-intensityOfIllumination0 = resource.get_radiation(
-    path="jinan_changqing-hour.dat", num_hour=num_hour0
-)
 
 
 # 水源热泵
-waterSourceHeatPumps = WaterHeatPump(
+waterSourceHeatPumps = WaterHeatPump( # you are not using the electricity of photothermal power?
     num_hour0,
     model1,
     device_max=2000,
@@ -49,19 +46,6 @@ waterSourceHeatPumps = WaterHeatPump(
     device_name="waterSourceHeatPumps",
 )
 waterSourceHeatPumps.constraints_register(model1)
-
-# 槽式光热设备
-troughPhotoThermal = TroughPhotoThermal(
-    num_hour0,
-    model1,
-    troughPhotoThermal_device_max=5000,
-    troughPhotoThermal_price=2000,
-    troughPhotoThermalSolidHeatStorage_price=1000,
-    intensityOfIllumination0=intensityOfIllumination0,
-    efficiency=0.8,
-)
-troughPhotoThermal.constraints_register(model1)
-
 
 # 水储能罐
 waterStorageTank = WaterEnergyStorage(
@@ -84,8 +68,7 @@ waterStorageTank.constraints_register(
     model1, register_period_constraints=1, day_node=day_node
 )
 
-
-# 市政蒸汽
+# 市政热水
 municipalSteam = CitySupply(
     num_hour0,
     model1,
@@ -110,26 +93,25 @@ model1.add_constraints(
     + power_heatStorage[h]
     for h in range(0, num_hour0)
 )
-# 高温蒸汽去处
+
+# 高温热水去处
 model1.add_constraints(
     power_heat_sum[h] >= heat_load[h] for h in range(0, num_hour0)
-)  # 每小时蒸汽消耗 >= 每小时蒸汽负荷消耗量
-
+)  # 每小时热水消耗 >= 每小时热水负荷消耗量
 
 model1.add_constraints(
     waterSourceHeatPumps.power_waterSourceHeatPumps_heatStorage[h]
     + waterStorageTank.power_energyStorageSystem[h]
-    == power_heatStorage[h]  # 蓄冰机组平衡输出 = 三工况机组的制冰功率 + 双工况机组的制冰功率 + 冰蓄能充放能功率
+    == power_heatStorage[h] 
     for h in range(0, num_hour0)
 )
 linearization = Linearization()
+
 #
 linearization.max_zeros(
     # TODO: invert x/y position
-    # 修改之前： 要么冰蓄冷功率为0，冰蓄能装置不充不放; 要么冰蓄冷功率等于蓄冷装置充放功率（此时冰蓄能释放能量）
-    # 修改之后： 要么蓄冰机组平衡输出为0，冰蓄能装置充能（负数），制冰机组输出（正数）全部被冰蓄能装置吸收；要么制冰机组用于蓄冰的功率为0，冰蓄能装置放能（正数），蓄冰机组平衡输出（正数）全部由冰蓄能装置提供
     num_hour0,
     model1,
     y=power_heatStorage,
-    x=heatStorage.power_energyStorageSystem,
+    x=waterStorageTank.power_energyStorageSystem,
 )
