cd699ac93e57f24ab4200ed2cc284ab2dc8a2a9b
YuPan <example@example.com>
2023-02-23 10:09:02 +0800

    'update'

--- mini_refrigeration_system.py
+++ mini_refrigeration_system.py
@@ -15,6 +15,14 @@ model1 = Model(name=simulation_name)
 resource = ResourceGet()
 municipalHotWater_price0 = resource.get_municipalHotWater_price(num_hour0)
 
-model1.add_constraint(power_cooletStorage[h] == ) # no loading
+
+
+hotWaterLiBr = LiBrRefrigeration(
+    num_hour0, model1, LiBr_device_max=10000*10000, device_price=1000, efficiency=0.9
+)
+hotWaterLiBr.constraints_register(model1)
+
+model1.add_constraint(hotWaterLiBr.heat_LiBr_from[h] == )
+
 # consumption and production
-model1.add_constraint(cool_load[h] == power_cooletStorage[h] for h in range(num_hour0))
+model1.add_constraint(cool_load[h] == hotWaterLiBr.cool_LiBr[h] for h in range(num_hour0))
