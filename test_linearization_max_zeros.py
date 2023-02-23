8560066be3efb64f3ee86cb482e5454e8a3defab
YuPan <example@example.com>
2023-02-23 15:54:11 +0800

    'update'

--- test_linearization_max_zeros.py
+++ test_linearization_max_zeros.py
@@ -3,12 +3,16 @@ from docplex.mp.model import Model
 from docplex.mp.vartype import VarType
 from typing import List
 
-def max_zeros_2(num_hour:int,model:Model,x:List[VarType],y:List[VarType]):
-    x_positive = model.binary_var([i for i in range(num_hour)],name="x_positive")
-    model.add_constraint((2*x_positive[i]-1) *x[i]>= 0 for i in range(num_hour))
+bigNumber = 1e10
+
+def max_zeros_2(num_hour: int, model: Model, x: List[VarType], y: List[VarType]):
+    x_positive = model.binary_var_list([i for i in range(num_hour)], name="x_positive")
+    model.add_constraints((2 * x_positive[i] - 1) * x[i] >= 0 for i in range(num_hour))
     # flag == 1 -> positive x
     # flag == 0 -> negative x
-    model.add_constraint(y[i] == x_positive[])
+    model.add_constraints(y[i] == x_positive[i] * x[i] for i in range(num_hour))
+    # the same?
+
 
 model_name = "max_zeros_test"
 
@@ -16,9 +20,9 @@ model = Model(model_name)
 
 num_hours = 24
 
-b = model.continuous_var(name="b")
-x = model.continuous_var_list(keys=list(range(num_hours + 1)), name="x_n")
-y = model.continuous_var_list(keys=list(range(num_hours + 1)), name="y_n")
+b = model.continuous_var(lb=-100,ub=100,name="b")
+x = model.continuous_var_list(lb=-100,ub=100,keys=list(range(num_hours + 1)), name="x_n")
+y = model.continuous_var_list(lb=-100,ub=100,keys=list(range(num_hours + 1)), name="y_n")
 import math
 
 model.add_constraints(
@@ -31,6 +35,7 @@ model.add_constraints(
 # linearization.max_zeros(num_hour=num_hours + 1, model=model, x=x, y=y)
 
 # let's define the `max_zeros` ourselves.
+max_zeros_2(num_hour = num_hours+1, model=model,x=x,y=y)
 
 objective = model.abs(model.sum(x[i] + y[i] for i in range(num_hours + 1)))
 
