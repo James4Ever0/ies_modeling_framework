8c21767b947bf12bbadf06d0dcf4963620f729f5
YuPan <example@example.com>
2023-02-23 11:59:02 +0800

    'update'

--- generate_call_graph.cmd
+++ generate_call_graph.cmd
@@ -1,6 +1,7 @@
-@REM filepaths= ["cpExample.py","data_visualize_utils.py", "integratedEnergySystemPrototypes.py", "config.py"] 
+@REM filepaths= ["cpExample.py data_visualize_utils.py", "integratedEnergySystemPrototypes.py", "config.py"] 
 @REM import os
 @REM for filepath in filepaths:
 @REM     command = f"pyan3 {filepath} --uses --no-defines --colored --grouped --annotated --dot > myuses_{filepath.split('.')[0]}.dot"
 @REM     os.system(command)
-pycallgraph graphviz -- cpExample.py
+@REM pycallgraph graphviz -- cpExample.py
+code2flow cpExample.py data_visualize_utils.py integratedEnergySystemPrototypes.py config.py
