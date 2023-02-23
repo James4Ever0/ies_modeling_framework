747d4f5f3f47260a072eee23c00d23291f456f8a
YuPan <example@example.com>
2023-02-23 10:14:02 +0800

    'update'

--- mini_refrigeration_system.py
+++ mini_refrigeration_system.py
@@ -8,13 +8,16 @@ from docplex.mp.model import Model
 simulation_name = "micro_refrigeration"
 
 load = LoadGet()
+# let's augment the load.
+import math
+import numpy as np
 cool_load = load.get_cool_load(num_hour0)
+cool_load = np.array([math.cos(i*0.1) for i in range(len(cool_load))])
 
 model1 = Model(name=simulation_name)
 
 resource = ResourceGet()
 municipalHotWater_price0 = resource.get_municipalHotWater_price(num_hour0)
-
 
 
 hotWaterLiBr = LiBrRefrigeration(
