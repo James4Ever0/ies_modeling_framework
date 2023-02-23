0dd5c7f5b93f98107bbf403af95542af57f25685
YuPan <example@example.com>
2023-02-23 13:34:13 +0800

    'update'

--- mini_heat_system.py
+++ mini_heat_system.py
@@ -1,6 +1,13 @@
-from integratedEnergySystemPrototypes import TroughPhotoThermal,CombinedHeatAndPower,GroundSourceSteamGenerator, CitySupply, GasBoiler
+from integratedEnergySystemPrototypes import (
+    TroughPhotoThermal,
+    CombinedHeatAndPower,
+    GroundSourceSteamGenerator,
+    CitySupply,
+    GasBoiler,
+)
 from demo_utils import LoadGet, ResourceGet
 from config import num_hour0, day_node
+
 # num_hour0 *=3
 from docplex.mp.model import Model
 
@@ -11,18 +18,19 @@ load = LoadGet()
 import math
 import numpy as np
 
-steam_load=load.get_steam_load(num_hour0)
+steam_load = load.get_steam_load(num_hour0)
 delta = 0.3
-steam_load = np.array([(1-delta) + math.cos(i*0.1)*delta for i in range(len(steam_load))])*steam_load
+steam_load = (
+    np.array([(1 - delta) + math.cos(i * 0.2) * delta for i in range(len(steam_load))])
+    * steam_load
+)
 model1 = Model(name=simulation_name)
 
 resource = ResourceGet()
 gas_price0 = resource.get_gas_price(num_hour0)
 municipalSteam_price0 = resource.get_municipalSteam_price(num_hour0)
 electricity_price0 = resource.get_electricity_price(num_hour0)
-intensityOfIllumination0 = resource.get_radiation(
-    path="jinan_changqing-hour.dat", num_hour=num_hour0
-)*100
+intensityOfIllumination0 = resource.get_radiation(path="jinan_changqing-hour.dat", num_hour=num_hour0) 
 
 # 槽式光热设备
 troughPhotoThermal = TroughPhotoThermal(
@@ -36,40 +44,6 @@ troughPhotoThermal = TroughPhotoThermal(
 )
 troughPhotoThermal.constraints_register(model1)
 
-# 地热蒸汽发生器
-groundSourceSteamGenerator = GroundSourceSteamGenerator(
-    num_hour0,
-    model1,
-    groundSourceSteamGenerator_device_max=20000,
-    groundSourceSteamGenerator_price=200,
-    groundSourceSteamGeneratorSolidHeatStorage_price=200,  # gtxr? SolidHeatStorage？
-    electricity_price=electricity_price0,
-    efficiency=0.9,
-)
-groundSourceSteamGenerator.constraints_register(model1)
-
-# 热电联产机组
-combinedHeatAndPower = CombinedHeatAndPower(
-    num_hour0,
-    model1,
-    combinedHeatAndPower_num_max=5,
-    combinedHeatAndPower_price=2000,
-    gas_price=gas_price0,
-    combinedHeatAndPower_single_device=2000,
-    power_to_heat_ratio=1.2,  # dr? 电热?
-)
-combinedHeatAndPower.constraints_register(model1)
-
-# 燃气锅炉
-gasBoiler = GasBoiler(
-    num_hour0,
-    model1,
-    gasBoiler_device_max=5000,
-    gasBoiler_price=200,
-    gas_price=gas_price0,
-    efficiency=0.9,
-)
-gasBoiler.constraints_register(model1)
 
 # 市政蒸汽
 municipalSteam = CitySupply(
@@ -82,12 +56,6 @@ municipalSteam = CitySupply(
 )
 municipalSteam.constraints_register(model1)
 
-power_steam_used_product = model1.continuous_var_list(
-    [i for i in range(0, num_hour0)], name="power_steam_used_product"
-)
-power_steam_used_heatcool = model1.continuous_var_list(
-    [i for i in range(0, num_hour0)], name="power_steam_used_heatcool" # ? steam used for cooling ?
-)
 power_steam_sum = model1.continuous_var_list(
     [i for i in range(0, num_hour0)], name="power_steam_sum"
 )
@@ -96,10 +64,6 @@ model1.add_constraints(
     == municipalSteam.heat_citySupplied[h]
     + combinedHeatAndPower.wasteGasAndHeat_steam_device.heat_exchange[h]
     + troughPhotoThermal.power_troughPhotoThermal_steam[h]
-    + groundSourceSteamGenerator.power_groundSourceSteamGenerator_steam[h]
-    + gasBoiler.heat_gasBoiler[
-        h
-    ]  # （每小时）所有产生蒸汽量的总和 = 市政热量 + CHP余气余热蒸汽 + 槽式光热产蒸汽 + 燃气锅炉产生热量
     for h in range(0, num_hour0)
 )
 # 高温蒸汽去处
@@ -112,7 +76,7 @@ model1.add_constraints(
 steamAndWater_exchanger = Exchanger(
     num_hour0, model1, device_max=20000, device_price=400, k=50
 )
-steamAndWater_exchanger.constraints_register(model1) # qs - 泉水？ steamAndWater热交换器？
+steamAndWater_exchanger.constraints_register(model1)  # qs - 泉水？ steamAndWater热交换器？
 
 # 蒸汽溴化锂
 steamPowered_LiBr = LiBrRefrigeration(  # 蒸汽？
