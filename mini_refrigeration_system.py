3ca56102dfae3f13b1457dca9bf0ecfe4ba95ca3
YuPan <example@example.com>
2023-02-23 09:54:14 +0800

    'update'

--- mini_refrigeration_system.py
+++ mini_refrigeration_system.py
@@ -8,10 +8,13 @@ from docplex.mp.model import Model
 simulation_name = "micro_refrigeration"
 
 load = LoadGet()
-power_load = load.get_cool_load(num_hour0)
+cool_load = load.get_cool_load(num_hour0)
 
 model1 = Model(name=simulation_name)
 
 resource = ResourceGet()
 municipalHotWater_price0 = resource.get_municipalHotWater_price(num_hour0)
 
+
+# consumption and production
+model1.add_constraint(cool_load[h] == power_cooletStorage[h] for h in range(num_hour0))
