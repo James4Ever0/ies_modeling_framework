ac4b1db65d735b0500a225a9399f26dfc83d8015
YuPan <example@example.com>
2023-02-23 11:39:02 +0800

    'update'

--- generate_call_graph.py
+++ generate_call_graph.py
@@ -1,5 +1,5 @@
 filepaths= ["cpExample.py","data_visualize_utils.py", "integratedEnergySystemPrototypes.py", "config.py"] 
 import os
 for filepath in filepaths:
-    command = f"python D:\\ProgramFiles\\anaconda\\envs\\py37\\Scripts\\pyan {filepath} --uses --no-defines --colored --grouped --annotated --dot > myuses_{filepath.split('.')[0]}.dot"
-
+    command = f"pyan3 {filepath} --uses --no-defines --colored --grouped --annotated --dot > myuses_{filepath.split('.')[0]}.dot"
+    os.system(command)
