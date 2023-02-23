9d6db0ce4d8cea946eb2bbd71f27da77d34b8303
YuPan <example@example.com>
2023-02-23 15:24:02 +0800

    'update'

--- test_linearization_max_zeros.py
+++ test_linearization_max_zeros.py
@@ -5,8 +5,10 @@ model_name = "max_zeros_test"
 
 model = Model(model_name)
 
-linearization = Linearization()
+b = model.continuous_var()
 
+
+linearization = Linearization()
 linearization.max_zeros(num_hour=24, model=model, x=..., y=...)
 
 objective = ...
