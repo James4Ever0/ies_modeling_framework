c3197bba1ce8598cd5b55bc99b62e8c66beb2b72
YuPan <example@example.com>
2023-02-24 12:59:02 +0800

    'update'

--- validity_check.py
+++ validity_check.py
@@ -1,12 +1,13 @@
-def someFunction(param1,param2,param3=3):
+def someFunction(param1, param2, param3=3):
     ...
 
-arg_0 = {"param1":1}
-arg_1 = {'param1':1,"param2":2}
-arg_2 = {'param1':1,"param3":3}
-arg_3 = {'param1':1,"param2":2,'param3':3}
 
-args = [arg_0,arg_1,arg_2,arg_3]
+arg_0 = {"param1": 1}
+arg_1 = {"param1": 1, "param2": 2}
+arg_2 = {"param1": 1, "param3": 3}
+arg_3 = {"param1": 1, "param2": 2, "param3": 3}
+
+args = [arg_0, arg_1, arg_2, arg_3]
 
 for i, arg in enumerate(args):
     try:
