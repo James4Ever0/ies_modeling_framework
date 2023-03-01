143eb7530343a7234e9b4eb0a95fb41858e87018
YuPan <example@example.com>
2023-03-01 10:39:02 +0800

    'update'

--- mini_ies_test.py
+++ mini_ies_test.py
@@ -10,27 +10,34 @@ import os
 # add this or not?
 import sys
 
+
 def get_platform():
     platforms = {
-        'linux1' : 'Linux',
-        'linux2' : 'Linux',
-        'darwin' : 'OS X',
-        'win32' : 'Windows'
+        "linux1": "Linux",
+        "linux2": "Linux",
+        "darwin": "OS X",
+        "win32": "Windows",
     }
     if sys.platform not in platforms:
         return sys.platform
-    
+
     return platforms[sys.platform]
 
+
 platform = get_platform()
-if platform == 'darwin': # not my computer!
+if platform == "darwin":  # not my computer!
     os.environ["PATH"] = (
         "/Applications/CPLEX_Studio1210/cplex/bin/x86-64_osx:" + os.environ["PATH"]
     )  # not working?
 
 # print(os.environ['PATH'])
 
-from integratedEnergySystemPrototypes import GridNet, EnergyStorageSystem, PhotoVoltaic, symbols
+from integratedEnergySystemPrototypes import (
+    GridNet,
+    EnergyStorageSystem,
+    PhotoVoltaic,
+    symbols,
+)
 from demo_utils import LoadGet, ResourceGet
 from config import num_hour, day_node, epsilon
 
@@ -55,7 +62,7 @@ photoVoltaic = PhotoVoltaic(
     num_hour,
     model,
     device_count_max=5000,  # how about let's alter this?
-    device_price=4500/100,
+    device_price=4500 * 0,
     intensityOfIllumination=intensityOfIllumination,
     efficiency=0.8,
     device_name="PhotoVoltaic",
@@ -69,8 +76,8 @@ gridNet = GridNet(
     model,
     device_count_max=10000,
     device_price=0,
-    electricity_price=electricity_price*10,
-    electricity_price_upload=0.35*100,
+    electricity_price=electricity_price * 10,
+    electricity_price_upload=0.35 * 100,
     # device_count_min=5000,
 )
 gridNet.constraints_register(powerPeak_predicted=2000)
@@ -80,20 +87,20 @@ gridNet.constraints_register(powerPeak_predicted=2000)
 batteryEnergyStorageSystem = EnergyStorageSystem(
     num_hour,
     model,
-    device_count_max=20000*1000,
-    device_price=1800 / 1000,  # this won't save anything.
-    device_price_powerConversionSystem=250 / 100,
+    device_count_max=2000,
+    device_price=1800 * 0,  # this won't save anything.
+    device_price_powerConversionSystem=250 * 0,
     conversion_rate_max=2,
     efficiency=0.9,
     energy_init=1,  # this value will somehow affect system for sure. epsilon? fully charged? what is the size of the battery? let's set it to zero? (no do not do this or the system will not run. let's set it slightly greater than zero.) this parameter is not used when `register_period_constraints=1` (original) because the battery status will always stay at the same level both at the end and the start.
     stateOfCharge_min=0,  # state of charge
     stateOfCharge_max=1,
-    input_type='electricity',
-    output_type='electricity',
+    input_type="electricity",
+    output_type="electricity",
 )
 # original: battery
 batteryEnergyStorageSystem.constraints_register(  # using mode 1?
-                                                register_period_constraints=0, day_node=day_node
+    register_period_constraints=0, day_node=day_node
 )  # why it is not working under mode 0?
 
 # define energy balance restrictions
@@ -119,19 +126,21 @@ from integratedEnergySystemPrototypes import EnergyFlowNode
 
 # no checking!
 
-Node1 = EnergyFlowNode(model,num_hour,symbols.greater_equal)
-Node2 = EnergyFlowNode(model,num_hour,symbols.greater_equal)
+Node1 = EnergyFlowNode(model, num_hour, symbols.greater_equal)
+Node2 = EnergyFlowNode(model, num_hour, symbols.greater_equal)
 
-Channel1 = model.continuous_var_list([i for i in range(num_hour)], lb=0, name="channel_1")
+Channel1 = model.continuous_var_list(
+    [i for i in range(num_hour)], lb=0, name="channel_1"
+)
 
-Node1.add_input(photoVoltaic.power_of_outputs['electricity'])
-Node1.add_output(batteryEnergyStorageSystem.power_of_inputs['electricity'])
-Node1.add_output(gridNet.power_of_inputs['electricity'])
+Node1.add_input(photoVoltaic.power_of_outputs["electricity"])
+Node1.add_output(batteryEnergyStorageSystem.power_of_inputs["electricity"])
+Node1.add_output(gridNet.power_of_inputs["electricity"])
 Node1.add_output(Channel1)
 
 Node2.add_input(Channel1)
-Node2.add_input(gridNet.power_of_outputs['electricity'])
-Node2.add_input(batteryEnergyStorageSystem.power_of_outputs['electricity'])
+Node2.add_input(gridNet.power_of_outputs["electricity"])
+Node2.add_input(batteryEnergyStorageSystem.power_of_outputs["electricity"])
 Node2.add_output(power_load)
 
 Node1.build_relations()
