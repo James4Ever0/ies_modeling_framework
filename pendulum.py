58fae745e088c251d184d9b949af1b53316e68e9
YuPan <example@example.com>
2023-03-01 16:59:05 +0800

    'update'

--- pendulum.py
+++ pendulum.py
@@ -12,11 +12,11 @@ def pendulum(t, u, M):
 
 θ_0 = 0.01                          # initial angular deflection [rad]
 ω_0 = 0.0                            # initial angular velocity [rad/s]
-u_0 = [θ_0, ω0]                       # initial state vector
+u_0 = [θ_0, ω_0]                       # initial state vector
 t_span = (0.0, 10.0)                # time interval
 M = lambda t: 0.1*np.sin(t)         # external torque [Nm], function
 
-sol = solve_ivp(lambda t, y: pendulum(t, y, M), t_span, u0, method='RK45')
+sol = solve_ivp(lambda t, y: pendulum(t, y, M), t_span, u_0, method='RK45')
 
 angular_deflections = sol.y[0]
 angular_velocities = sol.y[1]
