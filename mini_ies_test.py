1b33b811ab44e84b0aa0f4ef49d2174f4bd11d74
YuPan <example@example.com>
2023-03-01 09:14:03 +0800

    'update'

--- mini_ies_test.py
+++ mini_ies_test.py
@@ -7,6 +7,20 @@
 
 import os
 
+# add this or not?
+import sys
+
+def get_platform():
+    platforms = {
+        'linux1' : 'Linux',
+        'linux2' : 'Linux',
+        'darwin' : 'OS X',
+        'win32' : 'Windows'
+    }
+    if sys.platform not in platforms:
+        return sys.platform
+    
+    return platforms[sys.platform]
 os.environ["PATH"] = (
     "/Applications/CPLEX_Studio1210/cplex/bin/x86-64_osx:" + os.environ["PATH"]
 )  # not working?
