0e41bdc3e5399a4cd18066c8ed2b3a76db371cd9
YuPan <example@example.com>
2023-03-03 14:44:02 +0800

    'update'

--- system_topology_utils.py
+++ system_topology_utils.py
@@ -1,17 +1,33 @@
 from integratedEnergySystemPrototypes import EnergyFlowNodeFactory
 import networkx as nx
 import matplotlib.pyplot as plt
-def visualizeSystemTopology(NodeFactory:EnergyFlowNodeFactory):
+
+
+def visualizeSystemTopology(
+    NodeFactory: EnergyFlowNodeFactory,
+    draw_options={
+        "node_color": "yellow",
+        "node_size": 1000,
+    },
+):
     G = nx.DiGraph()
-    node_index=0
+    node_index = 0
+
     for device_id in NodeFactory.device_ids:
-        device_node_name = f'{device_id}_device'
-    draw_options = {'node_color': 'yellow','node_size': 1000,}
+        device_node_name = f"{device_id}_device"
+        G.add_node(device_node_name)
+
     for node in NodeFactory.nodes:
         node_name = f"{node.energy_type}_{node_index}"
         G.add_node(node_name)
-        node_index+=1
+        node_index += 1
         # G.add_node(2,"BESS")
-        for device_node in NodeFactory.input_ids:
-            device_node_name=f"{device_node.}"
-    
+        for input_id in node.input_ids:
+            device_id = input_id.split("_")[0]
+            device_node_name = f"{device_node}_device"
+            G.add_edge(device_node_name,node_name)
+
+        for output_id in node.output_ids:
+            input_id.split("_")[0]
+            device_node_name = f"{device_node}_device"
+            G.add_edge(node_name,device_node_name)
