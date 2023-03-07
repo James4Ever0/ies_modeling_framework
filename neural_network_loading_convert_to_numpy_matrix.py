ee524c5f9baa10766286f6c3225b3a75bea69112
YuPan <example@example.com>
2023-03-07 16:09:04 +0800

    'update'

--- neural_network_loading_convert_to_numpy_matrix.py
+++ neural_network_loading_convert_to_numpy_matrix.py
@@ -1,6 +1,15 @@
 from neural_network_demo import model, input_layer, hidden_layer, output_layer
 import numpy as np
 
+######## need that hard
+
 model_save_path = "model.pt"
 
 import torch
+
+model.load_state_dict(torch.load(model_save_path))
+model.eval()
+
+# input_layer.bias
+bias_list = [input_layer.bias.detach().numpy(),hidden_layer.bias.detach().numpy(),]
+breakpoint()
