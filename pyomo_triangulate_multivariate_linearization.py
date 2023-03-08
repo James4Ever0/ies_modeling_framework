04e9b6ea01993ee1b7b11d9ba11ba36ccf292e0b
YuPan <example@example.com>
2023-03-08 11:14:13 +0800

    'update'

--- pyomo_triangulate_multivariate_linearization.py
+++ pyomo_triangulate_multivariate_linearization.py
@@ -24,3 +24,4 @@ from pyomo.core.kernel.piecewise_library.transforms_nd import piecewise_nd
 piecewise_z_func = piecewise_nd(delaunay,z_vals)
 
 # breakpoint()
+# can it be used in actual optimization?
