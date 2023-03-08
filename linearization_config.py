0c99e841f73982863109a97dbb093c19a4943220
YuPan <example@example.com>
2023-03-08 10:34:02 +0800

    'update'

--- linearization_config.py
+++ linearization_config.py
@@ -1,4 +1,8 @@
+import numpy as np
+
 x_lb, x_ub = 0,3
 y_lb, y_ub = 0, 2*np.pi
 
 x_sample_size = y_sample_size = 100
+
+z_func = lambda x,y:x*np.sin(y)
--- pyomo_triangulate_multivariate_linearization.py
+++ pyomo_triangulate_multivariate_linearization.py
@@ -1,6 +1,14 @@
 import pyomo
-
+import pyomo.core.kernel.piecewise_library.util
 from linearization_config import *
 
 # func: z = x*sin(y)
 
+from pyomo.core.kernel.variable import variable
+
+x =variable(lb=x_lb,ub=x_ub)
+y =variable(lb=y_lb,ub=y_ub)
+
+delaunay = pyomo.core.kernel.piecewise_library.util.generate_delaunay([x,y],num=x_sample_size)
+
+function = 
