415ed3c01e5e4faa749134dcca2159f8b249effc
YuPan <example@example.com>
2023-03-09 12:49:05 +0800

    'update'

--- pyomo_test_solve_nonlinear_cplex.py
+++ pyomo_test_solve_nonlinear_cplex.py
@@ -4,19 +4,19 @@
 from math import pi # do not use `sin` from here!
 
 # pip install pyomocontrib_simplemodel
-
+from pyomo.environ import *
 # from pyomo.core.expr import current as EXPR
-from pyomo.core import ConcreteModel, Var, Objective, minimize, sin
+# from pyomo.core import ConcreteModel, Var, Objective, minimize, sin
 
 model = ConcreteModel()
 model.x = Var(bounds=(0, 3))
 model.y = Var(bounds=(0, 2 * pi))
-
+model.z = Vaer
 
 def ObjRule(model):
     return model.x * sin(model.y)
 
-from pyomo.opt import SolverFactory
+# from pyomo.opt import SolverFactory # not good!
 
 model.obj2 = Objective(rule=ObjRule, sense=minimize)
 
