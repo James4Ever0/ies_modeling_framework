005862bf27d6327c0f848adf836f1fbd96d0ac10
YuPan <example@example.com>
2023-03-09 12:54:25 +0800

    'update'

--- pyomo_test_solve_nonlinear_cplex.py
+++ pyomo_test_solve_nonlinear_cplex.py
@@ -1,25 +1,31 @@
 # no enviroment tweaks. (will not work at all) just create shim for scoop.
 
+# MINLP open source solvers:
+# https://pyomo.readthedocs.io/en/stable/contributed_packages/mindtpy.html
+# SHOT
+
 # from pyomo.contrib.simplemodel import SimpleModel
-from math import pi # do not use `sin` from here!
+from math import pi  # do not use `sin` from here!
 
 # pip install pyomocontrib_simplemodel
-from pyomo.environ import *
+from pyomo.environ import * # different approaches gives different import paths. may not always share all solvers.
+
 # from pyomo.core.expr import current as EXPR
 # from pyomo.core import ConcreteModel, Var, Objective, minimize, sin
 
 model = ConcreteModel()
 model.x = Var(bounds=(0, 3))
 model.y = Var(bounds=(0, 2 * pi))
-model.z = Vaer
+model.z = Var()
 
-def ObjRule(model):
-    return model.x * sin(model.y)
+model.c1 = Constraint(expr=model.z == model.x * sin(model.y))
+
 
 # from pyomo.opt import SolverFactory # not good!
 
-model.obj2 = Objective(rule=ObjRule, sense=minimize)
+model.obj = Objective(expr=model.z, sense=minimize)
 
-opt = SolverFactory('mindtpy')
-solution = opt.solve(model)
-breakpoint()
+opt = SolverFactory("mindtpy")
+opt.solve(model, mip_solver="cplex", nlp_solver="ipopt", tee=True)  # nothing returned?
+model.obj.display()
+# breakpoint()
