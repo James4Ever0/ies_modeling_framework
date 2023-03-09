132c710018c82888309f5979a8bc4c65324b8515
YuPan <example@example.com>
2023-03-09 15:54:03 +0800

    'update'

--- pyomo_shot_test.py
+++ pyomo_shot_test.py
@@ -1,1 +1,41 @@
 from pyomo.environ import *  # different approaches gives different import paths. may not always share all solvers.
+# no enviroment tweaks. (will not work at all) just create shim for scoop.
+
+# MINLP open source solvers:
+# https://pyomo.readthedocs.io/en/stable/contributed_packages/mindtpy.html
+# SHOT
+# https://projects.coin-or.org/Couenne
+# https://projects.coin-or.org/Bonmin
+
+# from pyomo.contrib.simplemodel import SimpleModel
+from math import pi  # do not use `sin` from here!
+
+# pip install pyomocontrib_simplemodel
+from pyomo.environ import *  # different approaches gives different import paths. may not always share all solvers.
+
+# from pyomo.core.expr import current as EXPR
+# from pyomo.core import ConcreteModel, Var, Objective, minimize, sin
+
+model = ConcreteModel()
+model.x = Var(bounds=(0, 3))
+model.y = Var(bounds=(0, 2 * pi))
+model.z = Var()
+
+model.c1 = Constraint(expr=model.z == model.x * sin(model.y))
+
+
+# from pyomo.opt import SolverFactory # not good!
+
+model.obj = Objective(expr=model.z, sense=minimize)
+
+opt = SolverFactory("mindtpy")  # <- this thing does not support.
+opt.solve(model, mip_solver="cplex", 
+        #   nlp_solver="SHOT",
+          nlp_solver="ipopt",
+        #  Error: value SHOT not in domain ['ipopt', 'appsi_ipopt', 'gams', 'baron']
+          tee=True)
+model.obj.display()
+model.x.display()
+model.y.display()
+
+opt = SolverFactory("SHOT") # working!
