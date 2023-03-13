74fe9e310743b30bd771d55109ddcd77f6e9193a
YuPan <example@example.com>
2023-03-13 11:19:02 +0800

    'update'

--- test_client.py
+++ test_client.py
@@ -1,6 +1,10 @@
 import requests
 import json
 
+port = 3398
+
 with open("test_graph_data.json",'r') as f:
     data = f.read()
     data_dict = json.loads(data)
+
+requests.post(url,)
--- test_server.py
+++ test_server.py
@@ -2,4 +2,9 @@ import fastapi
 import time
 
 def mock_calculation(sleep_time:float=20):
+    """
+    mocking the heavy calculation of system optimization.
+
+    
+    """
     time.sleep(sleep_time)
