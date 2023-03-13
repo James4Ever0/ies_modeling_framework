4337eef51475856f348201774791d5c546d21430
YuPan <example@example.com>
2023-03-13 14:09:01 +0800

    'update'

--- test_server.py
+++ test_server.py
@@ -20,41 +20,50 @@ app = FastAPI()
 
 # of course we will set limit to max running time per task
 
-GLOBAL_TASK_COUNT=0
+GLOBAL_TASK_COUNT = 0
 
 # could there be multiple requests? use lock please?
 import threading
+
 LOCK = threading.Lock()
+
 
 def add_one_task():
     global GLOBAL_TASK_COUNT, LOCK
     with LOCK:
         if GLOBAL_TASK_COUNT < MAX_TASK_COUNT:
-            GLOBAL_TASK_COUNT+=1
+            GLOBAL_TASK_COUNT += 1
             return True
     return False
+
 
 def remove_one_task():
     global GLOBAL_TASK_COUNT, LOCK
     with LOCK:
-        if GLOBAL_TASK_COUNT >=0 and GLOBAL_TASK_COUNT<=MAX_TASK_COUNT:
-            GLOBAL_TASK_COUNT-=1
+        if GLOBAL_TASK_COUNT >= 0 and GLOBAL_TASK_COUNT <= MAX_TASK_COUNT:
+            GLOBAL_TASK_COUNT -= 1
             return True
     return False
 
 
-class MaxTaskLimit():
+class MaxTaskLimit:
     def __init__(self):
-        ...
+        if not add_one_task():
+            WARNING = "Max Running Tasks!"
+            print(WARNING)
+            raise Exception(WARNING)
+
     def __enter__(self):
         return self
-     
+
     def __exit__(self, exc_type, exc_value, exc_traceback):
         ...
 
+
 @app.post(f"/{endpoint_suffix.UPLOAD_GRAPH}")
 def run_sync():
-    ...
+    with MaxTaskLimit():
+        
 
 
 @app.post(f"/{endpoint_suffix.UPLOAD_GRAPH_ASYNC}")
