ad59670f57e2792fbf5c162afd45f27010051da3
YuPan <example@example.com>
2023-03-13 17:24:23 +0800

    'update'

--- cloudpss_model_revised.py
+++ cloudpss_model_revised.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from pydantic import BaseModel
 
 @dataclass
 class 设备:
@@ -6,6 +7,13 @@ class 设备:
     生产型号:str
     设备额定运行参数:dict
     设备运行约束:dict
+    设备经济性参数:dict
 
 class 光伏:
-    class 光伏设备额定运行参数
+    class 光伏设备额定运行参数(BaseModel):
+        光伏板面积
+        光电转换效率
+        功率因数
+    
+    class 光伏设备运行约束(BaseModel):
+        最大发电功率
