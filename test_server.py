b97ee500014f0fa796b5acb687d0e0269bd7c795
YuPan <example@example.com>
2023-03-13 13:59:19 +0800

    'update'

--- test_server.py
+++ test_server.py
@@ -18,6 +18,30 @@ app = FastAPI()
 
 # create some context manager? sure?
 
+# of course we will set limit to max running time per task
+
+GLOBAL_TASK_COUNT=0
+
+# could there be multiple requests? use lock please?
+
+def add_one_task():
+    global GLOBAL_TASK_COUNT
+    if GLOBAL_TASK_COUNT < MAX_TASK_COUNT:
+        GLOBAL_TASK_COUNT+=1
+        return True
+
+
+class MaxTaskLimit():
+    def __init__(self):
+        
+        # print('init method called')
+         
+    def __enter__(self):
+        # print('enter method called')
+        return self
+     
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        # print('exit method called')
 
 @app.post(f"/{endpoint_suffix.UPLOAD_GRAPH}")
 def run_sync():
