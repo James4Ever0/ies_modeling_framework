7583c215a7358f692eed24f0e7b3cc257384e57a
YuPan <example@example.com>
2023-03-13 13:54:02 +0800

    'update'

--- test_server.py
+++ test_server.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 import time
 from test_server_client_configs import *
 
-def mock_calculation(sleep_time:float=20):
+
+def mock_calculation(sleep_time: float = 20):
     """
     Mocking the heavy calculation of system optimization.
 
@@ -11,21 +12,28 @@ def mock_calculation(sleep_time:float=20):
     """
     time.sleep(sleep_time)
 
+
 app = FastAPI()
 # where is the port?
 
-@app.post()
+# create some context manager? sure?
+
+
+@app.post(f"/{endpoint_suffix.UPLOAD_GRAPH}")
 def run_sync():
     ...
 
-@app.post()
+
+@app.post(f"/{endpoint_suffix.UPLOAD_GRAPH_ASYNC}")
 def run_async():
     ...
 
-@app.post()
+
+@app.post(f"/{endpoint_suffix.CHECK_RESULT_ASYNC}")
 def get_result_async():
     ...
 
+
 import uvicorn
 
-uvicorn.run(app,port=port)
+uvicorn.run(app, port=port)
