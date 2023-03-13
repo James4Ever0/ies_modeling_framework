30a2615c2c20335faa0ced605bc8918d20cb36d7
YuPan <example@example.com>
2023-03-13 11:24:03 +0800

    'update'

--- test_client.py
+++ test_client.py
@@ -7,4 +7,17 @@ with open("test_graph_data.json",'r') as f:
     data = f.read()
     data_dict = json.loads(data)
 
-requests.post(url,)
+url_base= f"http://localhost:{port}"
+
+get_url = lambda suffix: f"{url_base}/{suffix}"
+
+# will refuse connection if current task count is above 3.
+
+url = get_url("upload_graph")
+
+async_url = get_url("upload_graph_async")
+check_result_async = get_url("check_result_async")
+
+r = requests.post(url,data_dict)
+status_code = r.status_code
+r_json = r.json()
--- test_server.py
+++ test_server.py
@@ -1,10 +1,12 @@
 import fastapi
 import time
 
+MAX_TASK_COUNT=3
+
 def mock_calculation(sleep_time:float=20):
     """
     mocking the heavy calculation of system optimization.
 
-    
+
     """
     time.sleep(sleep_time)
