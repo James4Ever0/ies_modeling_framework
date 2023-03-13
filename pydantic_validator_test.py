5d61c0c2e37aae63667c7959e968712a5fed0598
YuPan <example@example.com>
2023-03-13 15:34:02 +0800

    'update'

--- pydantic_validator_test.py
+++ pydantic_validator_test.py
@@ -6,5 +6,11 @@ class TestDataClass(BaseModel):
     key_1:str
     key_2:Optional[str]
     key_3:str
+    
     @validator("key_1")
-    def validate_key_1():
+    def validate_key_1(cls,value):
+        json.loads(value) # will raise error if it is not parseable.
+    
+# try parsing?
+obj = dict(key_1={"k":"abc"},key_3='def')
+TestDataClass.parse_raw(json.dumps(obj))
