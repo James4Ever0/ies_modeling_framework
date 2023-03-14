9a19922ff5a6cff00337323eae013ac78876d560
YuPan <example@example.com>
2023-03-14 10:54:19 +0800

    'update'

--- cloudpss_code_generator.py
+++ cloudpss_code_generator.py
@@ -4,26 +4,39 @@ import rich
 
 load_path = "cloudpss_inputs.json"
 
-with open(load_path,'r',encoding='utf-8') as f:
+with open(load_path, "r", encoding="utf-8") as f:
     data = json.loads(f.read())
     # rich.print(data)
 
-excelMap = data['excelMap']
+excelMap = data["excelMap"]
 
-dataParams = {"ratedParam":"","":"","":""}
+dataParams = {
+    "ratedParam": "设备额定运行参数",
+    "operationalConstraints": "设备运行约束",
+    "economicParam": "设备经济性参数",
+}
+
+# 设备额定运行参数
+# 设备运行约束
+# 设备经济性参数
+# 设备工况
 
 for key, value in excelMap.items():
     if type(value) == dict:
         if "生产厂商" in value.keys():
+            print("DEVICE NAME:", key)
             # this is a device for sure.
             # rich.print(value)
-            for k,v in value.items():
-                if v.split(".")[0] in dataParams.keys():
-                    k0 = dataParams[v.split(".")[0]]
-                    print(k0, k, v.split(".")[-1])
-                else in ['manufacturer','equipType']:
-                    print(k,v)
+            for k, v in value.items():
+                if type(v) == str:
+                    if v.split(".")[0] in dataParams.keys():
+                        k0 = dataParams[v.split(".")[0]]
+                        print(k0, k, v.split(".")[-1])
+                    else:
+                        if v not in ["manufacturer", "equipType"]:
+                            print(">> UNIDENTIFIED PARAM TYPE <<")
+                        print(k, v)
                 else:
-                    print(">> UNIDENTIFIED PARAM TYPE <<")
-                    print(k,v)
-            print("_"*30)
+                    print(">> UNIDENTIFIED VALUE TYPE <<")
+                    print(k, type(v), v)
+            print("_" * 30)
