2b3bd94fd0d8dda41dded156a31c17dde4597f89
YuPan <example@example.com>
2023-03-14 10:14:02 +0800

    'update'

--- cloudpss_unit_conversions_test.py
+++ cloudpss_unit_conversions_test.py
@@ -1,8 +1,9 @@
 # there involves multiplication, division, scale factors.
 import pint
+# similar projects: https://pint.readthedocs.io/en/stable/getting/faq.html
 ureg = pint.UnitRegistry()
 
 a = ureg.cm
 print(a) # centimeter?
-# b = ureg.RMB # not defined? let's define new units?
+# b = ureg.dollar # not defined? let's define new units?
 # print(b)
