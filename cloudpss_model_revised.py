2f30936b0a0377dac93250b100bcfc4c6b93dce9
YuPan <example@example.com>
2023-03-14 15:09:12 +0800

    'update'

--- cloudpss_model_revised.py
+++ cloudpss_model_revised.py
@@ -10,10 +10,10 @@ class 设备:
         model,
         生产厂商: str,
         生产型号: str,
-        设备额定运行参数: dict = {},
+        设备额定运行参数: dict = {},  # if any
         设备运行约束: dict = {},  # if any
         设备经济性参数: dict = {},  #  if any
-        设备工况: dict = {},  # OperateParam (vue)
+        设备工况: dict = {},  # OperateParam
     ):
         self.model = model
         self.生产厂商 = 生产厂商
@@ -23,20 +23,27 @@ class 设备:
         self.设备经济性参数 = 设备经济性参数
         self.设备工况 = 设备工况
 
-    def 设置设备额定运行参数(self):
-        ...
-    
-    def 设置设备运行约束(self):
-        ...
-    
-    def 设置设备经济性参数(self):
-        ...
-
-    def 设置设备工况(self):
-        ...
-
 
 class 光伏(设备):
+    def __init__(
+        self,
+        model,
+        生产厂商: str,
+        生产型号: str,
+        设备额定运行参数: dict = {},  # if any
+        设备运行约束: dict = {},  # if any
+        设备经济性参数: dict = {},  #  if any
+        设备工况: dict = {},  # OperateParam
+    ):
+        super().__init__(
+            model=model,
+            生产厂商=生产厂商,
+            生产型号=生产型号,
+            设备额定运行参数=设备额定运行参数,
+            设备运行约束=设备运行约束,
+            设备经济性参数=设备经济性参数,
+            设备工况=设备工况,
+        )
 
     def 设备额定运行参数(
         self,
@@ -44,11 +51,13 @@ class 光伏(设备):
         光伏板面积: float,
         光电转换效率: float,
         功率因数: float,
+        设备数量: int,
         输出功率: float,
         最大输出功率: float,
         光照强度: float,
     ):
         self.model = model
+        self.设备数量 = 设备数量
         self.光伏板面积 = 光伏板面积
         self.光电转换效率 = 光电转换效率
         self.功率因数 = 功率因数
--- test_class_create.py
+++ test_class_create.py
@@ -1,13 +1,16 @@
 
-class new_class(test_class):
+class new_class:
     def __init__(self,data, value):
         self.data = data
         self.value = value
+        """
+        hint of value
+        """
         self.execute_init(2)
     def execute_init(self,val):
         self.val = val
         """
-        some hint of val?
+        some hint of val? will not be displayed
         """
 
 a = new_class(4,5)
