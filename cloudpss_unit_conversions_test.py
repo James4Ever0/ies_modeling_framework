8088257f1783e24648870a845a96ace201415edf
YuPan <example@example.com>
2023-03-14 10:29:02 +0800

    'update'

--- cloudpss_unit_conversions_test.py
+++ cloudpss_unit_conversions_test.py
@@ -1,10 +1,11 @@
 # there involves multiplication, division, scale factors.
 import pint
+
 # similar projects: https://pint.readthedocs.io/en/stable/getting/faq.html
 ureg = pint.UnitRegistry()
 
-a = 1*ureg.cm
-print(a) # centimeter?
+a = 1 * ureg.cm
+print(a)  # centimeter?
 
 # either load from definition file or just here.
 # https://pint.readthedocs.io/en/stable/advanced/defining.html
@@ -14,9 +15,9 @@ ureg.define("千元 = 1000 * 元")
 ureg.define("万元 = 10000 * 元")
 ureg.define("亿元 = 10000 * 万元")
 # b = ureg.dollar # not defined? let's define new units?
-b = 1*ureg.元
+b = 1 * ureg.元
 print(b)
 
 c = ureg.celsius
 # you cannot do this: ureg['℃']
-print(c) # invalid charactor: ℃
+print(c)  # invalid charactor: ℃
