44717381c426c8353d62bd70e0c791e95f2b320e
YuPan <example@example.com>
2023-03-14 10:49:01 +0800

    'update'

--- cloudpss_code_generator.py
+++ cloudpss_code_generator.py
@@ -10,9 +10,20 @@ with open(load_path,'r',encoding='utf-8') as f:
 
 excelMap = data['excelMap']
 
+dataParams = {"ratedParam":"","":"","":""}
+
 for key, value in excelMap.items():
     if type(value) == dict:
         if "生产厂商" in value.keys():
             # this is a device for sure.
-            rich.print(value)
+            # rich.print(value)
+            for k,v in value.items():
+                if v.split(".")[0] in dataParams.keys():
+                    k0 = dataParams[v.split(".")[0]]
+                    print(k0, k, v.split(".")[-1])
+                else in ['manufacturer','equipType']:
+                    print(k,v)
+                else:
+                    print(">> UNIDENTIFIED PARAM TYPE <<")
+                    print(k,v)
             print("_"*30)
