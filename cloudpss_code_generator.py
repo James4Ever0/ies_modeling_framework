f4f1782411ec990f7f12851dfe38397dd79cea80
YuPan <example@example.com>
2023-03-14 09:59:02 +0800

    'update'

--- cloudpss_code_generator.py
+++ cloudpss_code_generator.py
@@ -1,4 +1,15 @@
 import jinja2
 import json
+import rich
 
-with open()
+load_path = "cloudpss_inputs.json"
+
+with open(load_path,'r',encoding='utf-8') as f:
+    data = json.loads(f.read())
+    # rich.print(data)
+
+excelMap = data['excelMap']
+
+for key, value in excelMap.items():
+    if type(value) == dict:
+        if "" in 
