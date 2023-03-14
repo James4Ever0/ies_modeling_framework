1877509f5d70da81681a224d5bd6046c95f1560d
YuPan <example@example.com>
2023-03-14 10:19:02 +0800

    'update'

--- cloudpss_unit_conversions_test.py
+++ cloudpss_unit_conversions_test.py
@@ -5,5 +5,13 @@ ureg = pint.UnitRegistry()
 
 a = ureg.cm
 print(a) # centimeter?
+
+# either load from definition file or just here.
+# https://pint.readthedocs.io/en/stable/advanced/defining.html
+ureg.define("元 = [currency]")
+ureg.define("百元 = 100 * 元")
+ureg.define("千元 = 1000 * 元")
+ureg.define("万元 = 10000 * 元")
+ureg.define("亿元 = 10000 * 万元")
 # b = ureg.dollar # not defined? let's define new units?
 # print(b)
