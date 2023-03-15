169583524ab4b8b58368915b7864f2cf0d7d19e3
YuPan <example@example.com>
2023-03-15 17:58:53 +0800

    'update'

--- regex_extract_bracket.py
+++ regex_extract_bracket.py
@@ -1,9 +1,15 @@
 import re
 
 source = "额定发电功率(kW)"
+source_2 = "生产厂商"
 
-a=re.match("",source)
-
+pattern = r'(\w+)\((\w+)\)'
+result = re.findall(pattern, source_2)
+if len(result) > 0:
+    a, b = result[0]
+    print(f"a={a}\nb={b}")
+else:
+    print(f"a={source_2}")
 #a = "额定发电功率"
 #b = "kW"
 
