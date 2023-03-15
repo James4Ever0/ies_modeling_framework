5b47e4b48b93bc0d34a45753df2f3ecaa95401e4
YuPan <example@example.com>
2023-03-15 17:19:11 +0800

    'update'

--- cloudpss_unit_create_convert_compatible.py
+++ cloudpss_unit_create_convert_compatible.py
@@ -5,3 +5,5 @@ ureg = pint.UnitRegistry()
 
 ureg.define("元 = [currency]")
 ureg.define("dollar = 7 元")
+
+compatible_units = ureg.get_compatible_units("元",group_or_system=myGroup) # maybe in different unit system?
--- jinja_test.py
+++ jinja_test.py
@@ -1,9 +1,18 @@
-import jinja2
+from jinja2 import Environment,FileSystemLoader
 
-template = open('jinja_test.j2','r').read()
+#template = open('jinja_test.j2','r').read()
 
-mylist = [1,2,3]
-
+#mylist = [1,2,3]
+def main():
+    env = Environment(loader = FileSystemLoader('./'))
+    tpl = env.get_template('jinja_test.j2')
+    
+    with open('page.txt','w+') as fout:
+        render_content = tpl.render(mylist = [1,2,3])
+        fout.write(render_content)
+        
+if __name__ == '__main__':
+    main()
 # print:
 # prefix 1
 # prefix 2
