bcbc3d4ca86b3152424002da50e869c0c09284a8
YuPan <example@example.com>
2023-03-15 17:49:02 +0800

    'update'

--- cloudpss_unit_create_convert_compatible.py
+++ cloudpss_unit_create_convert_compatible.py
@@ -1,17 +1,24 @@
 import pint
 
 # similar projects: https://pint.readthedocs.io/en/stable/getting/faq.html
-ureg = pint.UnitRegistry()
-
+ureg = pint.UnitRegistry(filename = "currency_units.txt")
+ureg.enable_contexts()
 # myGroup = ureg.Group("new_group")
 # ureg.define("元 = [currency]")
 # ureg.define("dollar = 7 元")
-ureg.load_definitions("currency_units.txt")
+# ureg.load_definitions("currency_units.txt")
+# ureg.define("dollar = [currency]")
+# ureg.define("million_dollar = 10000000 dollar")
 
 # error! 
 
 # File "D:\ProgramFiles\anaconda\envs\py37\lib\site-packages\pint\registry.py", line 939, in _get_compatible_units
 #     return self._cache.dimensional_equivalents[src_dim]
 
+print("EQUIVS:")
+print(ureg._cache.dimensional_equivalents)
 
 # compatible_units = ureg.get_compatible_units("元") # maybe in different unit system?
+
+# compatible_units = ureg.get_compatible_units("dollar")
+# print("COMPAT:", compatible_units)
---
+++ regex_extract_bracket.py
@@ -0,0 +1,7 @@
+import re
+
+source = "额定发电功率(kW)"
+
+a = "额定发电功率"
+b = "kW"
+
