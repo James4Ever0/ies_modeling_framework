9dffc4a92df886085c7b9f4ecd8f45ec7a0eb0d1
YuPan <example@example.com>
2023-03-16 10:29:20 +0800

    'update'

--- pyomo_define_array_list.py
+++ pyomo_define_array_list.py
@@ -7,20 +7,28 @@ from pyomo.environ import *
 arr = np.arange(10)
 # 定义参数
 model = ConcreteModel()
-model.A = Param(range(10), initialize={i: arr[i] for i in range(10)}) # index: 0 to 9
+# model.A = Param(range(10), initialize={i: arr[i] for i in range(10)}) # index: 0 to 9
 
 
 ##2. 定义变量组：可以使用pyomo.environ.Set组件定义变量组，并使用pyomo.environ.Var组件定义变量。例如，定义一个包含10个变量的变量组，可以使用以下代码：
 # 定义变量组
-model.I = Set(initialize=range(10)) # index: 1 to 10
+model.I = Set(initialize=range(2))
+# model.I = Set(initialize=range(10)) # index: 1 to 10
 # 定义变量
-model.x = Var(model.I, initialize=0)
+model.x = Var(model.I, initialize=0, domain=(-10,10)) # index from 0 to 1
 ##在上述代码中，我们定义了一个变量组I，其中包含10个变量，然后使用pyomo.environ.Var组件定义了一个变量x，它是变量组I中的每个变量。
+# breakpoint()
+# model.Y = Var()
+
+Constraint(model.I[1] == 2*model.x)
+Constraint(model.I[2] == 2*model.y)
+
 
 # breakpoint()
 
 def objective(self):
-    return sum(model.I[i] * model.A[j] for i in range(1,11) for j in range(10))
+    # return sum(model.I[i] * model.A[j] for i in range(1,11) for j in range(10))
+    return sum(model.I[1]+model.I[2])
 
 
 model.OBJ = Objective(rule=objective)
