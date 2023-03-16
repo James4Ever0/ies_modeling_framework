e0ec42f6db0cc68ca9fc946b8c6816dd64dddfc8
YuPan <example@example.com>
2023-03-16 11:34:12 +0800

    'update'

--- pint_convert_units.py
+++ pint_convert_units.py
@@ -1,16 +1,18 @@
 import pint
 
-def unitFactorCalculator(ureg:pint.UnitRegistry, standard_units:frozenset,old_unit_name:str): # like "元/kWh"
-    assert old_unit_name !=""
+
+def unitFactorCalculator(
+    ureg: pint.UnitRegistry, standard_units: frozenset, old_unit_name: str
+):  # like "元/kWh"
+    assert old_unit_name != ""
     assert type(old_unit_name) == str
     ## now, the classic test?
     try:
-        quantity = ureg.Quantity(1, old_unit_name) # one, undoubtable.
+        quantity = ureg.Quantity(1, old_unit_name)  # one, undoubtable.
     except:
         raise Exception("Unknown unit name:", old_unit_name)
     # quantity = ureg.Quantity(1, ureg.元/ureg.kWh)
     magnitude, units = quantity.to_tuple()
-
 
     new_units_list = []
     for unit, power in units:
@@ -23,7 +25,9 @@ def unitFactorCalculator(ureg:pint.UnitRegistry, standard_units:frozenset,old_un
                 # ready to convert?
                 unit = str(list(intersection)[0])
             else:
-                raise Exception("Too many intersections with standard units:", intersection)
+                raise Exception(
+                    "Too many intersections with standard units:", intersection
+                )
         else:
             raise Exception("no common units")
         new_units_list.append((unit, power))
@@ -37,7 +41,7 @@ def unitFactorCalculator(ureg:pint.UnitRegistry, standard_units:frozenset,old_un
     print("NEW QUANTITY:", new_quantity)
 
     # get the magnitude?
-    new_magnitude = new_quantity.magnitude # you multiply that.
+    new_magnitude = new_quantity.magnitude  # you multiply that.
     print("FACTOR:", new_magnitude)
     new_unit_name = str(new_unit)
     print("NEW UNIT NAME:", new_unit_name)
