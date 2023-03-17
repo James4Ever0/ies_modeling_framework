a9e7ab4486d8f69ca86be6651df938c693f38cfa
YuPan <example@example.com>
2023-03-17 16:49:14 +0800

    'update'

---
+++ fastapi_client_test.py
@@ -0,0 +1,12 @@
+from fastapi_server_with_doc import port
+import requests
+
+baseurl = "http://127.0.0.1:" + port + "/"
+
+url = baseurl + "items"
+json_data = dict(
+        name="hello"
+        price=1
+        is_offer=False,
+        myDict={'mydict':1})
+r = requests.post(url,json=json_data)
--- fastapi_server_with_doc.py
+++ fastapi_server_with_doc.py
@@ -1,99 +1,100 @@
 # test to create a server with fastapi, generate doc automatically.
 # code from: https://fastapi.tiangolo.com/tutorial/metadata/
 
-from fastapi import FastAPI, HTTPException
-
-AppName = "ChimichangApp"
-description = f"""
-{AppName} API helps you do awesome stuff. 🚀
-
-## Items
-
-You can **read items**.
-
-## Users
-
-You will be able to:
-
-* **Create users** (_not implemented_).
-* **Read users** (_not implemented_).
-"""  # this is not docstring. this is passed as parameter.
-
-app = FastAPI(
-    title=AppName,
-    description=description,
-    version="0.0.1",
-    terms_of_service="http://example.com/terms/",
-    contact={
-        "name": "Deadpoolio the Amazing",
-        "url": "http://x-force.example.com/contact/",
-        "email": "dp@x-force.example.com",
-    },  # contact?
-    license_info={
-        "name": "Apache 2.0",
-        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
-    },
-)
-
-
-@app.get("/items/")
-async def read_items():
-    return [{"name": "Katana"}]
-
-
-from pydantic import BaseModel
-from typing import Mapping
-
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool = None
-    myDict: Mapping
-
-
-inventory = []
-
-
-class ResponseModel(BaseModel):
-    ans: str
-    ans_1: str
-
-# when it is async, no parallelism!
-# but who needs that?
-@app.post(
-    "/items/",
-    description="api for creating an item",
-    response_description="respond if creation is successful.",
-    response_model=ResponseModel,
-)
-async def create_item(item: Item):
-    """
-    Create a new item.
-    ## Parameters
-    - **item**: Item object to create.
-    ## Returns
-    The created item.
-    ## Errors
-    - **400 Bad Request**: Invalid request data.
-    """
-    if item.name == "foo":
-        raise HTTPException(status_code=400, detail="Item name cannot be foo.")
-    inventory.append(item)
-    return ResponseModel(ans="1", ans_1="2")
-
-
-# how to generate doc?
-# visit: http://<host_ip>:9981/docs
-#
-# how to export doc?
-#
-# By default, the OpenAPI schema is served at /openapi.json
-# this json file is needed to create project in apifox.
-# what is that json anyway?
-
 port = 9982
 
-import uvicorn
+if __name__ == "__main__":
+    from fastapi import FastAPI, HTTPException
 
-uvicorn.run(app, host="0.0.0.0", port=port)
+    AppName = "ChimichangApp"
+    description = f"""
+    {AppName} API helps you do awesome stuff. 🚀
+
+    ## Items
+
+    You can **read items**.
+
+    ## Users
+
+    You will be able to:
+
+    * **Create users** (_not implemented_).
+    * **Read users** (_not implemented_).
+    """  # this is not docstring. this is passed as parameter.
+
+    app = FastAPI(
+        title=AppName,
+        description=description,
+        version="0.0.1",
+        terms_of_service="http://example.com/terms/",
+        contact={
+            "name": "Deadpoolio the Amazing",
+            "url": "http://x-force.example.com/contact/",
+            "email": "dp@x-force.example.com",
+        },  # contact?
+        license_info={
+            "name": "Apache 2.0",
+            "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+        },
+    )
+
+
+    @app.get("/items/")
+    async def read_items():
+        return [{"name": "Katana"}]
+
+
+    from pydantic import BaseModel
+    from typing import Mapping
+
+
+    class Item(BaseModel):
+        name: str
+        price: float
+        is_offer: bool = None
+        myDict: Mapping
+
+
+    inventory = []
+
+
+    class ResponseModel(BaseModel):
+        ans: str
+        ans_1: str
+
+    # when it is async, no parallelism!
+    # but who needs that?
+    @app.post(
+        "/items/",
+        description="api for creating an item",
+        response_description="respond if creation is successful.",
+        response_model=ResponseModel,
+    )
+    async def create_item(item: Item):
+        """
+        Create a new item.
+        ## Parameters
+        - **item**: Item object to create.
+        ## Returns
+        The created item.
+        ## Errors
+        - **400 Bad Request**: Invalid request data.
+        """
+        if item.name == "foo":
+            raise HTTPException(status_code=400, detail="Item name cannot be foo.")
+        inventory.append(item)
+        return ResponseModel(ans="1", ans_1="2")
+
+
+    # how to generate doc?
+    # visit: http://<host_ip>:9981/docs
+    #
+    # how to export doc?
+    #
+    # By default, the OpenAPI schema is served at /openapi.json
+    # this json file is needed to create project in apifox.
+    # what is that json anyway?
+
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=port)
