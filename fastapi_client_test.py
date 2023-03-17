b560d2e56d8370b8ea28c2d79f7b4d8b08778fda
YuPan <example@example.com>
2023-03-17 16:54:02 +0800

    'update'

--- fastapi_client_test.py
+++ fastapi_client_test.py
@@ -4,9 +4,5 @@ import requests
 baseurl = "http://127.0.0.1:" + port + "/"
 
 url = baseurl + "items"
-json_data = dict(
-        name="hello"
-        price=1
-        is_offer=False,
-        myDict={'mydict':1})
-r = requests.post(url,json=json_data)
+json_data = dict(name="hello", price=1, is_offer=False, myDict={"mydict": 1})
+r = requests.post(url, json=json_data)
