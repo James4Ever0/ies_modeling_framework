beb975ed2fceb814e5bc4a892c1f429189e98d65
YuPan <example@example.com>
2023-03-20 11:59:02 +0800

    'update'

---
+++ cloudpss_sdk_demo.py
@@ -0,0 +1,10 @@
+SDK_TOKEN='eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NjQ5NiwidXNlcm5hbWUiOiJTdGV2ZW4wMTI4Iiwic2NvcGVzIjpbXSwidHlwZSI6ImFwcGx5IiwiZXhwIjoxNzEwMzg4NjQ3LCJpYXQiOjE2NzkyODQ2NDd9.bn-opCnMyy7Fgj8oIKPdeyaUTvcTxNLgdmC4TUv-MEigA6xiStlAJrsdOOpUDTB_ccH1ERIs292gUjLaXnLt9ZJ4ncrCeq0Pf_nTsSHWYXwDXjZU4xhJ66zk917N0Cr-cEiiLU-iHBZhmovhg6RHnfGKMoToALWqszHbMXDGlcQ'
+
+
+"""
+
+# 
+
+"""
+
+import cloudpss
