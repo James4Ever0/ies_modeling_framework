faccf13e24af4e24b9042f933699568e8211f42b
YuPan <example@example.com>
2023-03-20 15:04:03 +0800

    'update'

--- fastapi_client_generated_test.py
+++ fastapi_client_generated_test.py
@@ -12,12 +12,21 @@ sys.path.append(client_location)
 
 # where's the path?
 
-import chimichang_app_client  as CA
-import chimichang_app_client.api.default as DEFAULT
+import chimichang_app_client as CA
+import chimichang_app_client.api.default.post_item_api_name_items_post as POST
 import chimichang_app_client.api.users as USERS
 import chimichang_app_client.models as M
 
+client = CA.Client(base_url=f"http://localhost:{{port}}", verify_ssl=False, raise_on_unexpected_status=True, timeout=5)
+
+someRandomDict=M.ItemMydict.from_dict(dict(a = 1))# strange it is
+myItem = M.Item(name='myName', price=20,is_offer=True, my_dict = someRandomDict)
+
+data = POST.sync(client=client,json_body=myItem)
+
 # breakpoint()
+print("RESPONSE:", data)
+
 """
 import os
 filepath = os.path.join(client_location,os.path.basename(__file__))
