38000f2aacf9f7d591d57152839877c2d3ced427
YuPan <example@example.com>
2023-03-20 15:49:02 +0800

    'update'

--- chimichang-app-client/fastapi_client_generated_test.py
+++ chimichang-app-client/fastapi_client_generated_test.py
@@ -9,7 +9,7 @@ sys.path.append(client_location)
 # where's the path?
 
 import chimichang_app_client as CA
-import chimichang_app_client.api.default.post_item_api_name_items_post as POST
+from chimichang_app_client.api.default import *
 import chimichang_app_client.api.users as USERS
 import chimichang_app_client.models as M
 
