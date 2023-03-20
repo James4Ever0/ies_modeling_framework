9356842dfe77b461e02cea6810178e6536874cac
YuPan <example@example.com>
2023-03-20 15:34:02 +0800

    'update'

--- fastapi_typescript_client.ts
+++ fastapi_typescript_client.ts
@@ -1,3 +1,7 @@
 import {readItemsItemsGet, postItemApiNameItemsPost} from "./typescript_fastapi_client";
 
-// var read_req = readItemsItemsGet({}).then((response:Response, data:Data) =>{console.log(response, data)}) // again, callback hell!
+// why there is no typehint?
+
+var read_req = readItemsItemsGet({})// again, callback hell!
+
+var var_post_req = postItemApiNameItemsPost({})
