4d5fc187d14e2246fc6fe262cdeaf0c950f76ef0
YuPan <example@example.com>
2023-03-20 15:19:03 +0800

    'update'

--- fastapi_typescript_client.ts
+++ fastapi_typescript_client.ts
@@ -1,1 +1,3 @@
-import {} from "./typescript_fastapi_client";
+import {readItemsItemsGet, postItemApiNameItemsPost} from "./typescript_fastapi_client";
+
+// var read_req = readItemsItemsGet({}).then((response:Response, data:Data) =>{console.log(response, data)}) // again, callback hell!
