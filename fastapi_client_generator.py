30cd4102a8e8f3c0159f72ee4bd21d9b67873c7e
YuPan <example@example.com>
2023-03-20 15:39:02 +0800

    'update'

--- fastapi_client_generator.py
+++ fastapi_client_generator.py
@@ -4,10 +4,12 @@ target_file = "fastapi_client_generated.py"
 
 import os
 
-# suggest you to clean this openapi json first:
+# suggest you to clean this openapi json (the function name) first:
 # https://fastapi.tiangolo.com/advanced/generate-clients/
 #
 json_file_path = 'fastapi_openapi.json'
+
+# also try to automatically generate appropriate `__init__.py` file so we can have multiple api callers in the same namespace.
 
 cmd = f'openapi-python-client generate --path {json_file_path}' # generate to: chimichang-app-client, with chimichang_app_client wrapped inside.
 
