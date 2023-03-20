c9e99ed99c7f59cbad419fb870037e23e1e55504
YuPan <example@example.com>
2023-03-20 16:24:02 +0800

    'update'

--- cloudpss_extract_component_ports.py
+++ cloudpss_extract_component_ports.py
@@ -2,5 +2,7 @@ source = "cloudpss_component_ports.json"
 import json
 from bs4 import BeautifulSoup
 
-
-print("")
+keys = ['母线', '燃气内燃机', '负荷',]
+values = ['', '<text str=\"电接口\" x=\"15.983333333333334\" y=\"28.52569986979166\" align=\"center\" valign=\"middle\" vertical=\"0\" rotation=\"0\" localized=\"0\" align-shape=\"0\" />', '<text str=\"热水接口\" x=\"24.6356416004801\" y=\"44.88333333333334\" align=\"center\" valign=\"middle\" vertical=\"0\" rotation=\"0\" localized=\"0\" align-shape=\"0\" />',]
+my_dict = dict(zip(keys, values))
+print(my_dict)
