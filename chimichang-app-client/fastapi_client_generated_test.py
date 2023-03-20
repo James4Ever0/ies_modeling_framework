94c8119e253dad495220ebcd2b6f67036d37f145
YuPan <example@example.com>
2023-03-20 14:44:02 +0800

    'update'

---
+++ chimichang-app-client/fastapi_client_generated_test.py
@@ -0,0 +1,16 @@
+
+port = 9982
+
+import sys
+client_location ="D:/project/xianxing/chimichang-app-client"
+
+sys.path.append(client_location)
+
+# where's the path?
+
+import chimichang_app_client  as CA
+import chimichang_app_client.api.default as DEFAULT
+import chimichang_app_client.api.users as USERS
+import chimichang_app_client.models as M
+
+breakpoint()
--- fastapi_client_generated_test.py
+++ fastapi_client_generated_test.py
@@ -1,10 +1,12 @@
 
 from fastapi_server_with_doc import port # 9982?
+client_location = 'D:/project/xianxing/chimichang-app-client'
+
 CODE = f"""
 port = {port}
 
 import sys
-client_location ="D:\\project\\xianxing\\chimichang-app-client"
+client_location ="{client_location}"
 
 sys.path.append(client_location)
 
@@ -15,5 +17,14 @@ import chimichang_app_client.api.default as DEFAULT
 import chimichang_app_client.api.users as USERS
 import chimichang_app_client.models as M
 
-breakpoint()
+# breakpoint()
 """
+import os
+filepath = os.path.join(client_location,os.path.basename(__file__))
+
+with open(filepath,'w+',encoding='utf-8') as f:
+    f.write(CODE)
+
+import subprocess
+
+subprocess.run(['python', filepath],cwd=client_location)
