e4648cf2236a082c4294caa6fc0b4786031c6f15
YuPan <example@example.com>
2023-03-20 14:59:12 +0800

    'update'

--- chimichang-app-client/fastapi_client_generated_test.py
+++ chimichang-app-client/fastapi_client_generated_test.py
@@ -15,7 +15,7 @@ import chimichang_app_client.models as M
 
 client = CA.Client(base_url=f"http://localhost:{port}", verify_ssl=False, raise_on_unexpected_status=True, timeout=5)
 
-someRandomDict=M.ItemMydict(a = 1)# strange it is
+someRandomDict=M.ItemMydict.from_dict(dict(a = 1))# strange it is
 myItem = M.Item(name='myName', price=20,is_offer=True, my_dict = someRandomDict)
 
 data = POST.sync(client=client,json_body=myItem)
--- fastapi_client_generator.py
+++ fastapi_client_generator.py
@@ -4,6 +4,9 @@ target_file = "fastapi_client_generated.py"
 
 import os
 
+# suggest you to clean this openapi json first:
+# https://fastapi.tiangolo.com/advanced/generate-clients/
+#
 json_file_path = 'fastapi_openapi.json'
 
 cmd = f'openapi-python-client generate --path {json_file_path}' # generate to: chimichang-app-client, with chimichang_app_client wrapped inside.
