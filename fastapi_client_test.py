a907bafd993dc3e4f2ca78155e79a78ba7924577
YuPan <example@example.com>
2023-03-20 14:09:13 +0800

    'update'

--- fastapi_client_test.py
+++ fastapi_client_test.py
@@ -4,8 +4,12 @@ import requests
 baseurl = f"http://127.0.0.1:{port}/"
 
 url = baseurl + "items"
-json_data = dict(name="hello", price=1, is_offer=False, myDict={"mydict": {'more':[1,2,3]}})
+json_data = dict(
+    name="hello", price=1, is_offer=False, myDict={"mydict": {"more": [1, 2, 3]}}
+)
 r = requests.post(url, json=json_data)
 response = r.json()
 print("RESP:", response)
 print("STATUS_CODE", r.status_code)
+
+# let's generate the client from this json.
