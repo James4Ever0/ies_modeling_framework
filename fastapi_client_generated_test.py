3b81709ab1341ff39a92c31ed28a99ed1381e2d7
YuPan <example@example.com>
2023-03-20 15:54:02 +0800

    'update'

--- fastapi_client_generated_test.py
+++ fastapi_client_generated_test.py
@@ -13,7 +13,8 @@ sys.path.append(client_location)
 # where's the path?
 
 import chimichang_app_client as CA
-import chimichang_app_client.api.default.post_item_api_name_items_post as POST
+# import chimichang_app_client.api.default.post_item_api_name_items_post as POST
+from chimichang_app_client.api.default import * # in a hurry?
 import chimichang_app_client.api.users as USERS
 import chimichang_app_client.models as M
 
