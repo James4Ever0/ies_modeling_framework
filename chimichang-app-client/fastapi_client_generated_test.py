853efad833bf2b398bd3541cb7c919575c676e7b
YuPan <example@example.com>
2023-03-20 14:54:02 +0800

    'update'

--- chimichang-app-client/fastapi_client_generated_test.py
+++ chimichang-app-client/fastapi_client_generated_test.py
@@ -1,8 +1,8 @@
-
 port = 9982
 
 import sys
-client_location ="D:/project/xianxing/chimichang-app-client"
+
+client_location = "D:/project/xianxing/chimichang-app-client"
 
 sys.path.append(client_location)
 
@@ -13,6 +13,12 @@ import chimichang_app_client.api.default.post_item_api_name_items_post as POST
 import chimichang_app_client.api.users as USERS
 import chimichang_app_client.models as M
 
-POST.
+client = CA.Client(base_url=f"http://localhost:{port}", verify_ssl=False, raise_on_unexpected_status=True, timeout=5)
 
-breakpoint()
+someRandomDict=M.ItemMydict(a = 1)# strange it is
+myItem = M.Item(name='myName', price=20,is_offer=True, my_dict = someRandomDict)
+
+data = POST.sync(client=client,json_body=myItem)
+
+# breakpoint()
+print("RESPONSE:", data)
