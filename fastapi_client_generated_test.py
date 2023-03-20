9c4d60d024d2472fc90d8b0ac95e5bbd6b21def7
YuPan <example@example.com>
2023-03-20 14:34:02 +0800

    'update'

--- fastapi_client_generated_test.py
+++ fastapi_client_generated_test.py
@@ -1,2 +1,9 @@
-import os
-os.path
+from fastapi_server_with_doc import port # 9982?
+
+import sys
+client_location ="D:\\project\\xianxing\\chimichang-app-client"
+sys.path.append(client_location)
+
+import chimichang_app_client  as CA
+
+breakpoint()
