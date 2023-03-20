6ad1a556c62efb41ef4edd52a43257420a972646
YuPan <example@example.com>
2023-03-20 16:44:02 +0800

    'update'

--- cloudpss_download_component_ports.py
+++ cloudpss_download_component_ports.py
@@ -1,2 +1,11 @@
-url_optim = lambda page: f'https://ies.cloudpss.net:8201/editor/componentheatList/?page={}'
-url_simul = lambda page: f'https://ies.cloudpss.net:8202/editor/componentheatList/?page={}'
+url_optim = lambda page: f'https://ies.cloudpss.net:8201/editor/componentheatList/?page={page}'
+url_simul = lambda page: f'https://ies.cloudpss.net:8202/editor/componentheatList/?page={page}'
+
+import requests
+
+r = requests.get(url_optim(1))
+
+
+data = r.
+import rich
+rich.print(data)
