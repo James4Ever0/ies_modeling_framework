c84836a457d391769fe256825143b6b66e62175b
YuPan <example@example.com>
2023-03-21 11:44:02 +0800

    'update'

--- cloudpss_extract_component_ports.py
+++ cloudpss_extract_component_ports.py
@@ -5,13 +5,19 @@ from bs4 import BeautifulSoup
 # keys = ['母线', '燃气内燃机', '负荷',]
 # values = ['', '<text str=\"电接口\" x=\"15.983333333333334\" y=\"28.52569986979166\" align=\"center\" valign=\"middle\" vertical=\"0\" rotation=\"0\" localized=\"0\" align-shape=\"0\" />', '<text str=\"热水接口\" x=\"24.6356416004801\" y=\"44.88333333333334\" align=\"center\" valign=\"middle\" vertical=\"0\" rotation=\"0\" localized=\"0\" align-shape=\"0\" />',]
 
-forbidden_words = ['%CompName']
+forbidden_words = [
+    "%CompName",
+    "${isNaN(V)?V:Number(V).toFixed(4)}∠${isNaN(Angle)?Angle:Number(Angle).toFixed(2)}°",
+    "%VBase",
+    "1.0000∠0.00°",
+    "115.0 [kV]",
+]
 
 content = open(source, "r", encoding="utf-8").read()
 data = json.loads(content)
 for element in data["cmp"]:
     value = element["typename"]
-    print(value)
+    print("DEVICE:", value)
     print()
     shape = element["shape"]
     xml_shape = BeautifulSoup(shape, features="xml")
