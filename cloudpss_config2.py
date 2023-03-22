2c3bc8be9e7604f9ebf970dce13287ba9fa34456
YuPan <example@example.com>
2023-03-22 11:04:03 +0800

    'update'

--- cloudpss_config2.py
+++ cloudpss_config2.py
@@ -1,7 +1,11 @@
 sources = ["cloudpss_optim_config2.json", "cloudpss_simulation_config2.json"]
 
+# must place all components on the graph to get the data.
+
 import json
 
 for source in sources:
     with open(source, "r", encoding="utf-8") as f:
         data = json.loads(f.read())
+
+    print(data)
