3d89729c53326aa7f6e16b41f43a05c3a6c2003b
YuPan <example@example.com>
2023-03-22 13:49:02 +0800

    'update'

--- cloudpss_config2.py
+++ cloudpss_config2.py
@@ -26,7 +26,6 @@ for source in sources:
             existing_keys.append(key_prefix)
             print(key_prefix)
             
-            component_info = []
             # val_prefix=val
             # if val_prefix not in ['defaultApp']+existing_vals:
             #     existing_vals.append(val_prefix)
@@ -35,14 +34,23 @@ for source in sources:
             # shall create this table for every device.
             params = val['param']
             input_types = list(params.keys())
+            for input_type in input_types:
+                component_info = []
+                input_data = params[input_type]
+                for k,v in input_data['params'].items():
+                    valDict ={"ID": k}
+                    # valDict ={"InputType": input_type,"ID": k}
+                    valDict.update({k0:v0 for k0,v0 in v.items()})
+                    component_info.append(valDict)
+                    
+                df = pd.DataFrame(component_info)
+                print(f"Data Input {input_type} in {key_prefix}:")
+                # print(df.to_string(index=False))
+                markdown_table = df.to_markdown(index=False)
+                print(markdown_table)
             
             # component_info.append({"ID": key, "Name":val.get("name"),"Type": val.get("type"), "Thutype": val.get("thutype")})
     
-            df = pd.DataFrame(component_info)
-            print(f"Data Input {input_type} in {key_prefix}:")
-            # print(df.to_string(index=False))
-            markdown_table = df.to_markdown(index=False)
-            print(markdown_table)
 
         
 
