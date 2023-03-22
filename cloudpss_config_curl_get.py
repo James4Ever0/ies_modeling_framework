92199f0cc3de12cd4fb1b311681647b33b84fb49
YuPan <example@example.com>
2023-03-22 15:09:04 +0800

    'update'

--- cloudpss_config_curl_get.py
+++ cloudpss_config_curl_get.py
@@ -5,7 +5,6 @@ choice = "simu"
 
 mjson_path = sources_curl_get[choice]
 
-
 import json
 import pandas as pd
 
@@ -15,6 +14,7 @@ headliner = lambda level: "#" * level
 with open(mjson_path, "r", encoding="utf-8") as f:
     lines = f.readlines()
 
+level_shift = 0
 
 existing_keys = []
 for line in lines:
@@ -23,21 +23,23 @@ for line in lines:
         param = data["ele"]["param"]
         key_prefix = name = param["name"]
 
-        pin = [v for _, v in param["pin"].items()]  # iterate through keys.
-        pin_df = pd.DataFrame(pin)
-        print(headliner(3), "针脚定义")
-        print()
-        print(pin_df.to_markdown())
-        # you can also get conditional pins and connection types.
-        existing_keys = []
-
-        print()
-        print(headliner(3), "参数填写")
-
         if key_prefix not in existing_keys:
             existing_keys.append(key_prefix)
-            print(key_prefix)
 
+            print()
+            print(headliner(level_shift + 2), key_prefix)
+            print()
+
+            pin = [v for _, v in param["pin"].items()]  # iterate through keys.
+            pin_df = pd.DataFrame(pin)
+            print(headliner(level_shift + 3), "针脚定义")
+            print()
+            print(pin_df.to_markdown())
+            # you can also get conditional pins and connection types.
+            existing_keys = []
+
+            print()
+            print(headliner(level_shift + 3), "参数填写")
             # shall create this table for every device.
             params = param["param"]
             input_types = list(params.keys())
@@ -50,10 +52,12 @@ for line in lines:
                     component_info.append(valDict)
 
                 df = pd.DataFrame(component_info)
-                print(headliner(4), input_type)
+                print()
+                print(headliner(level_shift + 4), input_type)
+                print()
                 markdown_table = df.to_markdown(index=False)
                 print(markdown_table)
-
+        print()
     except:
         # obviously we've hit something hard.
         continue
