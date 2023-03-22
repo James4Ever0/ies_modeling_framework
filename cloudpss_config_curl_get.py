4b73d10cd2f59fadde4266c3a45656ec460db27e
YuPan <example@example.com>
2023-03-22 15:49:04 +0800

    'update'

--- cloudpss_config_curl_get.py
+++ cloudpss_config_curl_get.py
@@ -1,6 +1,7 @@
 sources_curl_get = dict(optim="cloudpss_optim.mjson", simu="cloudpss_simu.mjson")
 # almost the same as `cloudpss_config2.py`, with slight alternation.
 choice = "optim"
+choices = sources_curl_get.keys()
 
 param_translate_maps = dict(
     optim=dict(
@@ -10,6 +11,38 @@ param_translate_maps = dict(
     ),
     simu=dict(参数分类=[], 中文名称=[], 有关设备=[]),
 )
+prelude = """
+# 建模仿真和规划设计的输入参数和区别
+
+规划设计在设备信息库内添加了经济性参数，而建模仿真对某些设备将额定工况变为了多工况的输入。
+下面介绍在能流拓扑图中两种模式的输入项区别：
+"""
+
+建模仿真参数
+参数分类
+
+基础参数
+要指定设备台数
+仿真参数
+配电传输设备除模块化多电平变流器都不具备仿真参数，及管道、采暖制冷负荷、电负荷都不具备
+优化参数
+具备优化参数的设备可选是否优化，部分设备优化参数具有其他参数，例如柔性电负荷的最大负荷
+运行约束
+采暖制冷负荷具备运行约束，供热\制冷最大、最小出口温度。
+详细说明
+
+规划设计参数
+
+机组参数
+在没有选择具体设备时，不能指定设备台数，但可以指定设备额定运行参数。指定了设备类型时，可以指定设备台数，但是不能指定额定运行参数。
+部分参数
+运行参数组
+不能指定部分参数，或者可选指定部分参数
+计算参数组
+有的设备没有计算参数组，例如吸收式制冷机，余热锅炉
+负荷设置
+负荷元件特有的设置
+
 
 
 mjson_path = sources_curl_get[choice]
