dca63b31bea9f9785af2f1db533544c3f4c6dc9e
YuPan <example@example.com>
2023-03-22 11:34:02 +0800

    'update'

--- cloudpss_config2.py
+++ cloudpss_config2.py
@@ -20,15 +20,9 @@ for source in sources:
         if key_prefix not in ['defaultApp']+existing_keys:
             existing_keys.append(key_prefix)
             print(key_prefix)
-grouped_components = {}
-for key_prefix in existing_keys:
-    group_name = "_".join(key_prefix)
-    group_components = {}
-    for key, val in components.items():
-        if key.startswith(group_name):
-            group_components[key] = val
-    grouped_components[group_name] = group_components
-print(grouped_components)
+
+        val_prefix=val.
+
 # with open('cloudpss_file.json', 'w+') as f3:
 #     json.dump(data, f3)
 
--- curl_component_get.py
+++ curl_component_get.py
@@ -4,8 +4,12 @@ template_paths = dict(
 
 import jinja2
 from jinja2 import StrictUndefined
+import subprocess
+
+access_paths = ['CPS','Heat']
 
 for template_path in template_paths:
     with open(template_path,'r', encoding='utf-8') as f:
         source = f.read()
         template = jinja2.Template(source = source, undefined=StrictUndefined)
+        template.render(access_path=access_path, id=_id)
