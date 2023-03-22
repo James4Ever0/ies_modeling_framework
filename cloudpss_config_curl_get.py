8c56c565e6b8e06bf750c33d9166db88b9f2f5e1
YuPan <example@example.com>
2023-03-22 14:24:04 +0800

    'update'

--- cloudpss_config_curl_get.py
+++ cloudpss_config_curl_get.py
@@ -22,39 +22,39 @@ for line in lines:
         # obviously we've hit something hard.
         break
 
+headliner = lambda level: "#" * level
+
 param = data["ele"]['param']
 name = param['name']
 pin = [v for _,v in param['pin'].items()] # iterate through keys.
+pin_df = pd.DataFrame(pin)
+print(headliner(3),"针脚定义")
+print()
+print(pin_df.to_markdown())
 # you can also get conditional pins and connection types.
 existing_keys = []
 
-headliner = lambda level: "#" * level
+print()
+print(headliner(3),"参数填写")
 
-    key_prefix = param['name']
-    if key_prefix not in ["defaultApp"] + existing_keys:
-        existing_keys.append(key_prefix)
-        print(key_prefix)
+key_prefix = param['name']
+if key_prefix not in ["defaultApp"] + existing_keys:
+    existing_keys.append(key_prefix)
+    print(key_prefix)
 
-        # shall create this table for every device.
-        params = val["param"]
-        input_types = list(params.keys())
-        for input_type in input_types:
-            component_info = []
-            input_data = params[input_type]
-            for k, v in input_data["params"].items():
-                valDict = {"ID": k}
-                # valDict ={"InputType": input_type,"ID": k}
-                valDict.update({k0: v0 for k0, v0 in v.items()})
-                component_info.append(valDict)
+    # shall create this table for every device.
+    params = param["param"]
+    input_types = list(params.keys())
+    for input_type in input_types:
+        component_info = []
+        input_data = params[input_type]
+        for k, v in input_data["params"].items():
+            valDict = {"ID": k}
+            # valDict ={"InputType": input_type,"ID": k}
+            valDict.update({k0: v0 for k0, v0 in v.items()})
+            component_info.append(valDict)
 
-            df = pd.DataFrame(component_info)
-            print(f"Data Input {input_type} in {key_prefix}:")
-            # print(df.to_string(index=False))
-            markdown_table = df.to_markdown(index=False)
-            print(markdown_table)
-
-        # component_info.append({"ID": key, "Name":val.get("name"),"Type": val.get("type"), "Thutype": val.get("thutype")})
-
-
-# with open('cloudpss_file.json', 'w+') as f3:
-#     json.dump(data, f3)
+        df = pd.DataFrame(component_info)
+        print(headliner(4), input_type)
+        markdown_table = df.to_markdown(index=False)
+        print(markdown_table)
