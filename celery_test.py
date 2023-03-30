dee86f72e4442f90ccd59e98b4b9331ea1a5a9ed
jamesbrown <randomvoidmail@foxmail.com>
2023-03-30 12:18:38 +0800

    [2023-03-30 04:18:38]
    FILE:
    celery_test.py
    
    DIR:
    fastapi-celery
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /Users/jamesbrown/Desktop/works/jubilant-adventure/fastapi-celery
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- celery_test.py
+++ celery_test.py
@@ -46,6 +46,7 @@ class AddResultNested(BaseModel):
 def add(x, y):
     # with filelock.FileLock(LOCK_FILE): # this lock is not sharing.
     # with lock:
+    # why not working?
         print("CALCULATING:", x, y)
         # but we plan to do this for 10 seconds.
         import time
