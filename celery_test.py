34a26b9ce0bf3c99005041814f6cd7a0c2afe827
jamesbrown <randomvoidmail@foxmail.com>
2023-03-30 13:02:31 +0800

    [2023-03-30 05:02:31]
    FILE:
    celery_test.py
    
    DIR:
    fastapi-celery
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /Users/jamesbrown/Desktop/works/jubilant-adventure/fastapi-celery
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- celery_test.py
+++ celery_test.py
@@ -60,7 +60,7 @@ def add(x, y):
     # why not working?
     # with lock:
     # still the same?
-    with RedisLock(redis_instance, name="task_id"):
+    with RedisLock(redis_instance, name="task_id"): # no one will have the lock.
         print("CALCULATING:", x, y)
         # but we plan to do this for 10 seconds.
         import time
