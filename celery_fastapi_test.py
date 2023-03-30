a1daa5d52e00156d8f07d808c4b0b8601d1fff3f
jamesbrown <randomvoidmail@foxmail.com>
2023-03-30 12:11:48 +0800

    [2023-03-30 04:11:48]
    FILE:
    celery_fastapi_test.py
    
    DIR:
    fastapi-celery
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /Users/jamesbrown/Desktop/works/jubilant-adventure/fastapi-celery
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- celery_fastapi_test.py
+++ celery_fastapi_test.py
@@ -79,6 +79,7 @@ def add_get(a, b, background_task: BackgroundTasks):
     args = [a, b]
     # print("")
     # never registered. use name instead?
+    from celery_test import MAIN_NAME
     task_name = "celery_.add"
     task: AsyncResult = celery_app.send_task(task_name, args=args)
     # task:AsyncResult = add.apply_async(args=args)
