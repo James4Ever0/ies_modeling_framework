b347361e93e1542ee18a81e98363e86bd1ba7f57
jamesbrown <randomvoidmail@foxmail.com>
2023-03-30 11:11:43 +0800

    [2023-03-30 03:11:43]
    FILE:
    fastapi_server_template.py
    
    DIR:
    fastapi-celery
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /Users/jamesbrown/Desktop/works/jubilant-adventure/fastapi-celery
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- fastapi_server_template.py
+++ fastapi_server_template.py
@@ -113,7 +113,7 @@ def calculate_async(graph: EnergyFlowGraph) -> CalculationAsyncSubmitResult:
         calculation_id=calculation_id, submit_result=submit_result
     )
 
-@app.get("/get_calculation_state", )
+@app.get("/get_calculation_state", response_description="",summary="", description="")
 def get_calculation_state(calculation_id: str) -> Union[None, str]:
     """
     根据计算ID获取计算状态
