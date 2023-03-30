741bd819fa092e7eba611613d87b9cd0d9eb679f
jamesbrown <randomvoidmail@foxmail.com>
2023-03-30 14:40:05 +0800

    [2023-03-30 06:40:05]
    FILE:
    fastapi_server_template.py
    
    DIR:
    fastapi-celery
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /Users/jamesbrown/Desktop/works/jubilant-adventure/fastapi-celery
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- fastapi_server_template.py
+++ fastapi_server_template.py
@@ -109,7 +109,7 @@ def background_on_message(task: AsyncResult):
     后台获取任务计算结果的方法
     
     Args:
-        task (AsyncResult): 
+        task (AsyncResult): 任务对象
     """
     value = task.get(on_message=celery_on_message, propagate=False)
     # shall you not check here.
