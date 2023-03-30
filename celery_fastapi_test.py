f4a663b52289544e883c0dd3d996ffb597c4a74e
jamesbrown <randomvoidmail@foxmail.com>
2023-03-30 14:26:57 +0800

    [2023-03-30 06:26:57]
    FILE:
    celery_fastapi_test.py
    
    DIR:
    fastapi-celery
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /Users/jamesbrown/Desktop/works/jubilant-adventure/fastapi-celery
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- celery_fastapi_test.py
+++ celery_fastapi_test.py
@@ -124,7 +124,7 @@ def revoke_task(task_id: str):
     return "REVOKED"
 
 
-@app.get("/task_result")
+@app.get("/task_result", )
 def get_task_result(task_id: str):
     task_result = taskResult.get(task_id, None)
     task_status = "MISSING"
