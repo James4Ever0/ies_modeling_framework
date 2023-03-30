2cf801649016ff10e2ead04b030a7ff6e6804bf4
jamesbrown <randomvoidmail@foxmail.com>
2023-03-30 14:38:18 +0800

    [2023-03-30 06:38:18]
    FILE:
    fastapi_celery_client_test.py
    
    DIR:
    fastapi-celery
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /Users/jamesbrown/Desktop/works/jubilant-adventure/fastapi-celery
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- fastapi_celery_client_test.py
+++ fastapi_celery_client_test.py
@@ -1,1 +1,52 @@
 import requests
+
+LOOP_COUNT = 20
+
+base_url = "http://localhost:8010"
+url = f"{base_url}/add/2/1"
+
+r = requests.get(url)
+data = r.json()
+
+print("DATA", data)
+task_id = data["task_id"]
+
+r2 = requests.get(f'{base_url}/add/1/2')
+data2 = r2.json()
+task_id2 = data2['task_id']
+
+url_new = f"{base_url}/task_status"
+
+import time
+
+# import celery.states as S
+# PENDING, RECEIVED, STARTED, SUCCESS, FAILURE, RETRY, REVOKED,
+
+# how to limit the task threads?
+
+for _ in range(LOOP_COUNT):
+    r2 = requests.get(url_new, params=dict(task_id=task_id))
+    data2 = r2.json()
+    print("DATA2", data2)  # STARTED.
+    # till: SUCCESS.
+    r3 = requests.get(url_new, params=dict(task_id=task_id))
+    data3 = r3.json()
+    print("DATA3", data3)
+    # how many status indicators can it have?
+    time.sleep(1)
+
+url_revoke = f"{base_url}/revoke"
+
+r3 = requests.get(url_revoke, params=dict(task_id=task_id))
+data3 = r3.json()
+print("REVOKE RESULT?", data3)
+
+r3 = requests.get(url_revoke, params=dict(task_id=task_id))
+data3 = r3.json()
+print("REVOKE RESULT?", data3)
+time.sleep(1)
+r2 = requests.get(url_new, params=dict(task_id=task_id))
+data2 = r2.json()
+print(
+    "DATA2", data2
+)  # SUCCESS if revoke after SUCCESS. but if revoke before SUCCESS it is REVOKED.
