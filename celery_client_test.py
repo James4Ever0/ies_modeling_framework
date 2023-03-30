0ab6616fcdfc8e0e627797d7cd48d5051899b9e4
jamesbrown <randomvoidmail@foxmail.com>
2023-03-30 11:52:45 +0800

    [2023-03-30 03:52:45]
    FILE:
    celery_client_test.py
    
    DIR:
    fastapi-celery
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /Users/jamesbrown/Desktop/works/jubilant-adventure/fastapi-celery
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- celery_client_test.py
+++ celery_client_test.py
@@ -29,6 +29,7 @@ for _ in range(LOOP_COUNT):
     data2 = r2.json()
     print("DATA2", data2)  # STARTED.
     # till: SUCCESS.
+    r3 = requests.get(url_new, )
     # how many status indicators can it have?
     time.sleep(1)
 
