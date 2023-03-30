31f23b2cd5be9561442960b95b83ec0a434c178b
jamesbrown <randomvoidmail@foxmail.com>
2023-03-30 13:01:42 +0800

    [2023-03-30 05:01:41]
    FILE:
    
    DIR:
    fastapi-celery
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /Users/jamesbrown/Desktop/works/jubilant-adventure/fastapi-celery
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- celery_test.py
+++ celery_test.py
@@ -49,7 +49,7 @@ from redis import Redis
 from redis.lock import Lock as RedisLock
 
 redis_instance = Redis.from_url(redis_url)
-lock = RedisLock(redis_instance, name="task_id")
+
 
 REDIS_TASK_KEY = "current_task"
 
