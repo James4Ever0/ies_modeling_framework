b74efcf5d45dbf2465dc9e1b05c60e5748f3a15f
jamesbrown <randomvoidmail@foxmail.com>
2023-03-30 17:39:38 +0800

    [2023-03-30 09:39:38]
    FILE:
    fastapi_celery_server.py
    
    DIR:
    fastapi-celery
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /Users/jamesbrown/Desktop/works/jubilant-adventure/fastapi-celery
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- fastapi_celery_server.py
+++ fastapi_celery_server.py
@@ -30,7 +30,7 @@ app.conf.update(worker_send_task_events=True)
 app.conf.update(worker_concurrency=1)
 memory_limit = 20_000_000 #kB
 app.conf.update(worker_max_memory_per_child=memory_limit)
-
+time_limit = # sec
 app.conf.update(worker_time_limit = time_limit)
 # limits on ram usage, concurrency, execution time
 
