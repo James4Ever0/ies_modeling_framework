419e5f3c59f882a761b30b677dce410c563fb89e
jamesbrown <randomvoidmail@foxmail.com>
2023-03-31 10:23:23 +0800

    [2023-03-31 02:23:23]
    FILE:
    fastapi_celery_client_test.py
    
    DIR:
    fastapi-celery
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /Users/jamesbrown/Desktop/works/jubilant-adventure/fastapi-celery
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- fastapi_celery_client_test.py
+++ fastapi_celery_client_test.py
@@ -5,7 +5,6 @@ LOOP_COUNT = 20
 
 base_url = "http://localhost:9870"
 url = f"{base_url}/calculate_async"
-import json
 with open("test_graph_data.json",'r') as f:
     test_graph = json.loads(f.read())
 r = requests.post(url, json=test_graph)
