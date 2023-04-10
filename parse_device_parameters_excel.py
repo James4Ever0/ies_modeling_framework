5b507ec2d68df9e74d534787f3724266a8f1e6da
jamesbrown <randomvoidmail@foxmail.com>
2023-04-10 11:47:51 +0800

    [2023-04-10 03:47:51]
    FILE:
    parse_device_parameters_excel.py
    
    DIR:
    fastapi-celery
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /Users/jamesbrown/Desktop/works/jubilant-adventure/fastapi-celery
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- parse_device_parameters_excel.py
+++ parse_device_parameters_excel.py
@@ -3,4 +3,8 @@ import openpyxl
 filepath = "device_parameters_v3.3.xlsx"
 
 excel_file =openpyxl.load_workbook(filename=filepath)
-print(excel_file.chartsheets)
+# print(excel_file.sheetnames) # ['Sheet1']
+
+sheet1 = excel_file['Sheet1']
+
+print(sheet1)
