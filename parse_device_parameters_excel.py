04c916674526a5354077809bd29c9e04458f4055
jamesbrown <randomvoidmail@foxmail.com>
2023-04-10 12:48:10 +0800

    [2023-04-10 04:48:10]
    FILE:
    parse_device_parameters_excel.py
    
    DIR:
    fastapi-celery
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /Users/jamesbrown/Desktop/works/jubilant-adventure/fastapi-celery
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- parse_device_parameters_excel.py
+++ parse_device_parameters_excel.py
@@ -5,6 +5,7 @@ filepath = "device_parameters_v3.3.xlsx"
 excel_file =openpyxl.load_workbook(filename=filepath)
 # print(excel_file.sheetnames) # ['Sheet1']
 from openpyxl.worksheet.worksheet import Worksheet
+from openpyxl.cell.cell import Cell
 sheet1= excel_file['Sheet1']
 if type(sheet1) == Worksheet:
     # print(sheet1)
@@ -15,9 +16,9 @@ if type(sheet1) == Worksheet:
 
     cell1 = sheet1.cell(row=1, column=1) # cell or merged cell.
     # need to determine its type.
-    # if type(cell1) == 
-    print(type(cell1))
-    breakpoint()
+    if type(cell1) == Cell: 
+    # print(type(cell1))
+    # breakpoint()
 
     cell1_fill = cell1.fill
     cell1_value = cell1.value
