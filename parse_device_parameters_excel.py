34dd2cbc3b2ee4b6a3196d7deaa3ba6f33abb1ae
jamesbrown <randomvoidmail@foxmail.com>
2023-04-10 12:02:09 +0800

    [2023-04-10 04:02:09]
    FILE:
    parse_device_parameters_excel.py
    
    DIR:
    fastapi-celery
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /Users/jamesbrown/Desktop/works/jubilant-adventure/fastapi-celery
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- parse_device_parameters_excel.py
+++ parse_device_parameters_excel.py
@@ -11,7 +11,7 @@ print(sheet1)
 print(dir(sheet1))
 breakpoint()
 
-cell1 = sheet1.cell(row=1, column=1)
+cell1 = sheet1.cell(row=1, column=1) # cell or merged cell.
 
 cell1_fill = cell1.fill
 cell1_value = cell1.value
