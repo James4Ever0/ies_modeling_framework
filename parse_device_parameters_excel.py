e2721c2962671dc8b178ec815941ebdc5b711e8d
jamesbrown <randomvoidmail@foxmail.com>
2023-04-10 12:50:08 +0800

    [2023-04-10 04:50:08]
    FILE:
    parse_device_parameters_excel.py
    
    DIR:
    fastapi-celery
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    /Users/jamesbrown/Desktop/works/jubilant-adventure/fastapi-celery
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- parse_device_parameters_excel.py
+++ parse_device_parameters_excel.py
@@ -2,11 +2,12 @@ import openpyxl
 
 filepath = "device_parameters_v3.3.xlsx"
 
-excel_file =openpyxl.load_workbook(filename=filepath)
+excel_file = openpyxl.load_workbook(filename=filepath)
 # print(excel_file.sheetnames) # ['Sheet1']
 from openpyxl.worksheet.worksheet import Worksheet
 from openpyxl.cell.cell import Cell, MergedCell
-sheet1= excel_file['Sheet1']
+
+sheet1 = excel_file["Sheet1"]
 if type(sheet1) == Worksheet:
     # print(sheet1)
     # print(type(sheet1))
@@ -14,16 +15,16 @@ if type(sheet1) == Worksheet:
     # print(dir(sheet1))
     # breakpoint()
 
-    cell1 = sheet1.cell(row=1, column=1) # cell or merged cell.
+    cell1 = sheet1.cell(row=1, column=1)  # cell or merged cell.
     # need to determine its type.
-    if type(cell1) == Cell: 
+    if type(cell1) == Cell:
         # print(type(cell1))
         # breakpoint()
         cell1_fill = cell1.fill
         cell1_value = cell1.value
         cell1_column_letter = cell1.column_letter
-        print('FILL:',cell1_fill)
-        print('VALUE:',cell1_value)
+        print("FILL:", cell1_fill)
+        print("VALUE:", cell1_value)
     elif type(cell1) == MergedCell:
         ...
     else:
