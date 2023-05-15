9c5db811cbf4bdb8ad92f395ae6e68444f58ffa4
jamesbrown <randomvoidmail@foxmail.com>
2023-05-15 15:28:00 +0800

    update

--- microgrid_base/parse_params.py
+++ microgrid_base/parse_params.py
@@ -1,11 +1,6 @@
 from lib_parse_params import main_parser
 
-worklist = [( "设备信息库各参数.xlsx"
-, "设备参数"
-, "device_params_intermediate.json"
-)]
+worklist = [("设备信息库各参数", "设备参数", "device_params_intermediate"), ("", "", "")]
 
-for (filepath,
-sheet_name,
-output_path) in worklist:
-    
+for (filepath, sheet_name, output_path) in worklist:
+    main_parser(f"{filepath}.xlsx", sheet_name, f"{output_path}.json")
