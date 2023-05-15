8d395e1386d1d77e5634231407216550bd1f66fe
jamesbrown <randomvoidmail@foxmail.com>
2023-05-15 14:42:00 +0800

    update

--- microgrid_base/parse_params.py
+++ microgrid_base/parse_params.py
@@ -7,21 +7,6 @@ excel_file = openpyxl.load_workbook(filepath)
 
 sheet1 = excel_file["设备参数"]
 
-dims = sheet1.row_dimensions, sheet1.column_dimensions
-
-uniqs = {}
-
-def getColumnRangePerRow(start, end):
-    flag = True
-    for index, row in enumerate(sheet1.rows):
-        if flag:
-            flag = False
-            continue
-        yield index, [col.value for col in row[start:end]]
-
-heads = getColumnRangePerRow(0, 1)
-
-for i, cvals in heads:
-    print(i)
-    print(cvals)
-    print()
+for row in sheet1.rows:
+    for row_col in row:
+        print(row_col.val)
