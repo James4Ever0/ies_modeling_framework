82d976616a0e017638663a6bbd58420b78094018
jamesbrown <randomvoidmail@foxmail.com>
2023-05-16 16:46:01 +0800

    update

--- microgrid_base/type_system.py
+++ microgrid_base/type_system.py
@@ -69,6 +69,6 @@ import json
 #             keys.append(k0)
             
 # rich.print(keys)
-port_df = pandas.read_csv("设备接口-离网型微电网.csv", header=None)
+port_df = pandas.read_csv("设备接口-离网型微电网.csv", header=None, error_bad_lines=False)
 
 print(port_df)
