e7182b6eafbba9ced5fd53fb3a61f6bd5655e573
jamesbrown <randomvoidmail@foxmail.com>
2023-05-16 15:18:00 +0800

    update

--- microgrid_base/parse_units_and_names.py
+++ microgrid_base/parse_units_and_names.py
@@ -37,7 +37,7 @@ BASE_TRANSLATION_TABLE_WITH_BASE_UNIT = {
     ),
     "Efficiency": ("percent", {"": ["电电转换效率"]}),
     "Power": ("kW", {"": [], "Unit-": ["组件额定功率"], "MaxUnit-": ["最大发电功率"]}),
-    "DeltaLimit": ("percent/second", {"":["发电爬坡率"]})
+    "DeltaLimit": ("percent/second", {"":[],"Power-":["发电爬坡率"]})
 }  # EnglishName: (BaseUnit, {convert_string:[ChineseName, ...], ...})
 
 
