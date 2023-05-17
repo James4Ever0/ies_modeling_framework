0774041d6ea7906a6e6b5aefd778af30cb25e6b6
jamesbrown <randomvoidmail@foxmail.com>
2023-05-17 10:00:00 +0800

    update

--- microgrid_base/type_system.py
+++ microgrid_base/type_system.py
@@ -36,18 +36,25 @@ def IO(type_base_name):
     return f"{type_base_name.strip()}输入输出"
 
 coax_triplets = [ # Input, Output, ConnectionBaseName
-    (Output("供电端"), Input("变流器"), "供电端母线"),
-    (IO("储能端"), IO("双向变流器"), "储能端母线"),
-    (Input("母线"), Output("母线"), "母线"),
-    (Input("柴油"), Output('柴油'), "柴油母线")
+    (Output("供电端"), Input("变流器"), "供电端母线", False),
+    (IO("储能端"), IO("双向变流器"), "储能端母线", True),
+    (Input("母线"), Output("母线"), "母线", False),
+    (Input("柴油"), Output('柴油'), "柴油母线", False)
 ]
+
+io_coax_triplets = []
 
 types = set()
 
 types_connectivity_matrix = {} # {frozenset([start, end]): generated_type}
 
-for _input, _output, wire_name in coax_triplets:
-    
+for i,o , wire_name, is_io in coax_triplets:
+    if is_io:
+        start = IO(o)
+        end = IO(o)
+    else:
+        start = Input(i)
+        end = Output(o)
     connectable_wire_name, unconnectable_wire_name = (
         f"可连接{wire_name}",
         f"不可连接{wire_name}",
