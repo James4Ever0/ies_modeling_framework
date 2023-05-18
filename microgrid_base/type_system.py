a8ac2f22d0417e7b27421896ad1f326f56508f74
jamesbrown <randomvoidmail@foxmail.com>
2023-05-18 11:38:00 +0800

    update

--- microgrid_base/type_system.py
+++ microgrid_base/type_system.py
@@ -255,25 +255,27 @@ import json
 # rich.print(keys)
 csv_path = "设备接口-离网型微电网.csv"
 
-lines = []
-line_sep_count_list = []
-with open(csv_path, "r") as f:
-    for line in f.readlines():
-        line_sep_count = line.count(",")
-        if line_sep_count == 0:
-            continue
-        lines.append(line)
-        line_sep_count_list.append(line_sep_count)
+from utils import fix_csv_and_return_dataframe
+port_df = fix_csv_and_
+# lines = []
+# line_sep_count_list = []
+# with open(csv_path, "r") as f:
+#     for line in f.readlines():
+#         line_sep_count = line.count(",")
+#         if line_sep_count == 0:
+#             continue
+#         lines.append(line)
+#         line_sep_count_list.append(line_sep_count)
 
-line_sep_count_max = max(line_sep_count_list)
-for index, line_sep_count in enumerate(line_sep_count_list):
-    lines[index] = lines[index].strip() + "," * (line_sep_count_max - line_sep_count)
+# line_sep_count_max = max(line_sep_count_list)
+# for index, line_sep_count in enumerate(line_sep_count_list):
+#     lines[index] = lines[index].strip() + "," * (line_sep_count_max - line_sep_count)
 
-with open(csv_path, "w+") as f:
-    for line in lines:
-        f.write(line + "\n")
+# with open(csv_path, "w+") as f:
+#     for line in lines:
+#         f.write(line + "\n")
 
-port_df = pandas.read_csv(csv_path, header=None, on_bad_lines="warn")
+# port_df = pandas.read_csv(csv_path, header=None, on_bad_lines="warn")
 
 # print(port_df)
 import numpy
--- microgrid_base/utils.py
+++ microgrid_base/utils.py
@@ -1,2 +1,24 @@
+import pandas
+
 def fix_csv_and_return_dataframe(csv_path):
+        
+    lines = []
+    line_sep_count_list = []
+    with open(csv_path, "r") as f:
+        for line in f.readlines():
+            line_sep_count = line.count(",")
+            if line_sep_count == 0:
+                continue
+            lines.append(line)
+            line_sep_count_list.append(line_sep_count)
+
+    line_sep_count_max = max(line_sep_count_list)
+    for index, line_sep_count in enumerate(line_sep_count_list):
+        lines[index] = lines[index].strip() + "," * (line_sep_count_max - line_sep_count)
+
+    with open(csv_path, "w+") as f:
+        for line in lines:
+            f.write(line + "\n")
+
+    df = pandas.read_csv(csv_path, header=None, on_bad_lines="warn")
     return df
