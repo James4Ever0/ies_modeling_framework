a5532ab3f7580989be5dadf2501fcdade1399aff
jamesbrown <randomvoidmail@foxmail.com>
2023-05-18 14:46:00 +0800

    update

--- microgrid_base/type_system.py
+++ microgrid_base/type_system.py
@@ -387,7 +387,7 @@ print_with_banner(types_connectivity_matrix_for_json, "connectivity_matrix","mic
 # print("=========[DEVICE PORT TYPES]=========")
 # print_with_banner(types,'device_port_types',"microgrid")
 # print("=========[ALL TYPES STRUCTURED]=========")
-all_types_structured = {"设备":list(types),"连接线":list(wire_types)}
+all_types_structured = {"设备":list(v)types),"连接线":list(wire_types)}
 print_with_banner(all_types_structured,"all_types_structured","microgrid")
 
 mtypes = set([e for k, v in types.items() for e in v])
