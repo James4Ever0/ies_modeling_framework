531a007bcbb589c4b08652dc755c41069a21c59c
jamesbrown <randomvoidmail@foxmail.com>
2023-05-23 14:24:01 +0800

    update

--- microgrid_base/test_topo_check.py
+++ microgrid_base/test_topo_check.py
@@ -5,7 +5,11 @@ from topo_check import ...
 ###############
 graph = 拓扑图()
 
- = 设备(graph, "光伏发电", )
+PV = 设备(graph, "光伏发电", port_definition = {"电接口":"供电端输出"})
+
+conn = 
+
+PV.ports['电接口'], 
 
 ###############
 # dump to dict
