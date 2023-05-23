83689e1030434736821314d8b3dfa29eed3a45b9
jamesbrown <randomvoidmail@foxmail.com>
2023-05-23 11:38:00 +0800

    update

--- microgrid_base/jinja_template_model_generator.py
+++ microgrid_base/jinja_template_model_generator.py
@@ -3,3 +3,5 @@
 # to generate the serialized connectivity matrix, you need structures.
 
 # the test code may not be generated.
+type_sys = 
+microgrid_v2_all_types_structured.json类型分类表 #（不包含设备名称）分类->能源->类型microgrid_v2_connectivity_matrix.json连接类型映射表 #"端点1_端点2"->生成连接类型microgrid_v2_device_port_type_mapping.json设备锚点类型表 #设备分类->设备名称->锚点名称->锚点类型
