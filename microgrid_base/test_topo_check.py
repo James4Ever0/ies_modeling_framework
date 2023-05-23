9e5efe5bb9c31d152931e027206eedcfbf340618
jamesbrown <randomvoidmail@foxmail.com>
2023-05-23 14:26:00 +0800

    update

--- microgrid_base/test_topo_check.py
+++ microgrid_base/test_topo_check.py
@@ -7,7 +7,9 @@ graph = 拓扑图()
 
 PV = 设备(graph, "光伏发电", port_definition = {"电接口":"供电端输出"})
 
-conn = 
+LOAD = 设备(graph, "电负荷", port_definition = {"电接口":"供电端输出"})
+
+conn = 连接线()
 
 PV.ports['电接口'], 
 
