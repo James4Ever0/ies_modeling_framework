e32403cd012b1994960056d36fdf437f915e6f5a
jamesbrown <randomvoidmail@foxmail.com>
2023-05-23 11:30:00 +0800

    update

--- microgrid_base/jinja_template_model_generator.py
+++ microgrid_base/jinja_template_model_generator.py
@@ -1,1 +1,5 @@
 # serialized connectivity matrix -> connectivity matrix -> verify matrix -> calculation model -> calculate
+
+# to generate the serialized connectivity matrix, you need structures.
+
+# the test code may not be generated.
