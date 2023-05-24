bffad5e61cc51201b6d5813c2aafc7dadedb64ab
jamesbrown <randomvoidmail@foxmail.com>
2023-05-24 10:50:01 +0800

    update

--- microgrid_base/test_class.py
+++ microgrid_base/test_class.py
@@ -22,7 +22,10 @@ G.add_node(1, **{"val":1, "val2":2})
 G.add_node(2, **{"val":1, "val2":2})
 G.add_node(3, **{"val":1, "val2":2})
 
+G.add_edge(1,2)
+
 for n_with_items in G.nodes.items():
     n,d = n_with_items
     print("NODE", n, type(n))
     print("DATA", d, type(d))
+    
