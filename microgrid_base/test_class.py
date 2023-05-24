82f3f8e592dac1e703e33b4e80129e4810cd7365
jamesbrown <randomvoidmail@foxmail.com>
2023-05-24 09:34:01 +0800

    update

--- microgrid_base/test_class.py
+++ microgrid_base/test_class.py
@@ -13,3 +13,14 @@ class B:
 
 a = A()
 b = a.B()
+
+import networkx as nx
+
+G = nx.Graph()
+
+G.add_node(1, **{"val":1, "val2":2})
+G.add_node(2, **{"val":1, "val2":2})
+G.add_node(3, **{"val":1, "val2":2})
+
+for n_with_items in G.nodes.items():
+    print("NODE", n, dir(n))
