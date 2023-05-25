4c9fb555d95aeeac43c9d10abcb585b7abc4e477
jamesbrown <randomvoidmail@foxmail.com>
2023-05-25 15:00:00 +0800

    update

--- microgrid_base/test_topo_check.py
+++ microgrid_base/test_topo_check.py
@@ -1,5 +1,5 @@
 from topo_check import *
-
+import rich
 ###############
 # build from code.
 ###############
@@ -12,27 +12,27 @@ DEL1 = 变流器(topo)
 DEL2 = 变压器(topo)
 LOAD = 电负荷(topo, 功率=3000)
 
-连接线(topo, "", PV1.电接口, DEL1.电输入)  # connected?
-连接线(topo, "", DEL1.电输出, LOAD.电接口)  # connected?
-连接线(topo, "", PV1.电接口, LOAD.电接口)  # connected?
+连接线(topo, "不可连接供电端母线", PV1.电接口, DEL1.电输入)
+连接线(topo, "不可连接电母线", DEL1.电输出, DEL2.电输入)
+连接线(topo, "不可连接负荷电母线",DEL2.电输出, LOAD.电接口)
 
-L1 = 母线(graph)
+# L1 = 母线(graph)
 
-# walk over all connections.
+# # walk over all connections.
 
-conn = 连接线(graph, PV.ports["电接口"], LOAD.ports["电接口"])
+# conn = 连接线(graph, PV.ports["电接口"], LOAD.ports["电接口"])
 
-conn_merge = 合并线(L0, L1)  # what do you do?
+# conn_merge = 合并线(L0, L1)  # what do you do?
 
 # shall raise error.
 
 # methods for computing.
 
-devs = graph.get_all_devices()
+devs = topo.get_all_devices()
 
 # device, ports, device_data
 
-adders = graph.get_all_adders()
+adders = topo.get_all_adders()
 
 # input, output, io
 
@@ -40,13 +40,13 @@ adders = graph.get_all_adders()
 # dump to dict
 ###############
 
-mdict = graph.to_json()
+mdict = topo.to_json()
 
 ###############
 # load from dict
 ###############
 
-graph_load = graph.from_json(mdict)  # static method
+graph_load = topo.from_json(mdict)  # static method
 
 # how to check error now?
 # all connected?
@@ -54,11 +54,11 @@ graph_load = graph.from_json(mdict)  # static method
 ## COMPUTE THIS GRAPH ##
 # use devs, adders
 
-graph_data = graph.get_graph_data()
+graph_data = topo.get_graph_data()
 
 # objective is contained in the graph data.
 # so all we need to pass to the compute function are: devs, adders, graph_data
 
-from ies_optim import compute
+# from ies_optim import compute
 
-result = compute(devs, adders, graph_data)
+# result = compute(devs, adders, graph_data)
