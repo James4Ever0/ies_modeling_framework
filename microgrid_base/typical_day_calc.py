559bf9debfb657d586597210755b3d64773de1ef
jamesbrown <randomvoidmail@foxmail.com>
2023-05-26 16:28:00 +0800

    update

--- microgrid_base/typical_day_calc.py
+++ microgrid_base/typical_day_calc.py
@@ -1,11 +1,11 @@
-load_format = {"workday": [0,1,2,3], "hoilday": [6,7]}
-load_format1 = {"workday": [2,3,4], "hoilday": [5,6]}
+load_format = {"workday": [0,1,2,3,4], "hoilday": [5,6]}
+load_format1 = {"workday": [1,2,3,4,5], "hoilday": [0,6]}
 
 lfs = [load_format, load_format1]
 
-assert day in range(7)
+# assert day in range(7)
 
 for lf in lfs:
-    wd, hd = lf['workday'], lf['hoilday']
-    assert set(wd).intersection(set(hd)) == set()
-    assert 
+    wd, hd = set(lf['workday']), set(lf['hoilday'])
+    assert wd.intersection(hd) == set()
+    assert wd.union(hd) == set(range(7))
