f2a2da9aa9ec0ca018411607d1dd42e445c37eae
jamesbrown <randomvoidmail@foxmail.com>
2023-05-26 16:42:00 +0800

    update

--- microgrid_base/typical_day_calc.py
+++ microgrid_base/typical_day_calc.py
@@ -1,3 +1,6 @@
+import numpy
+
+
 load_format = {"workday": [0,1,2,3,4], "hoilday": [5,6]}
 load_format1 = {"workday": [1,2,3,4,5], "hoilday": [0,6]}
 
@@ -12,10 +15,14 @@ lfs = [load_format, load_format1]
 # 平年的2月是28天，闰年2月是29天。
 # 4月、6月、9月、11月各是30天。
 # 1月、3月、5月、7月、8月、10月、12月各是31天。
-month_days = [0]*12
-4月、6月、9月、11月=30 
+month_days = [31]*12
+month_days[1] = 28
+month_days[4-1] = month_days[6-1] = month_days[9-1] = month_days[11-1]=30 
 
-{day_index: {"main": main_category, ""}}
+mdr = numpy.cumsum(month_days)
+print(mdr)
+
+# {day_index: {"main": main_category, ""}}
 
 # append by keys.
 # {data_key: [index, ...]}
