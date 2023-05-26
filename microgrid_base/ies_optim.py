2634268d8a9aa84379c0890076d73f81e350c8ea
jamesbrown <randomvoidmail@foxmail.com>
2023-05-26 17:51:43 +0800

    update

--- microgrid_base/ies_optim.py
+++ microgrid_base/ies_optim.py
@@ -204,13 +204,13 @@ class 风力发电信息(BaseModel):  # 发电设备
 
 class 柴油发电ID(BaseModel):
     ID: int
-    电接口: int
-    """
-    类型: 供电端输出
-    """
     燃料接口: int
     """
     类型: 柴油输入
+    """
+    电接口: int
+    """
+    类型: 供电端输出
     """
 
 
@@ -401,13 +401,13 @@ class 锂电池信息(BaseModel):  # 储能设备
 
 class 变压器ID(BaseModel):
     ID: int
-    电输出: int
-    """
-    类型: 变压器输出
-    """
     电输入: int
     """
     类型: 电母线输入
+    """
+    电输出: int
+    """
+    类型: 变压器输出
     """
 
 
@@ -635,13 +635,13 @@ class 双向变流器信息(BaseModel):  # 配电传输
 
 class 传输线ID(BaseModel):
     ID: int
-    电输出: int
-    """
-    类型: 电母线输出
-    """
     电输入: int
     """
     类型: 电母线输入
+    """
+    电输出: int
+    """
+    类型: 电母线输出
     """
 
 
@@ -849,7 +849,7 @@ class 光伏发电模型(设备模型):
 
         ##### PORT VARIABLE DEFINITION ####
 
-        self.变量列表(电接口, within=NonNegativeReals)
+        self.电接口 = self.变量列表("电接口", within=NonNegativeReals)
         """
         类型: 供电端输出
         """
@@ -944,7 +944,7 @@ class 风力发电模型(设备模型):
 
         ##### PORT VARIABLE DEFINITION ####
 
-        self.变量列表(电接口, within=NonNegativeReals)
+        self.电接口 = self.变量列表("电接口", within=NonNegativeReals)
         """
         类型: 供电端输出
         """
@@ -1039,14 +1039,14 @@ class 柴油发电模型(设备模型):
 
         ##### PORT VARIABLE DEFINITION ####
 
-        self.变量列表(电接口, within=NonNegativeReals)
-        """
-        类型: 供电端输出
-        """
-
-        self.变量列表(燃料接口, within=NegativeReals)
+        self.燃料接口 = self.变量列表("燃料接口", within=NegativeReals)
         """
         类型: 柴油输入
+        """
+
+        self.电接口 = self.变量列表("电接口", within=NonNegativeReals)
+        """
+        类型: 供电端输出
         """
 
     def constraints_register(self):
@@ -1151,7 +1151,7 @@ class 锂电池模型(设备模型):
 
         ##### PORT VARIABLE DEFINITION ####
 
-        self.变量列表(电接口, within=Reals)
+        self.电接口 = self.变量列表("电接口", within=Reals)
         """
         类型: 电储能端输入输出
         """
@@ -1228,14 +1228,14 @@ class 变压器模型(设备模型):
 
         ##### PORT VARIABLE DEFINITION ####
 
-        self.变量列表(电输出, within=NonNegativeReals)
-        """
-        类型: 变压器输出
-        """
-
-        self.变量列表(电输入, within=NegativeReals)
+        self.电输入 = self.变量列表("电输入", within=NegativeReals)
         """
         类型: 电母线输入
+        """
+
+        self.电输出 = self.变量列表("电输出", within=NonNegativeReals)
+        """
+        类型: 变压器输出
         """
 
     def constraints_register(self):
@@ -1310,12 +1310,12 @@ class 变流器模型(设备模型):
 
         ##### PORT VARIABLE DEFINITION ####
 
-        self.变量列表(电输出, within=NonNegativeReals)
+        self.电输出 = self.变量列表("电输出", within=NonNegativeReals)
         """
         类型: 电母线输出
         """
 
-        self.变量列表(电输入, within=NegativeReals)
+        self.电输入 = self.变量列表("电输入", within=NegativeReals)
         """
         类型: 变流器输入
         """
@@ -1394,12 +1394,12 @@ class 双向变流器模型(设备模型):
 
         ##### PORT VARIABLE DEFINITION ####
 
-        self.变量列表(储能端, within=Reals)
+        self.储能端 = self.变量列表("储能端", within=Reals)
         """
         类型: 双向变流器储能端输入输出
         """
 
-        self.变量列表(线路端, within=Reals)
+        self.线路端 = self.变量列表("线路端", within=Reals)
         """
         类型: 双向变流器线路端输入输出
         """
@@ -1458,14 +1458,14 @@ class 传输线模型(设备模型):
 
         ##### PORT VARIABLE DEFINITION ####
 
-        self.变量列表(电输出, within=NonNegativeReals)
-        """
-        类型: 电母线输出
-        """
-
-        self.变量列表(电输入, within=NegativeReals)
+        self.电输入 = self.变量列表("电输入", within=NegativeReals)
         """
         类型: 电母线输入
+        """
+
+        self.电输出 = self.变量列表("电输出", within=NonNegativeReals)
+        """
+        类型: 电母线输出
         """
 
     def constraints_register(self):
