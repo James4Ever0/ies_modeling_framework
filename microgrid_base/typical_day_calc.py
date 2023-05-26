2c1913bc996cad70cfdc392afc007d4bce44ac83
jamesbrown <randomvoidmail@foxmail.com>
2023-05-26 16:58:00 +0800

    update

--- microgrid_base/typical_day_calc.py
+++ microgrid_base/typical_day_calc.py
@@ -1,5 +1,5 @@
 import numpy
-import scipy # for 
+import scipy # for KNN
 
 load_format = {"workday": [0, 1, 2, 3, 4], "hoilday": [5, 6]}
 load_format1 = {"workday": [1, 2, 3, 4, 5], "hoilday": [0, 6]}
