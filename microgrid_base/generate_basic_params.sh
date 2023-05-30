c6d2b9377412297c6a76cd69b16f5ad76fcb587e
jamesbrown <randomvoidmail@foxmail.com>
2023-05-30 09:26:01 +0800

    update

--- microgrid_base/generate_basic_params.sh
+++ microgrid_base/generate_basic_params.sh
@@ -1,6 +1,14 @@
 python3 parse_params.py
-echo "exit code? $?"
-python3 parse_units_and_names.py
-echo "exit code? $?"
+# echo "exit code? $?"
+# if [ $? -ne 0 ] ; then
+# echo "nonzero exit code for: `python3 parse_params.py`"
+# exit
+# fi
 
+python3 parse_units_and_names.py
+# echo "exit code? $?"
+# if [ $? -ne 0 ] ; then
+# echo "nonzero exit code for: `python3 parse_units_and_names.py`"
+# exit
+# fi
 # then execute the template to generate code
