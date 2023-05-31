b6c62c06985f8f4bc6bfe80ab6836227f961b03f
jamesbrown <randomvoidmail@foxmail.com>
2023-05-31 11:50:00 +0800

    update

--- microgrid_base/ies_optim.py
+++ microgrid_base/ies_optim.py
@@ -218,13 +218,13 @@ class 风力发电信息(BaseModel):  # 发电设备
 
 class 柴油发电ID(BaseModel):
     ID: int
-    燃料接口: int
-    """
-    类型: 柴油输入
-    """
     电接口: int
     """
     类型: 供电端输出
+    """
+    燃料接口: int
+    """
+    类型: 柴油输入
     """
 
 
@@ -615,13 +615,13 @@ class 变流器信息(BaseModel):  # 配电传输
 
 class 双向变流器ID(BaseModel):
     ID: int
-    储能端: int
-    """
-    类型: 双向变流器储能端输入输出
-    """
     线路端: int
     """
     类型: 双向变流器线路端输入输出
+    """
+    储能端: int
+    """
+    类型: 双向变流器储能端输入输出
     """
 
 
@@ -1123,10 +1123,13 @@ class 光伏发电模型(设备模型):
 
         ##### PORT VARIABLE DEFINITION ####
 
+        self.ports = {}
+
         self.电接口 = self.变量列表("电接口", within=NonNegativeReals)
         """
         类型: 供电端输出
         """
+        self.ports["电接口"] = self.电接口
 
         # 设备特有约束（变量）
         self.电输出 = self.电接口
@@ -1306,10 +1309,13 @@ class 风力发电模型(设备模型):
 
         ##### PORT VARIABLE DEFINITION ####
 
+        self.ports = {}
+
         self.电接口 = self.变量列表("电接口", within=NonNegativeReals)
         """
         类型: 供电端输出
         """
+        self.ports["电接口"] = self.电接口
 
         # 设备特有约束（变量）
         self.电输出 = self.电接口
@@ -1501,15 +1507,19 @@ class 柴油发电模型(设备模型):
 
         ##### PORT VARIABLE DEFINITION ####
 
-        self.燃料接口 = self.变量列表("燃料接口", within=NegativeReals)
-        """
-        类型: 柴油输入
-        """
+        self.ports = {}
 
         self.电接口 = self.变量列表("电接口", within=NonNegativeReals)
         """
         类型: 供电端输出
         """
+        self.ports["电接口"] = self.电接口
+
+        self.燃料接口 = self.变量列表("燃料接口", within=NegativeReals)
+        """
+        类型: 柴油输入
+        """
+        self.ports["燃料接口"] = self.燃料接口
 
         # 设备特有约束（变量）
         self.电输出 = self.电接口
@@ -1763,10 +1773,13 @@ class 锂电池模型(设备模型):
 
         ##### PORT VARIABLE DEFINITION ####
 
+        self.ports = {}
+
         self.电接口 = self.变量列表("电接口", within=Reals)
         """
         类型: 电储能端输入输出
         """
+        self.ports["电接口"] = self.电接口
 
         # 设备特有约束（变量）
         if self.计算参数.计算类型 == "设计规划":
@@ -2009,15 +2022,19 @@ class 变压器模型(设备模型):
 
         ##### PORT VARIABLE DEFINITION ####
 
+        self.ports = {}
+
         self.电输出 = self.变量列表("电输出", within=NonNegativeReals)
         """
         类型: 变压器输出
         """
+        self.ports["电输出"] = self.电输出
 
         self.电输入 = self.变量列表("电输入", within=NegativeReals)
         """
         类型: 电母线输入
         """
+        self.ports["电输入"] = self.电输入
 
         # 设备特有约束（变量）
 
@@ -2152,15 +2169,19 @@ class 变流器模型(设备模型):
 
         ##### PORT VARIABLE DEFINITION ####
 
+        self.ports = {}
+
         self.电输出 = self.变量列表("电输出", within=NonNegativeReals)
         """
         类型: 电母线输出
         """
+        self.ports["电输出"] = self.电输出
 
         self.电输入 = self.变量列表("电输入", within=NegativeReals)
         """
         类型: 变流器输入
         """
+        self.ports["电输入"] = self.电输入
 
         # 设备特有约束（变量）
 
@@ -2297,15 +2318,19 @@ class 双向变流器模型(设备模型):
 
         ##### PORT VARIABLE DEFINITION ####
 
-        self.储能端 = self.变量列表("储能端", within=Reals)
-        """
-        类型: 双向变流器储能端输入输出
-        """
+        self.ports = {}
 
         self.线路端 = self.变量列表("线路端", within=Reals)
         """
         类型: 双向变流器线路端输入输出
         """
+        self.ports["线路端"] = self.线路端
+
+        self.储能端 = self.变量列表("储能端", within=Reals)
+        """
+        类型: 双向变流器储能端输入输出
+        """
+        self.ports["储能端"] = self.储能端
 
         # 设备特有约束（变量）
         self.线路端_ = self.变量列表_带指示变量("线路端_")
@@ -2427,15 +2452,19 @@ class 传输线模型(设备模型):
 
         ##### PORT VARIABLE DEFINITION ####
 
+        self.ports = {}
+
         self.电输出 = self.变量列表("电输出", within=NonNegativeReals)
         """
         类型: 电母线输出
         """
+        self.ports["电输出"] = self.电输出
 
         self.电输入 = self.变量列表("电输入", within=NegativeReals)
         """
         类型: 电母线输入
         """
+        self.ports["电输入"] = self.电输入
 
         # 设备特有约束（变量）
         self.电输入_去除损耗 = self.变量列表_带指示变量("电输入_去除损耗")
