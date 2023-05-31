e22b01181c77d00b77cfb3de34609dfd674acfe6
jamesbrown <randomvoidmail@foxmail.com>
2023-05-31 13:22:00 +0800

    update

--- microgrid_base/ies_optim.py
+++ microgrid_base/ies_optim.py
@@ -218,13 +218,13 @@ class 风力发电信息(BaseModel):  # 发电设备
 
 class 柴油发电ID(BaseModel):
     ID: int
-    电接口: int
-    """
-    类型: 供电端输出
-    """
     燃料接口: int
     """
     类型: 柴油输入
+    """
+    电接口: int
+    """
+    类型: 供电端输出
     """
 
 
@@ -531,13 +531,13 @@ class 变压器信息(BaseModel):  # 配电传输
 
 class 变流器ID(BaseModel):
     ID: int
-    电输出: int
-    """
-    类型: 电母线输出
-    """
     电输入: int
     """
     类型: 变流器输入
+    """
+    电输出: int
+    """
+    类型: 电母线输出
     """
 
 
@@ -699,13 +699,13 @@ class 双向变流器信息(BaseModel):  # 配电传输
 
 class 传输线ID(BaseModel):
     ID: int
-    电输出: int
-    """
-    类型: 电母线输出
-    """
     电输入: int
     """
     类型: 电母线输入
+    """
+    电输出: int
+    """
+    类型: 电母线输出
     """
 
 
@@ -1511,14 +1511,14 @@ class 柴油发电模型(设备模型):
 
         self.ports = {}
 
-        self.ports["电接口"] = self.电接口 = self.变量列表("电接口", within=NonNegativeReals)
-        """
-        类型: 供电端输出
-        """
-
         self.ports["燃料接口"] = self.燃料接口 = self.变量列表("燃料接口", within=NegativeReals)
         """
         类型: 柴油输入
+        """
+
+        self.ports["电接口"] = self.电接口 = self.变量列表("电接口", within=NonNegativeReals)
+        """
+        类型: 供电端输出
         """
 
         # 设备特有约束（变量）
@@ -2174,14 +2174,14 @@ class 变流器模型(设备模型):
 
         self.ports = {}
 
-        self.ports["电输出"] = self.电输出 = self.变量列表("电输出", within=NonNegativeReals)
-        """
-        类型: 电母线输出
-        """
-
         self.ports["电输入"] = self.电输入 = self.变量列表("电输入", within=NegativeReals)
         """
         类型: 变流器输入
+        """
+
+        self.ports["电输出"] = self.电输出 = self.变量列表("电输出", within=NonNegativeReals)
+        """
+        类型: 电母线输出
         """
 
         # 设备特有约束（变量）
@@ -2457,14 +2457,14 @@ class 传输线模型(设备模型):
 
         self.ports = {}
 
-        self.ports["电输出"] = self.电输出 = self.变量列表("电输出", within=NonNegativeReals)
-        """
-        类型: 电母线输出
-        """
-
         self.ports["电输入"] = self.电输入 = self.变量列表("电输入", within=NegativeReals)
         """
         类型: 电母线输入
+        """
+
+        self.ports["电输出"] = self.电输出 = self.变量列表("电输出", within=NonNegativeReals)
+        """
+        类型: 电母线输出
         """
 
         # 设备特有约束（变量）
@@ -2504,6 +2504,14 @@ class 电负荷:
         self,
     ):
         ...
+        ##### PORT VARIABLE DEFINITION ####
+
+        self.ports = {}
+
+        self.ports["电接口"] = self.电接口 = self.变量列表("电接口", within=NegativeReals)
+        """
+        类型: 负荷电输入
+        """
 
     def constraints_register(
         self,
