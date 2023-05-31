f5092823c2f787ecf0f11a8aee8217e4b03a05ff
jamesbrown <randomvoidmail@foxmail.com>
2023-05-31 17:54:01 +0800

    update

--- microgrid_base/ies_optim.py
+++ microgrid_base/ies_optim.py
@@ -65,13 +65,13 @@ class 风力发电ID(BaseModel):
 
 class 柴油发电ID(BaseModel):
     ID: int
-    燃料接口: int
-    """
-    类型: 柴油输入
-    """
     电接口: int
     """
     类型: 供电端输出
+    """
+    燃料接口: int
+    """
+    类型: 柴油输入
     """
 
 
@@ -85,49 +85,49 @@ class 锂电池ID(BaseModel):
 
 class 变压器ID(BaseModel):
     ID: int
-    电输入: int
-    """
-    类型: 电母线输入
-    """
     电输出: int
     """
     类型: 变压器输出
+    """
+    电输入: int
+    """
+    类型: 电母线输入
     """
 
 
 class 变流器ID(BaseModel):
     ID: int
-    电输入: int
-    """
-    类型: 变流器输入
-    """
     电输出: int
     """
     类型: 电母线输出
+    """
+    电输入: int
+    """
+    类型: 变流器输入
     """
 
 
 class 双向变流器ID(BaseModel):
     ID: int
-    储能端: int
-    """
-    类型: 双向变流器储能端输入输出
-    """
     线路端: int
     """
     类型: 双向变流器线路端输入输出
+    """
+    储能端: int
+    """
+    类型: 双向变流器储能端输入输出
     """
 
 
 class 传输线ID(BaseModel):
     ID: int
-    电输入: int
-    """
-    类型: 电母线输入
-    """
     电输出: int
     """
     类型: 电母线输出
+    """
+    电输入: int
+    """
+    类型: 电母线输入
     """
 
 
@@ -1574,14 +1574,14 @@ class 柴油发电模型(设备模型):
 
         self.ports = {}
 
-        self.ports["燃料接口"] = self.燃料接口 = self.变量列表("燃料接口", within=NegativeReals)
-        """
-        类型: 柴油输入
-        """
-
         self.ports["电接口"] = self.电接口 = self.变量列表("电接口", within=NonNegativeReals)
         """
         类型: 供电端输出
+        """
+
+        self.ports["燃料接口"] = self.燃料接口 = self.变量列表("燃料接口", within=NegativeReals)
+        """
+        类型: 柴油输入
         """
 
         # 设备特有约束（变量）
@@ -2127,14 +2127,14 @@ class 变压器模型(设备模型):
 
         self.ports = {}
 
-        self.ports["电输入"] = self.电输入 = self.变量列表("电输入", within=NegativeReals)
-        """
-        类型: 电母线输入
-        """
-
         self.ports["电输出"] = self.电输出 = self.变量列表("电输出", within=NonNegativeReals)
         """
         类型: 变压器输出
+        """
+
+        self.ports["电输入"] = self.电输入 = self.变量列表("电输入", within=NegativeReals)
+        """
+        类型: 电母线输入
         """
 
         # 设备特有约束（变量）
@@ -2282,14 +2282,14 @@ class 变流器模型(设备模型):
 
         self.ports = {}
 
-        self.ports["电输入"] = self.电输入 = self.变量列表("电输入", within=NegativeReals)
-        """
-        类型: 变流器输入
-        """
-
         self.ports["电输出"] = self.电输出 = self.变量列表("电输出", within=NonNegativeReals)
         """
         类型: 电母线输出
+        """
+
+        self.ports["电输入"] = self.电输入 = self.变量列表("电输入", within=NegativeReals)
+        """
+        类型: 变流器输入
         """
 
         # 设备特有约束（变量）
@@ -2431,14 +2431,14 @@ class 双向变流器模型(设备模型):
 
         self.ports = {}
 
-        self.ports["储能端"] = self.储能端 = self.变量列表("储能端", within=Reals)
-        """
-        类型: 双向变流器储能端输入输出
-        """
-
         self.ports["线路端"] = self.线路端 = self.变量列表("线路端", within=Reals)
         """
         类型: 双向变流器线路端输入输出
+        """
+
+        self.ports["储能端"] = self.储能端 = self.变量列表("储能端", within=Reals)
+        """
+        类型: 双向变流器储能端输入输出
         """
 
         # 设备特有约束（变量）
@@ -2569,14 +2569,14 @@ class 传输线模型(设备模型):
 
         self.ports = {}
 
-        self.ports["电输入"] = self.电输入 = self.变量列表("电输入", within=NegativeReals)
-        """
-        类型: 电母线输入
-        """
-
         self.ports["电输出"] = self.电输出 = self.变量列表("电输出", within=NonNegativeReals)
         """
         类型: 电母线输出
+        """
+
+        self.ports["电输入"] = self.电输入 = self.变量列表("电输入", within=NegativeReals)
+        """
+        类型: 电母线输入
         """
 
         # 设备特有约束（变量）
@@ -2861,4 +2861,8 @@ def compute(devs: List[dict], adders: Dict[int, dict], graph_data: dict, G: Grap
 
             devInstDict.update({devID_int: devInst})
         for adder_index, adder in adders.items():
-            ...
+            input_indexs, output_indexs, io_indexs = (
+                adder["input"],
+                adder["output"],
+                adder["IO"],
+            )
