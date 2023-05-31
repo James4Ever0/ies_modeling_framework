83d86b5121429f09105ee60afc960db3b55c58d9
jamesbrown <randomvoidmail@foxmail.com>
2023-05-31 13:25:55 +0800

    update

--- microgrid_base/ies_optim.py
+++ microgrid_base/ies_optim.py
@@ -447,13 +447,13 @@ class 锂电池信息(BaseModel):  # 储能设备
 
 class 变压器ID(BaseModel):
     ID: int
-    电输入: int
-    """
-    类型: 电母线输入
-    """
     电输出: int
     """
     类型: 变压器输出
+    """
+    电输入: int
+    """
+    类型: 电母线输入
     """
 
 
@@ -699,13 +699,13 @@ class 双向变流器信息(BaseModel):  # 配电传输
 
 class 传输线ID(BaseModel):
     ID: int
-    电输入: int
-    """
-    类型: 电母线输入
-    """
     电输出: int
     """
     类型: 电母线输出
+    """
+    电输入: int
+    """
+    类型: 电母线输入
     """
 
 
@@ -2027,14 +2027,14 @@ class 变压器模型(设备模型):
 
         self.ports = {}
 
-        self.ports["电输入"] = self.电输入 = self.变量列表("电输入", within=NegativeReals)
-        """
-        类型: 电母线输入
-        """
-
         self.ports["电输出"] = self.电输出 = self.变量列表("电输出", within=NonNegativeReals)
         """
         类型: 变压器输出
+        """
+
+        self.ports["电输入"] = self.电输入 = self.变量列表("电输入", within=NegativeReals)
+        """
+        类型: 电母线输入
         """
 
         # 设备特有约束（变量）
@@ -2457,14 +2457,14 @@ class 传输线模型(设备模型):
 
         self.ports = {}
 
-        self.ports["电输入"] = self.电输入 = self.变量列表("电输入", within=NegativeReals)
-        """
-        类型: 电母线输入
-        """
-
         self.ports["电输出"] = self.电输出 = self.变量列表("电输出", within=NonNegativeReals)
         """
         类型: 电母线输出
+        """
+
+        self.ports["电输入"] = self.电输入 = self.变量列表("电输入", within=NegativeReals)
+        """
+        类型: 电母线输入
         """
 
         # 设备特有约束（变量）
@@ -2499,7 +2499,7 @@ class 传输线模型(设备模型):
         return 总成本年化
 
 
-class 电负荷:
+class 电负荷(设备模型):
     def __init__(
         self,
     ):
