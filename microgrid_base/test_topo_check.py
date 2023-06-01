769da1dbdac6f1fa26dfcf8879cb1846daf75df5
jamesbrown <randomvoidmail@foxmail.com>
2023-06-01 14:36:00 +0800

    update

--- microgrid_base/test_topo_check.py
+++ microgrid_base/test_topo_check.py
@@ -95,8 +95,30 @@ DEL1 = 变流器(
         MinDeviceCount=100,
     ).dict(),
 )
-DEL2 = 变压器(topo, param=变压器信息(**devParam, 
-                            
+DEL2 = 变压器(
+    topo,
+    param=变压器信息(
+        **devParam,
+        PowerParameter=0.9,
+        LoadRedundancyParameter=1.2,
+        RatedPower=20,
+        CostPerKilowatt=100,
+        CostPerYearPerKilowatt=100,
+        VariationalCostPerWork=100,
+        Life=20,
+        Efficiency=0.9,
+        BuildCostPerKilowatt=10,
+        BuildBaseCost=10,
+        DeviceCount=100,
+        MaxDeviceCount=200,
+        MinDeviceCount=100,
+    ).dict(),
+)
+LOAD = 电负荷(topo, param=电负荷信息(**devParam,EnergyConsumption=[], MaxEnergyConsumption=100).dict())
+
+BAT = 锂电池(topo, param=锂电池信息(**devParam,
+        PowerParameter=0.9,
+        LoadRedundancyParameter=1.2,
         RatedPower=20,
         CostPerKilowatt=100,
         CostPerYearPerKilowatt=100,
@@ -108,9 +130,6 @@ DEL2 = 变压器(topo, param=变压器信息(**devParam,
         DeviceCount=100,
         MaxDeviceCount=200,
         MinDeviceCount=100,).dict())
-LOAD = 电负荷(topo, param=电负荷信息(**devParam).dict())
-
-BAT = 锂电池(topo, param=锂电池信息(**devParam).dict())
 
 A1 = 母线(topo, "可连接供电端母线")
 A2 = 母线(topo, "可连接供电端母线")
