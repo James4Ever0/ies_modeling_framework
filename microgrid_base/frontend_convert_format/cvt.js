e05a7e57a878fce70914bb81579fedf42dfdbf4c
jamesbrown <randomvoidmail@foxmail.com>
2023-06-02 17:22:00 +0800

    update

--- microgrid_base/frontend_convert_format/cvt.js
+++ microgrid_base/frontend_convert_format/cvt.js
@@ -1,5 +1,9 @@
 var path = "sample.json"
 
+function pretty_print(obj){
+    console.log(JSON.stringify(obj,null, 4))
+}
+
 const fs = require('fs')
 
 var content = fs.readFileSync(path)
@@ -8,4 +12,5 @@ var obj = JSON.parse(content)
 
 obj.graph = JSON.parse(obj.graph)
 // console.log(obj)
-console.dir(obj)
+// console.dir(obj)
+pretty_print(obj)
