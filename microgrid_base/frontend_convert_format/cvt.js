51e32736caaa771d073c3b3798ee6c46ee41ba16
jamesbrown <randomvoidmail@foxmail.com>
2023-06-02 17:35:19 +0800

    update

--- microgrid_base/frontend_convert_format/cvt.js
+++ microgrid_base/frontend_convert_format/cvt.js
@@ -1,20 +1,27 @@
-var path = "sample.json"
+var path="sample.json"
 
-function pretty_print(obj,output_path){
-    let data = JSON.stringify(obj,null, 4)
+function pretty_print(obj,output_path) {
+    let data=JSON.stringify(obj,null,4)
     console.log(data)
-    fs.writeFileSync(output_path, data)
-    console.log("write to:", output_path)
+    fs.writeFileSync(output_path,data)
+    console.log("write to:",output_path)
 }
 
-const fs = require('fs')
+const fs=require('fs')
 
-var content = fs.readFileSync(path)
+var content=fs.readFileSync(path)
 
-var obj = JSON.parse(content)
+var obj=JSON.parse(content)
 
-obj.graph = JSON.parse(obj.graph)
+obj.graph=JSON.parse(obj.graph)
 // console.log(obj)
 // console.dir(obj)
-pretty_print(obj, 'sample_parse.json')
+pretty_print(obj,'sample_parse.json')
 
+var graph_data = {}
+
+var input_template={
+    "directed": false,
+    "multigraph": false,
+    "graph": graph_data,"nodes": [],"links": []
+}
