728e1e8b85972624918e1ca8fa7bddf0f8c6af3e
jamesbrown <randomvoidmail@foxmail.com>
2023-06-02 14:50:00 +0800

    update

--- microgrid_base/parse_export_format.py
+++ microgrid_base/parse_export_format.py
@@ -1,12 +1,17 @@
 excel_path ="设备信息库各参数.xlsx"
 
+from paddle import tolist
 import pandas
 
 table_name = "仿真结果"
 
-table = pandas.read_excel(excel_path, sheet_name=  table_name)
+table = pandas.read_excel(excel_path, sheet_name=  table_name, header=None)
 
 # print(table)
+import numpy
+def check_empty(elem):
+    if type(elem) elem is numpy.nan
+
 for i, r in table.iterrows():
     # print(i)
-    print(r)
+    print(r.tolist())
