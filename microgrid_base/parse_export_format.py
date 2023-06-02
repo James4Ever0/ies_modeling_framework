778b0b2047427535966c3cfc5e3c54573d595e8c
jamesbrown <randomvoidmail@foxmail.com>
2023-06-02 14:46:00 +0800

    update

--- microgrid_base/parse_export_format.py
+++ microgrid_base/parse_export_format.py
@@ -1,13 +1,12 @@
 excel_path ="设备信息库各参数.xlsx"
 
-# import openpyxl
 import pandas
 
 table_name = "仿真结果"
 
-# excel = openpyxl.load_workbook(excel_path)
-
 table = pandas.read_excel(excel_path, sheet_name=  table_name)
 
-# table = excel[table_name]
-print(table)
+# print(table)
+for i, r in table.iterrows():
+    print(i)
+    print(r)
