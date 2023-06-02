a0e801706991d8a40e12ad89111a9bb1b9e705e1
jamesbrown <randomvoidmail@foxmail.com>
2023-06-02 14:58:00 +0800

    update

--- microgrid_base/parse_export_format.py
+++ microgrid_base/parse_export_format.py
@@ -1,11 +1,11 @@
-excel_path ="设备信息库各参数.xlsx"
+excel_path = "设备信息库各参数.xlsx"
 
 from paddle import tolist
 import pandas
 
 table_name = "仿真结果"
 
-table = pandas.read_excel(excel_path, sheet_name=  table_name, header=None)
+table = pandas.read_excel(excel_path, sheet_name=table_name, header=None)
 
 # print(table)
 def is_empty(elem):
@@ -14,9 +14,13 @@ def is_empty(elem):
     else:
         return True
 
+
+trough = True
+
 for i, r in table.iterrows():
-    rlist = [( e.strip() if type(e) == str else e) for e in r.tolist()]
+    rlist = [(e.strip() if type(e) == str else e) for e in r.tolist()]
     first_elem, second_elem = rlist[0], rlist[1]
     if is_empty(first_elem):
-        continue
-    if not is_empty(first_elem) and
+        trough = True
+    if not is_empty(first_elem) and is_empty(second_elem):
+        trough = True
