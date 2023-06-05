23f1f30772694e0628f015291ceac1dc601e9dfd
jamesbrown <randomvoidmail@foxmail.com>
2023-06-05 14:32:43 +0800

    update

--- microgrid_base/frontend_convert_format/cvt.js
+++ microgrid_base/frontend_convert_format/cvt.js
@@ -57,7 +57,7 @@ for(var e of obj.connectionsAnchors) {
     let conn = connLUT[conn_id]
     let source_anchor_id=e.sourceAnchors.port_id
     let target_anchor_id=e.targetAnchors.port_id
-    let sourceAnchor = anchorLUT[`${conn.source_id}_${source_anchor_id}`] 
+    let sourceAnchor = anchorLUT[`${conn.source_id}_${source_anchor_id}`]
     let targetAnchor = anchorLUT[`${conn.target_id}_${target_anchor_id}`]
 }
 
