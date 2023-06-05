9b9ebdd8447c48dcee98bf8c921e0873c81db1a7
jamesbrown <randomvoidmail@foxmail.com>
2023-06-05 17:16:00 +0800

    update

--- microgrid_base/parse_optim_constraints.py
+++ microgrid_base/parse_optim_constraints.py
@@ -16,3 +16,7 @@ for elem in mfile.body:
     if type(elem) == ast.ClassDef:
         cname = elem.name
         print(cname)
+        if cname.endswith('模型'):
+            ...
+    elif type(elem) == ast.FunctionDef:
+        funcName = elem.name
