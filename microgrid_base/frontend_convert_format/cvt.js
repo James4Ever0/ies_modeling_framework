ac27f437860494007c33d76f3f974bbd409656a8
jamesbrown <randomvoidmail@foxmail.com>
2023-06-05 15:14:00 +0800

    update

--- microgrid_base/frontend_convert_format/cvt.js
+++ microgrid_base/frontend_convert_format/cvt.js
@@ -18,7 +18,14 @@ obj.graph=JSON.parse(obj.graph)
 // console.dir(obj)
 pretty_print(obj,'sample_parse.json')
 
-var graph_data={}
+var graph_data={
+    "典型日ID": null,
+    "计算步长": "小时",
+    "典型日": false,
+    "典型日权重": 0,
+    "计算类型": "设计规划",
+    "风速": [],"光照": [],"气温": [],"年利率": 0.1
+}
 
 var input_template={
     "directed": false,
@@ -29,8 +36,8 @@ var input_template={
 var anchorLUT={}
 var connLUT={}
 var node_id_cursor=0
-var idLUT = {}
-var devLUT = {}
+var idLUT={}
+var devLUT={}
 
 // const myRe = /.+models\/(.+)\.svg.+/g;
 
@@ -39,35 +46,35 @@ for(var e of obj.graph.mxGraphModel.root.mxCell) {
     if(e.vertex) {
         let node_id=id
         // devType = myRe.exec(e._style)[0];
-        let val = e._style;
-        let devType = val.split("models/")[1].split(".svg")[0]
-        idLUT[`${node_id}`] = node_id_cursor++;
-        devLUT[`${node_id}`] = devType;
-        if (e.Array.Object.length === undefined){
-            e.Array.Object = [e.Array.Object]
+        let val=e._style;
+        let devType=val.split("models/")[1].split(".svg")[0]
+        idLUT[`${node_id}`]=node_id_cursor++;
+        devLUT[`${node_id}`]=devType;
+        if(e.Array.Object.length===undefined) {
+            e.Array.Object=[e.Array.Object]
         }
-        for (let o of e.Array.Object){
-            anchor_id = o._portId
-            anchor_title = o._title
-            anchor_k = `${node_id}_${anchor_id}`
-            anchorLUT[anchor_k] = anchor_title
-            idLUT[anchor_k] = node_id_cursor++;
+        for(let o of e.Array.Object) {
+            anchor_id=o._portId
+            anchor_title=o._title
+            anchor_k=`${node_id}_${anchor_id}`
+            anchorLUT[anchor_k]=anchor_title
+            idLUT[anchor_k]=node_id_cursor++;
         }
     } else if(e.edge) {
         connLUT[id].source_id=e.source
         connLUT[id].target_id=e.target
-        connLUT[id].connType = e.connType
-        idLUT[`${id}`] = node_id_cursor++; 
+        connLUT[id].connType=e.connType
+        idLUT[`${id}`]=node_id_cursor++;
     }
 }
 
 for(var e of obj.connectionsAnchors) {
     let conn_id=e.id
-    let conn = connLUT[conn_id]
+    let conn=connLUT[conn_id]
     let source_anchor_id=e.sourceAnchors.port_id
     let target_anchor_id=e.targetAnchors.port_id
-    let sourceAnchorType = anchorLUT[`${conn.source_id}_${source_anchor_id}`]
-    let targetAnchorType = anchorLUT[`${conn.target_id}_${target_anchor_id}`]
+    let sourceAnchorType=anchorLUT[`${conn.source_id}_${source_anchor_id}`]
+    let targetAnchorType=anchorLUT[`${conn.target_id}_${target_anchor_id}`]
 }
 
 for(var e of obj.rightParams) {
