dafb243ec8bfebb436def52b8639fff82ed666df
jamesbrown <randomvoidmail@foxmail.com>
2023-06-06 14:36:01 +0800

    update

--- microgrid_base/parse_export_format.py
+++ microgrid_base/parse_export_format.py
@@ -51,7 +51,14 @@ new_data = {k: {} for k in data.keys()}
 default_unit_maps = {"平均效率/平均COP": "one", "设备台数": "one", "时间": "one"}
 
 # None -> str
-from unit_utils import unitCleaner, unitParser, standard_units, unitFactorCalculator, ureg, translateUnit
+from unit_utils import (
+    unitCleaner,
+    unitParser,
+    standard_units,
+    unitFactorCalculator,
+    ureg,
+    translateUnit,
+)
 
 
 def convert_format(h_array):
@@ -67,41 +74,45 @@ def convert_format(h_array):
             unit = default_unit_maps.get(elem, None)
         if unit:
             old_unit_name = translateUnit(unit)
-            print('processing:',elem_name)
-            mag, new_unit_name = unitFactorCalculator(ureg,standard_units, old_unit_name)
+            print("processing:", elem_name)
+            mag, new_unit_name = unitFactorCalculator(
+                ureg, standard_units, old_unit_name
+            )
             unit = (mag, new_unit_name, old_unit_name)
-            
+
         result_mapping[elem_name] = unit
     return result_mapping
 
 
-              
 new_data["仿真结果"]["ALL"] = convert_format(data["仿真结果"][0]["headings"])
 
 from param_base import 设备接口集合
+
 all_device_names = list(设备接口集合.keys())
 
 print()
 rich.print(all_device_names)
 
-nonDevNames = ['柴油','电负荷']
-commonDevParams = []
-commonParams = ['元件名称']
+nonDevNames = ["柴油", "电负荷"]
+commonDevParams = ["设备型号", "设备台数"]
+commonParams = ["元件名称"]
+
 simDevParam = {
-    '柴油':[],
-    '电负荷':[],
-    '光伏发电':[],
-    '风力发电':[],
-    '柴油发电':[],
-    '锂电池':[],
-    '变压器':[],
-    '变流器':[],
-    '双向变流器':[],
-    '传输线':[],
+    "柴油": [],
+    "电负荷": [],
+    "光伏发电": [],
+    "风力发电": [],
+    "柴油发电": [],
+    "锂电池": [],
+    "变压器": [],
+    "变流器": [],
+    "双向变流器": [],
+    "传输线": [],
 }
+产冷量
 
 for d in all_device_names:
-    new_data['仿真结果'][d] = convert_format(simDevParam[d])
+    new_data["仿真结果"][d] = convert_format(simDevParam[d])
 
 
 print()
