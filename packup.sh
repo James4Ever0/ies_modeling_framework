d14a20edae6411e45d32f361b728c33f4eb0dd44
jamesbrown <randomvoidmail@foxmail.com>
2023-06-07 17:14:00 +0800

    update

--- packup.sh
+++ packup.sh
@@ -1,3 +1,3 @@
 archive_name=archive.7z
 rm -rf $archive_name
-find . | grep -E "*.(py|j2|json|txt)" | xargs -Iabc 7z a $archive_name abc
+find . | grep -E ".+(py|j2|json|txt)\$" | xargs -Iabc 7z a $archive_name abc
