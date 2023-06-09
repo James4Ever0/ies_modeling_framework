430f6df6d8107e239ccb6be2cc417f666ced4ace
jamesbrown <randomvoidmail@foxmail.com>
2023-06-09 10:40:01 +0800

    update

--- microgrid_base/test_topo_check.py
+++ microgrid_base/test_topo_check.py
@@ -339,7 +339,7 @@ if sys.argv[-1] in ["-f", "--full"]:
                 rich.print(出力曲线字典)
                 print()
                 仿真结果表.head()
-                仿真结果表.
+                # 仿真结果表.to_json(force_ascii=False)
             except:
                 import traceback
 
