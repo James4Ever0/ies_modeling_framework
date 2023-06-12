150cc81dd3c4c1f2b6cfa6c11ac04321117b7cf0
jamesbrown <randomvoidmail@foxmail.com>
2023-06-12 09:56:01 +0800

    update

--- microgrid_base/parse_frontend_sim_param_translation.py
+++ microgrid_base/parse_frontend_sim_param_translation.py
@@ -5,8 +5,18 @@ import parse
 with open(filepath,'r') as f:
     data = f.read()
     lines = data.split("\n")
-    print(lines)
+    # print(lines)
 
+resultMap = {}
 for line in lines:
     line = line.strip()
-    parse.parse("{englishName}:{space_1}'{sampleData}',{space_2}//{chineseName}",line)
+    result = parse.parse("{englishName}:{space_1}'{sampleData}',{space_2}//{chineseName}",line)
+    if result:
+        print(result)
+        resultMap[result['chineseName'].upper()] = result['englishName']
+    
+import json
+output_path = "frontend_sim_param_translation.json"
+with open(output_path,'w+') as f:
+    f.write(json.dumps(resultMap))
+print('writing to:', output_path)
