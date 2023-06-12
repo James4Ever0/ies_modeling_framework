6e2ef43c5a36e3c3e3b48a5d44592e79395d79b7
jamesbrown <randomvoidmail@foxmail.com>
2023-06-12 09:54:00 +0800

    update

--- microgrid_base/parse_frontend_sim_param_translation.py
+++ microgrid_base/parse_frontend_sim_param_translation.py
@@ -1,8 +1,12 @@
 filepath = "frontend_sim_param_translation.js"
 
-import re
+import parse
 
 with open(filepath,'r') as f:
     data = f.read()
     lines = data.split("\n")
     print(lines)
+
+for line in lines:
+    line = line.strip()
+    parse.parse("{englishName}:{space_1}'{sampleData}',{space_2}//{chineseName}",line)
