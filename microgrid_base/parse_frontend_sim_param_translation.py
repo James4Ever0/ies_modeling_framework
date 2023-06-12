5cf2465880383e66e6d8aa9f073336ce0862d445
jamesbrown <randomvoidmail@foxmail.com>
2023-06-12 15:52:00 +0800

    update

--- microgrid_base/parse_frontend_sim_param_translation.py
+++ microgrid_base/parse_frontend_sim_param_translation.py
@@ -9,9 +9,9 @@ with open(filepath, "r") as f:
 
 resultMap = {}
 for line in lines:
-    line = line.strip().replace(":", " : ").replace(",", " , ")
+    line = line.replace(":", ": ").replace(",", " , ").replace(" :",":").strip()
     result = parse.parse(
-        "{englishName}{space_0}:{space_1}'{sampleData}'{space_2},{space_3}//{chineseName}", line
+        "{englishName}:{space_1}'{sampleData}'{space_2},{space_3}//{chineseName}", line
     )
     if result:
         print(result)
