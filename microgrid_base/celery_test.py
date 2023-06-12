1eb15bae94088a3de8f38e829c0d922f0c6336b4
jamesbrown <randomvoidmail@foxmail.com>
2023-06-12 17:56:00 +0800

    update

--- microgrid_base/celery_test.py
+++ microgrid_base/celery_test.py
@@ -9,11 +9,12 @@ port = 9870
 url = f"http://127.0.0.1:{port}/calculate_async"
 from fastapi_datamodel_template import EnergyFlowGraph
 # from ies_optim import 设备节点
-# data = EnergyFlowGraph(mDictList=mDictList)
-mdata = mDictList[0]['nodes'][25] # 25-35
-import rich
-rich.print(mdata) # 设备
-# requests.get(url, json = data.dict())
-
+data = EnergyFlowGraph(mDictList=mDictList)
+# mdata = mDictList[0]['nodes'][25] # 25-35
+# import rich
+# rich.print(mdata) # 设备
+r = requests.post(url, json = data.dict())
+print(r.json())
+print(r.status_code)
 # t = 设备节点.parse_obj(mdata)
 # print(t)
