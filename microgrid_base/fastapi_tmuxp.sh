e931c31b520838f78d9f89ef7e8111ae9e9a1225
jamesbrown <randomvoidmail@foxmail.com>
2023-06-12 16:24:00 +0800

    update

--- microgrid_base/fastapi_tmuxp.sh
+++ microgrid_base/fastapi_tmuxp.sh
@@ -1,4 +1,4 @@
-ps aux | grep rabbitmq | grep -v grep | awk '{print $2}' | xargs -Iabc kill -s KILL abc
-ps aux | grep redis | grep 6380 | grep -v grep | awk '{print $2}' | xargs -Iabc kill -s KILL abc
+ps aux | grep rabbitmq | grep -v grep | awk '{print $2}' | xargs -Iabc kill -s TERM abc
+ps aux | grep redis | grep 6380 | grep -v grep | awk '{print $2}' | xargs -Iabc kill -s TERM abc
 tmux kill-session -t fastapi_tmuxp
 tmuxp load fastapi_tmuxp.yml
