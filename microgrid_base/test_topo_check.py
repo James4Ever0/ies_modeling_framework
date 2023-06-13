dbb2ed345c8f19f04cb572d1eea038c1eb9b0ca9
jamesbrown <randomvoidmail@foxmail.com>
2023-06-13 16:38:00 +0800

    update

--- microgrid_base/test_topo_check.py
+++ microgrid_base/test_topo_check.py
@@ -26,7 +26,10 @@ a = abs(np.random.random((8760,))).tolist()
 
 # algoParam = 计算参数(计算步长="小时", 典型日=False, 计算类型="仿真模拟", 风速=a, 光照=a, 气温=a, 年利率=0.1).dict()
 algoParam = 计算参数(
-    计算目标="经济", 计算步长="小时", 典型日=False, 计算类型="设计规划", 风速=a, 光照=a, 气温=a, 年利率=0.1
+    # 计算目标="经济",
+    # 计算目标="经济_环保",
+    计算目标="环保",
+    计算步长="小时", 典型日=False, 计算类型="设计规划", 风速=a, 光照=a, 气温=a, 年利率=0.1
 ).dict()
 # topo = 拓扑图()  # with structure?
 topo = 拓扑图(**algoParam)  # with structure?
