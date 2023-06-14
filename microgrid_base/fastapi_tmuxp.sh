9868d4c3a192f8951f0f6b1673f9bbcd33771db0
jamesbrown <randomvoidmail@foxmail.com>
2023-06-14 14:38:01 +0800

    update

--- microgrid_base/fastapi_tmuxp.sh
+++ microgrid_base/fastapi_tmuxp.sh
@@ -2,6 +2,13 @@ ps aux | grep rabbitmq | grep -v grep | awk '{print $2}' | xargs -Iabc kill -s T
 ps aux | grep redis | grep 6380 | grep -v grep | awk '{print $2}' | xargs -Iabc kill -s TERM abc
 tmux kill-session -t fastapi_tmuxp
 sleep 3
-echo $1
+if [ "$1" -eq "macos" ] ; then
+echo macos
+elif [ "$1" -eq "windows" ] ; then 
+echo windows
+else 
+echo unknown
+exit 1
+fi
 # env CPLEX_DIR=":/Applications/CPLEX_Studio1210/cplex/bin/x86-64_osx/" CONDA_ENV_NAME="rosetta" tmuxp load fastapi_tmuxp.yml
 # env CPLEX_DIR="" CONDA_ENV_NAME="cplex" tmuxp load fastapi_tmuxp.yml
