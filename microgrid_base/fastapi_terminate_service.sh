bc2a7afcc1d27933521fe414b13d052fe8b78bb0
jamesbrown <randomvoidmail@foxmail.com>
2023-06-14 15:50:00 +0800

    update

---
+++ microgrid_base/fastapi_terminate_service.sh
@@ -0,0 +1,4 @@
+ps aux | grep rabbitmq | grep -v grep | awk '{print $2}' | xargs -Iabc kill -s TERM abc
+ps aux | grep redis | grep 6380 | grep -v grep | awk '{print $2}' | xargs -Iabc kill -s TERM abc
+tmux kill-session -t fastapi_tmuxp
+sleep 3
