456031061d32bbc29f03d986aff8a7172cdaa9ca
jamesbrown <randomvoidmail@foxmail.com>
2023-06-15 15:30:00 +0800

    update

--- microgrid_base/init.sh
+++ microgrid_base/init.sh
@@ -1,2 +1,3 @@
 sudo apt install -y redis rabbitmq-server tmux tmuxp
-conda run -n cplex --live-stream --no-capture-output pip install -i https://mirrors.tuna.tsinghua.edu.cn/pypi -r requirements.txt
+conda run -n cplex --live-stream --no-capture-output pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+conda run -n cplex --live-stream --no-capture-output pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
