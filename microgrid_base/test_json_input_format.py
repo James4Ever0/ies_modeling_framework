bb6d3f049f9b37a4ca9d638ff79f5318f278b940
jamesbrown <randomvoidmail@foxmail.com>
2023-06-15 09:10:00 +0800

    update

--- microgrid_base/test_json_input_format.py
+++ microgrid_base/test_json_input_format.py
@@ -9,3 +9,12 @@ from ies_optim import EnergyFlowGraph
 from topo_check import 拓扑图
 
 EFG = EnergyFlowGraph.parse_obj(data)
+
+EFG_dict = EFG.dict()
+
+EFG2 = EnergyFlowGraph.parse_obj(EFG_dict)
+
+import rich
+for index, mDict in enumerate(EFG.mDictList):
+    print(f'parsing mDict #{index}')
+    topo = 拓扑图.from_json(mDict.dict())
