cee5e83ff2ab832ce74a1a2279462e34fd83eeb3
jamesbrown <randomvoidmail@foxmail.com>
2023-06-15 09:42:00 +0800

    update

--- microgrid_base/frontend_sim_param_translation.js
+++ microgrid_base/frontend_sim_param_translation.js
@@ -1,27 +1,27 @@
 // 计算结果表格数据
-let resultTable= [{
+let resultTable=[{
     name: '电负荷1',  //元件名称
-modelNumber:'SS1',//设备型号
-equiCounts:'1', //设备台数
-coolingCapacity: '0', //产冷量
+    modelNumber: 'SS1',//设备型号
+    equiCounts: '1', //设备台数
+    coolingCapacity: '0', //产冷量
     coolingLoad: '0', //冷负荷
-   electricSupply: '5445', //产电量
+    electricSupply: '5445', //产电量
     electricLoad: '0.45666', //电负荷
     heatingLoad: '0', //产热量
     heatLoad: '0', //热负荷
     steamProduction: '45', //蒸汽产量
     steamLoad: '89',  //蒸汽负荷
-hydrogenProduction: '89',  //氢气产量
-hydrogenConsumption: '89',  //氢气消耗量
-dieselConsumption: '89',  //柴油消耗量
-dieselConsumptionCosts: '89',  //柴油消耗费用
-naturalGasConsumption: '89',  //天然气消耗量
-naturalGasConsumptionCosts: '89',  //天然气消耗费用
-averageEfficiency: '89',  //平均效率/平均cop
-equipmentMaintenanceCosts: '89',  //设备维护费用
-coldIncome:'123',  //冷收入
-hotIncome:'123',  //热收入
-eletricIncome:'123',  //电收入
-steamIncome:'123',  //蒸汽收入
-hydrogenIncome:'123',  //氢气收入
+    hydrogenProduction: '89',  //氢气产量
+    hydrogenConsumption: '89',  //氢气消耗量
+    dieselConsumption: '89',  //柴油消耗量
+    dieselConsumptionCosts: '89',  //柴油消耗费用
+    naturalGasConsumption: '89',  //天然气消耗量
+    naturalGasConsumptionCosts: '89',  //天然气消耗费用
+    averageEfficiency: '89',  //平均效率/平均cop
+    equipmentMaintenanceCosts: '89',  //设备维护费用
+    coldIncome: '123',  //冷收入
+    hotIncome: '123',  //热收入
+    eletricIncome: '123',  //电收入
+    steamIncome: '123',  //蒸汽收入
+    hydrogenIncome: '123',  //氢气收入
 }]
