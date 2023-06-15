ab6199b43677e6c11fb7a45ed064fd689e6b5981
jamesbrown <randomvoidmail@foxmail.com>
2023-06-15 13:18:00 +0800

    update

--- microgrid_base/fastapi_terminate_service.sh
+++ microgrid_base/fastapi_terminate_service.sh
@@ -1,5 +1,7 @@
 ps aux | grep rabbitmq | grep -v grep | awk '{print $2}' | xargs -Iabc kill -s TERM abc
 ps aux | grep redis | grep 6380 | grep -v grep | awk '{print $2}' | xargs -Iabc kill -s TERM abc
 # shall you wait till terminated.
+# you may use libtmux for better 'killing' experience.
+# 
 tmux kill-session -t fastapi_tmuxp
 sleep 3
