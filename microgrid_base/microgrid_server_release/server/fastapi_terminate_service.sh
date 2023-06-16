d0562e35ebd942230feb9a2a63cecae59965c4f4
jamesbrown <randomvoidmail@foxmail.com>
2023-06-16 11:38:00 +0800

    update

--- microgrid_base/microgrid_server_release/server/fastapi_terminate_service.sh
+++ microgrid_base/microgrid_server_release/server/fastapi_terminate_service.sh
@@ -4,5 +4,6 @@ ps aux | grep redis | grep 6380 | grep -v grep | awk '{print $2}' | xargs -Iabc 
 # you may use libtmux for better 'killing' experience.
 # session name -> pane pids -> send SIGTERM -> kill session by name
 # check if it is really killed. if not, send SIGKILL
-tmux kill-session -t fastapi_tmuxp
+# tmux kill-session -t fastapi_tmuxp
+tmux kill-server # no other panes running anyway.
 sleep 3
