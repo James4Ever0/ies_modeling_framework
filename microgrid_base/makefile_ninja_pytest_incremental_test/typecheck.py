7cfa6a70908b91285d8fae33c7055b4314e833d0
jamesbrown <randomvoidmail@foxmail.com>
2023-06-18 15:38:02 +0800

    update

---
+++ microgrid_base/makefile_ninja_pytest_incremental_test/typecheck.py
@@ -0,0 +1,14 @@
+from typing import Never
+
+def never_call_me(arg: Never) -> None:
+    pass
+
+def int_or_str(arg: int | str) -> None:
+    never_call_me(arg)  # type checker error
+    match arg:
+        case int():
+            print("It's an int")
+        case str():
+            print("It's a str")
+        case _:
+            never_call_me(arg)  # OK, arg is of type Never
