1a2af356a20b8e9744d36eb1b824e34aafaca1cf
jamesbrown <randomvoidmail@foxmail.com>
2023-06-18 17:30:00 +0800

    update

--- microgrid_base/makefile_ninja_pytest_incremental_test/mytest.py
+++ microgrid_base/makefile_ninja_pytest_incremental_test/mytest.py
@@ -1,9 +1,13 @@
 import pytest
-from typing import Sequence, TypeVar
+from typing import Sequence, TypeVar, Optional
 
 Chooseable = TypeVar("Chooseable")
 # pytest -s to print to stdout without collecting
 # pytest --capture=tee-sys
+
+def demo(mtest: Optional[int] = ...):
+    ...
+
 def test_request_cache(request):
     val = request.caches.get("val")
     print(f"Value? {repr(val)}")
