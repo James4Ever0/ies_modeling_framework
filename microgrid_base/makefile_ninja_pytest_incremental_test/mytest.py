0ca4493e6430aaeee4b3a8398ea08780e1bd2e47
jamesbrown <randomvoidmail@foxmail.com>
2023-06-18 17:32:01 +0800

    update

--- microgrid_base/makefile_ninja_pytest_incremental_test/mytest.py
+++ microgrid_base/makefile_ninja_pytest_incremental_test/mytest.py
@@ -5,8 +5,11 @@ Chooseable = TypeVar("Chooseable")
 # pytest -s to print to stdout without collecting
 # pytest --capture=tee-sys
 
+# ninja -> invoke multiple pytest "persistant" sessions, not clearning cache and not rerunning the test if passed test -> clean cache only by passing  target "clean"
+
 def demo(mtest: Optional[int] = ...):
     ...
+
 
 def test_request_cache(request):
     val = request.caches.get("val")
