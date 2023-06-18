43e21cb150a31fdc29d1ad53aa646f7513f09f1e
jamesbrown <randomvoidmail@foxmail.com>
2023-06-18 17:14:01 +0800

    update

--- microgrid_base/makefile_ninja_pytest_incremental_test/mytest.py
+++ microgrid_base/makefile_ninja_pytest_incremental_test/mytest.py
@@ -1,5 +1,7 @@
 import pytest
 
+# pytest -s to print to stdout without collecting
+# pytest --capture=tee-sys
 def test_request_cache(request):
     val = request.caches.get("val")
     print(f"Value? {repr(val)}")
