d9afc59dbd2d483b79c923d9eb63f1311fa57b48
jamesbrown <randomvoidmail@foxmail.com>
2023-06-18 17:26:02 +0800

    update

--- microgrid_base/makefile_ninja_pytest_incremental_test/mytest.py
+++ microgrid_base/makefile_ninja_pytest_incremental_test/mytest.py
@@ -2,6 +2,8 @@ import pytest
 from typing import Sequence, TypeVar
 
 Chooseable = TypeVar("Chooseable")
+
+int.__covariant__
 # pytest -s to print to stdout without collecting
 # pytest --capture=tee-sys
 def test_request_cache(request):
