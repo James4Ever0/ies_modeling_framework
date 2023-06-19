ed6b17fc1702bd8c3e810d1f40fc977e1f28cb7b
jamesbrown <randomvoidmail@foxmail.com>
2023-06-19 19:50:00 +0800

    update

--- microgrid_base/makefile_ninja_pytest_incremental_test/type_check.py
+++ microgrid_base/makefile_ninja_pytest_incremental_test/type_check.py
@@ -1,5 +1,11 @@
-from typing import TypeVar
+from typing import TypeVar, List
+from typing_extensions import Never, TypeAlias
 
 T = TypeVar('T')
+NotList: TypeAlias = T if T is not List else Never
 
-def m
+def mfunc(param: NotList) -> T:
+    print("You don't care what I do")
+    return param
+
+mfunc(123)
