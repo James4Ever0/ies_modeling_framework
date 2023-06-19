7145288bd35c80acf380213e15b1d3a3d550530c
jamesbrown <randomvoidmail@foxmail.com>
2023-06-19 21:20:00 +0800

    update

--- microgrid_base/makefile_ninja_pytest_incremental_test/type_check.py
+++ microgrid_base/makefile_ninja_pytest_incremental_test/type_check.py
@@ -1,33 +1,47 @@
-from typing import Protocol, Any, Generic, TypeVar, runtime_checkable, cast
+from typing import Any, Generic, Protocol, TypeVar, cast, runtime_checkable
 
 T = TypeVar("T")
+
 
 class ArithmaticType(Protocol):
     """
     Protocol of which able to do arithmatic operations.
     """
-    
+
     def __add__(self, v, /) -> Any:
         ...
-        
+
     def __sub__(self, v, /) -> Any:
         ...
-        
+
     def __mul__(self, v, /) -> Any:
         ...
-        
+
     def __div__(self, v, /) -> Any:
         ...
-        
-    def __eq__(self, v, /) -> Any: ...
-    def __ne__(self, v, /) -> Any: ...
-    def __le__(self, v, /) -> Any: ...
-    def __ge__(self, v, /) -> Any: ...
 
-class Subscripable(Protocol):
-    def __getitem__(self, index:int, /) -> Any: ...
+    def __eq__(self, v, /) -> Any:
+        ...
 
-class 
+    def __ne__(self, v, /) -> Any:
+        ...
+
+    def __le__(self, v, /) -> Any:
+        ...
+
+    def __ge__(self, v, /) -> Any:
+        ...
+
+
+class ReadableBySubscription(Protocol):
+    def __getitem__(self, index: int, /) -> Any:
+        ...
+
+
+class Subscripable(ReadableBySubscription, Protocol):
+    def __setitem__(self, index: int, /) -> Any:
+        ...
+
 
 @runtime_checkable
 class AddAndLength(Protocol):
@@ -54,8 +68,9 @@ from pyomo.environ import *
 
 model = ConcreteModel()
 
-model.x = cast(Subscripable,Var([0,1,2]))
-model.x:  # type: ignore
+model.x:ReadableBySubscription= Var([0, 1, 2])
+# model.x = cast(ReadableBySubscription, Var([0, 1, 2]))
+
 model.x[1]
 model.v = cast(ArithmaticType, Var())
 val = model.v * 1
