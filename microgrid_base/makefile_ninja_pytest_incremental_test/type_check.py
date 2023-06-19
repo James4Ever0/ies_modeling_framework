89b2888007ca9df7fea601043653cb76ac82fda3
jamesbrown <randomvoidmail@foxmail.com>
2023-06-19 19:58:01 +0800

    update

--- microgrid_base/makefile_ninja_pytest_incremental_test/type_check.py
+++ microgrid_base/makefile_ninja_pytest_incremental_test/type_check.py
@@ -1,14 +1,20 @@
 from typing import Protocol, Any
 
+
 class AddAndLength(Protocol):
-    def __add__(self, v: Any) -> Any: ...
-    def __len__(self) -> int: ...
+    def __add__(self, *args: Any) -> Any:
+        ...
+
+    def __len__(self) -> int:
+        ...
+
 
 def mfunc(param: AddAndLength) -> int:
     print("You don't care what I do")
     length = len(param)
-    print('LEN:', length)
+    print("LEN:", length)
     return length
 
+
 mfunc(123)
-mfunc(['i am list'])
+mfunc(["i am list"])
