65b2251420176612856b5740facf6784165fc462
jamesbrown <randomvoidmail@foxmail.com>
2023-06-27 13:32:01 +0800

    update

--- microgrid_base/makefile_ninja_pytest_incremental_test/lex_yacc.py
+++ microgrid_base/makefile_ninja_pytest_incremental_test/lex_yacc.py
@@ -4,6 +4,7 @@ class LexMeta(type):
         #unprocessed_tokens = []
         skipped_tokens = []
         for attrName, attr in cdict.items():
+            # for parser starting with "p_"
             assert not attrName.startswith("t_"), f"Wrong attribute name: {attrName}\nShall not start any attribute with 't_'!"
             #if attrName.startswith("t_"):
             #    unprocessed_tokens.append(attrName)
