0d46cf17678b41583271a225f0d35ac35e27c2c5
jamesbrown <randomvoidmail@foxmail.com>
2023-06-27 13:24:28 +0800

    update

---
+++ microgrid_base/makefile_ninja_pytest_incremental_test/lex_yacc.py
@@ -0,0 +1,27 @@
+class LexMeta(type):
+    def __new__(cls, name, bases, cdict):
+        tokens = []
+        #unprocessed_tokens = []
+        skipped_tokens = []
+        for attrName, attr in cdict.items():
+            assert not attrName.startswith("t_"), f"Wrong attribute name: {attrName}\nShall not start any attribute with 't_'!"
+            #if attrName.startswith("t_"):
+            #    unprocessed_tokens.append(attrName)
+            splitedAttrName = list(filter(lambda e: len(e) >0, attrName.split("_")))
+            if len(splitedAttrName)>0:
+                indicator = splitedAttrName[-1][0]
+                if indicator.upper() == indicator and indicator.lower() != indicator:
+                    tokens.append(attrName)
+            else:
+                skipped_tokens.append(attrName)
+        cdict['tokens'] = tokens
+        for token in tokens+skipped_tokens:
+            cdict[f"t_{token}"] = cdict.pop(token)
+        return super(LexMeta,cls).__new__(cls, name, bases, cdict)
+
+class sampleLex(metaclass = LexMeta):
+    ID = r"\w+" 
+
+myLex = sampleLex()
+print(dir(sampleLex))
+print(dir(myLex))
