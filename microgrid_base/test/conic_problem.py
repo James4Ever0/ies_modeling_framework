6699c3f529bf2c1672264e638d86bb43fffab12e
jamesbrown <randomvoidmail@foxmail.com>
2023-06-27 15:00:00 +0800

    update

--- microgrid_base/test/conic_problem.py
+++ microgrid_base/test/conic_problem.py
@@ -3,4 +3,13 @@ import numpy as np
 
 expr = x*y - (x+y)
 # help(expr.evalf)
-print(expr.evalf(subs = {x: 1, y:2}))
+x_min, x_max = 1,5
+y_min, y_max = 2,4
+
+
+min_val = 1000
+max_val = -1000
+for _x in np.linspace(x_min, x_max, 100):
+    for _y in np.linspace(y_min, y_max, 100):
+        val = expr.evalf(subs = {x: _x, y:_y})
+        if 
