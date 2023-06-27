87bd844560fb78b79fca1244066a647263f0c80b
jamesbrown <randomvoidmail@foxmail.com>
2023-06-27 15:02:00 +0800

    update

--- microgrid_base/test/conic_problem.py
+++ microgrid_base/test/conic_problem.py
@@ -9,7 +9,16 @@ y_min, y_max = 2,4
 
 min_val = 1000
 max_val = -1000
+min_inputs = []
+max_inputs = []
 for _x in np.linspace(x_min, x_max, 100):
     for _y in np.linspace(y_min, y_max, 100):
         val = expr.evalf(subs = {x: _x, y:_y})
-        if 
+        if val < min_val: min_val = val
+            min_inputs = [_x, _y]
+        if val > max_val:
+            max_val = val
+            max_inputs = [_x, _y]
+        
+
+print(f"MIN: {min_val}, MAX: {max_val}")
