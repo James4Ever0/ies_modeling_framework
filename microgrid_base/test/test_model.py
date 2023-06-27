c46ce89f6450a46a0120f55be3c6d5bd8cfc6ca4
jamesbrown <randomvoidmail@foxmail.com>
2023-06-27 14:32:00 +0800

    update

--- microgrid_base/test/test_model.py
+++ microgrid_base/test/test_model.py
@@ -223,15 +223,15 @@ def test_VarMultiplySingle(
     v1_dict = dict(var=v1, max=v1_max, min=v1_min)
 
     v0_v1 = 测试设备模型.Multiply(v0_dict, v1_dict, "v0_v1")
-    
-    model_wrapper.Objective(expr=v0_v1, sense=sense)
+    obj_expr = v0_v1[0]
+    model_wrapper.Objective(expr=obj_expr, sense=sense)
     
     with SolverFactory("cplex") as solver:
         print(">>>SOLVING<<<")
         s_results = solver.solve(model_wrapper.model, tee=True)
         print("SOLVER RESULTS?")
         print(s_results)
-        assert abs(value(v0_v1) - expected) <= EPS
+        assert abs(value(obj_expr) - expected) <= EPS
 
 
 def test_单表达式生成指示变量(
