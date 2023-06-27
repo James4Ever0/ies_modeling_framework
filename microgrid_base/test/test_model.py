e5314a4df90af5b42be8d20eed988330b4280e4b
jamesbrown <randomvoidmail@foxmail.com>
2023-06-27 14:42:01 +0800

    update

--- microgrid_base/test/test_model.py
+++ microgrid_base/test/test_model.py
@@ -214,7 +214,12 @@ def test_BinVarMultiplySingle(
 def test_VarMultiplySingle(
     model_wrapper: ModelWrapper,
     测试设备模型: 设备模型,
-    v0_min, v0_max, v1_min, v1_max, sense, expected,
+    v0_min,
+    v0_max,
+    v1_min,
+    v1_max,
+    sense,
+    expected,
 ):
     v0 = 测试设备模型.变量列表("v0", bounds=(v0_min, v0_max))
     v0_dict = dict(var=v0, max=v0_max, min=v0_min)
@@ -225,7 +230,7 @@ def test_VarMultiplySingle(
     v0_v1 = 测试设备模型.Multiply(v0_dict, v1_dict, "v0_v1")
     obj_expr = v0_v1[0]
     model_wrapper.Objective(expr=obj_expr, sense=sense)
-    
+
     with SolverFactory("cplex") as solver:
         print(">>>SOLVING<<<")
         s_results = solver.solve(model_wrapper.model, tee=True)
@@ -233,17 +238,33 @@ def test_VarMultiplySingle(
         print(s_results)
         assert abs(value(obj_expr) - expected) <= EPS
 
-@pytest.mark.parametrize("v0_min, v0_max, sense, expected_v0, expected_v1_b_pos, expected_v1_x_pos, expected_v1_b_neg, expected_v1_x_neg, expected_v1_x_abs", [])
+
+@pytest.mark.parametrize(
+    "v0_min, v0_max, sense, expected_v0, expected_v1_b_pos, expected_v1_x_pos, expected_v1_b_neg, expected_v1_x_neg, expected_v1_x_abs",
+    [
+        (-1, 5, maximize, 5, 1, 5, 0, 0, 5),
+        (-1, 5, minimize, -1, 0, 0, 1, 1, 1),
+        (-2, 5, minimize, -2, 0, 0, 1, 2, 2),
+    ],
+)
 def test_单表达式生成指示变量(
     model_wrapper: ModelWrapper,
     测试设备模型: 设备模型,
-    v0_min, v0_max, sense, expected_v0, expected_v1_b_pos, expected_v1_x_pos, expected_v1_b_neg, expected_v1_x_neg, expected_v1_x_abs
+    v0_min,
+    v0_max,
+    sense,
+    expected_v0,
+    expected_v1_b_pos,
+    expected_v1_x_pos,
+    expected_v1_b_neg,
+    expected_v1_x_neg,
+    expected_v1_x_abs,
 ):
     v0 = 测试设备模型.单变量("v0", bounds=(v0_min, v0_max))
     v1 = 测试设备模型.单表达式生成指示变量("v1", v0)
     obj_expr = v0
     model_wrapper.Objective(expr=obj_expr, sense=sense)
-    
+
     with SolverFactory("cplex") as solver:
         print(">>>SOLVING<<<")
         s_results = solver.solve(model_wrapper.model, tee=True)
@@ -254,11 +275,15 @@ def test_单表达式生成指示变量(
         assert abs(value(expected_v1_x_neg) - v1.x_neg) <= EPS
         assert abs(value(expected_v1_b_neg) - v1.b_neg) <= EPS
         assert abs(value(expected_v1_x_pos) - v1.x_pos) <= EPS
+        assert abs(value(expected_v1_x_abs) - v1.x_abs) <= EPS
+
 
 def test_Piecewise(
     model_wrapper: ModelWrapper,
-    测试设备模型: 设备模型,):
+    测试设备模型: 设备模型,
+):
     ...
+
 
 def test_柴油发电(model_wrapper: ModelWrapper):
     from ies_optim import 柴油发电模型, 柴油发电信息
