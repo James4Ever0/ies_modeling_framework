a3237f1897186681c202c55472151f22746c60ec
jamesbrown <randomvoidmail@foxmail.com>
2023-06-27 13:44:03 +0800

    update

--- microgrid_base/makefile_ninja_pytest_incremental_test/lex_yacc.py
+++ microgrid_base/makefile_ninja_pytest_incremental_test/lex_yacc.py
@@ -1,27 +1,37 @@
 class LexMeta(type):
     def __new__(cls, name, bases, cdict):
         tokens = []
-        #unprocessed_tokens = []
+        # unprocessed_tokens = []
         skipped_tokens = []
         for attrName, attr in cdict.items():
             # for parser starting with "p_"
-            assert not attrName.startswith("t_"), f"Wrong attribute name: {attrName}\nShall not start any attribute with 't_'!"
-            #if attrName.startswith("t_"):
+            assert not attrName.startswith(
+                "t_"
+            ), f"Wrong attribute name: {attrName}\nShall not start any attribute with 't_'!"
+            # if attrName.startswith("t_"):
             #    unprocessed_tokens.append(attrName)
-            splitedAttrName = list(filter(lambda e: len(e) >0, attrName.split("_")))
-            if len(splitedAttrName)>0:
+            splitedAttrName = list(filter(lambda e: len(e) > 0, attrName.split("_")))
+            if len(splitedAttrName) > 0:
                 indicator = splitedAttrName[-1][0]
                 if indicator.upper() == indicator and indicator.lower() != indicator:
                     tokens.append(attrName)
             else:
                 skipped_tokens.append(attrName)
-        cdict['tokens'] = tokens
-        for token in tokens+skipped_tokens:
+        cdict["tokens"] = tokens
+        for token in tokens + skipped_tokens:
             cdict[f"t_{token}"] = cdict.pop(token)
-        return super(LexMeta,cls).__new__(cls, name, bases, cdict)
+        return super(LexMeta, cls).__new__(cls, name, bases, cdict)
 
-class sampleLex(metaclass = LexMeta):
-    ID = r"\w+" 
+
+import ply.lex as lex
+
+
+class sampleLex(metaclass=LexMeta):
+    ID = r"\w+"
+
+    def build(self, **kwargs):
+        self.lexer = lex.lex(object=self, **kwargs)
+
 
 myLex = sampleLex()
 print(dir(sampleLex))
