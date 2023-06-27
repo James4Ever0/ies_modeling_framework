5e9a1d07538e3979a11f721614bd76422fa4677a
jamesbrown <randomvoidmail@foxmail.com>
2023-06-27 16:04:00 +0800

    update

--- microgrid_base/test/test_model.py
+++ microgrid_base/test/test_model.py
@@ -63,9 +63,9 @@ EPS = 0.03
 #     print("VAL METHOD?", t2f.method())
 
 
+from ies_optim import 柴油信息
 @fixture(scope="session")
 def 测试柴油信息():
-    from ies_optim import 柴油信息
 
     return 柴油信息(设备名称="柴油1", Price=(10, "元/L"), 热值=(10, "MJ/L"), CO2=(10, "kg/L"))
 from ies_optim import 柴油发电信息
@@ -93,9 +93,11 @@ def 测试柴油发电信息():
         MinDeviceCount=100,
     )
 
-
+from ies_optim import 柴油发电模型
 @fixture()
-def 测试柴油发电模型(测试柴油发电信息: 柴油发电信息)
+def 测试柴油发电模型(测试柴油发电信息: 柴油发电信息):
+    mDieselEngineModel = 柴油发电模型(PD = {}, )
+    return mDieselEngineModel
 
 @fixture(scope="session")
 def 测试计算参数():
