511250ff4939724929c7b024d563434efe065dcc
jamesbrown <randomvoidmail@foxmail.com>
2023-06-28 16:50:00 +0800

    update

--- microgrid_base/test_json_input_format.py
+++ microgrid_base/test_json_input_format.py
@@ -32,5 +32,7 @@ mDictList = EFG.dict()["mDictList"]
 calcParamList = mDictListToCalcParamList(mDictList)
 resultList = solveModelFromCalcParamList(calcParamList)
 # breakpoint()
+rich.print(resultList[0]['simulationResultTable'])
+
 with open("export_format_test.json", 'w') as f:
     f.write(json.dumps(resultList, indent=4, ensure_ascii=False))
