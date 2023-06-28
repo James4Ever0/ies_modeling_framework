bf2983ab34a7727f48d995bac656c73433a7c542
jamesbrown <randomvoidmail@foxmail.com>
2023-06-28 15:26:00 +0800

    update

--- microgrid_base/test_json_input_format.py
+++ microgrid_base/test_json_input_format.py
@@ -1,5 +1,6 @@
 
-input_path = "test_data.json"
+# input_path = "test_data.json"
+input_path = "test/test_topo.json"
 # input_path = "template_input.json"
 
 import json
@@ -19,9 +20,13 @@ EFG2 = EnergyFlowGraph.parse_obj(EFG_dict)
 
 import rich
 
+from microgrid_base.solve_model import solveModelFromCalcParamList
+
 for index, mDict in enumerate(EFG.mDictList):
     rich.print(mDict)
     print()
     print(f"_____parsing mDict #{index}_____")
     topo = 拓扑图.from_json(mDict.dict())
     topo.check_consistency()
+
+resultList = solveModelFromCalcParamList(calcParamList)
