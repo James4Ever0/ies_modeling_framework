7211086b85ee8bd8df7eb8c880a65ece18f1bc41
jamesbrown <randomvoidmail@foxmail.com>
2023-06-28 15:38:00 +0800

    update

--- microgrid_base/test_json_input_format.py
+++ microgrid_base/test_json_input_format.py
@@ -1,4 +1,3 @@
-
 # input_path = "test_data.json"
 input_path = "test/test_topo.json"
 # input_path = "template_input.json"
@@ -6,7 +5,7 @@ input_path = "test/test_topo.json"
 import json
 
 with open(input_path, "r") as f:
-    data =json.load(f)
+    data = json.load(f)
     # data = dict(mDictList=json.load(f))
 
 from ies_optim import EnergyFlowGraph
@@ -20,16 +19,16 @@ EFG2 = EnergyFlowGraph.parse_obj(EFG_dict)
 
 import rich
 
-from solve_model import (mDictListToCalcParamList,solveModelFromCalcParamList
-)
+from solve_model import mDictListToCalcParamList, solveModelFromCalcParamList
+
 for index, mDict in enumerate(EFG.mDictList):
     rich.print(mDict)
     print()
     print(f"_____parsing mDict #{index}_____")
     topo = 拓扑图.from_json(mDict.dict())
     topo.check_consistency()
-    
-mDictList =EFG.dict()['mDictList']
+
+mDictList = EFG.dict()["mDictList"]
 calcParamList = mDictListToCalcParamList(mDictList)
 resultList = solveModelFromCalcParamList(calcParamList)
 # breakpoint()
