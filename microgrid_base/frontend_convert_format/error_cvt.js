b57c81b88fa3b91ef1d00c4ca68210557fc097c3
jamesbrown <randomvoidmail@foxmail.com>
2023-06-29 16:18:00 +0800

    update

--- microgrid_base/frontend_convert_format/error_cvt.js
+++ microgrid_base/frontend_convert_format/error_cvt.js
@@ -1,14 +1,17 @@
-const error_mapping = {
-    "":['infeasible', 'abnormal'],"":[]
+const error_mapping={
+    "不满足负荷": ['infeasible'],"拓扑错误": ["相邻","节点"]
 }
-const reserved_error_msg = '其他错误'
-function convertErrorMsg(error_msg){
-    var splited_lines = error_msg.split("\n")
-    var error_last_line = splited_lines[splited_lines.length-1]
-    for (var k in error_mapping){
-        var vlist = error_mapping[k]
-        for (var v of vlist){
-            
+const reserved_error_msg='其他错误'
+
+function convertErrorMsg(error_msg) {
+    var splited_lines=error_msg.split("\n")
+    var error_last_line=splited_lines[splited_lines.length-1]
+    for(var k in error_mapping) {
+        var vlist=error_mapping[k]
+        for(var v of vlist) {
+            if(error_last_line.includes(v)) {
+                return k
+            }
         }
     }
     return reserved_error_msg
