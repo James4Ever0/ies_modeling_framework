574fd3107bb0f0c2d1a5a6e52130e64f1aa8e8ad
jamesbrown <randomvoidmail@foxmail.com>
2023-06-29 16:12:00 +0800

    update

--- microgrid_base/frontend_convert_format/error_cvt.js
+++ microgrid_base/frontend_convert_format/error_cvt.js
@@ -3,5 +3,8 @@ const error_mapping = {
 }
 const reserved_error_msg = '其他错误'
 function convertErrorMsg(error_msg){
-    var last_line = error_msg.split("\n")
+    var splited_lines = error_msg.split("\n")
+    var error_last_line = splited_lines[splited_lines.length-1]
+
+    return converted_error_msg
 }
