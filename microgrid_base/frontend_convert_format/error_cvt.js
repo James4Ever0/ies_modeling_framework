ff442c4608a3f46e4696efb1db766ada4248a24c
jamesbrown <randomvoidmail@foxmail.com>
2023-06-29 16:14:01 +0800

    update

--- microgrid_base/frontend_convert_format/error_cvt.js
+++ microgrid_base/frontend_convert_format/error_cvt.js
@@ -1,10 +1,15 @@
 const error_mapping = {
-    
+    "":['infeasible'],"":[]
 }
 const reserved_error_msg = '其他错误'
 function convertErrorMsg(error_msg){
     var splited_lines = error_msg.split("\n")
     var error_last_line = splited_lines[splited_lines.length-1]
-
-    return converted_error_msg
+    for (var k in error_mapping){
+        var vlist = error_mapping[k]
+        if (vlist.indexOf(error_msg) != -1){
+            return k
+        }
+    }
+    return reserved_error_msg
 }
