a09e6460918cdc81edbec4c5783fd39ba6aaf099
jamesbrown <randomvoidmail@foxmail.com>
2023-06-29 18:10:44 +0800

    update

--- microgrid_base/test/dev_info_tmp_gen.py
+++ microgrid_base/test/dev_info_tmp_gen.py
@@ -9,5 +9,6 @@ from ies_optim import *
 for k, v in globals():
     # print(k)
     # print(k.__annotations__)
-    if k.endswith("信息") and not k.startswith("设备"):
+    if k.endswith("信息") and (not k.startswith("设备")):
+        ...
     # class/methods might have distinct annotations inside.
