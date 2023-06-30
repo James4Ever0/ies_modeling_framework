4443fbd1d2c21e9db43d832ce6d4c700ff67fb5d
jamesbrown <randomvoidmail@foxmail.com>
2023-06-30 09:18:01 +0800

    update

--- microgrid_base/test/dev_info_tmp_gen.py
+++ microgrid_base/test/dev_info_tmp_gen.py
@@ -11,10 +11,9 @@ for k, v in globals():
     # print(k.__annotations__)
     if k.endswith("设备名称") and (not k.startswith("设备")):
         if issubclass(v, 设备信息):
-            
             # 意味着有公共内容
-            devName = k.strip(""
-            设备名称
-            生产厂商 = ""
-            设备型号 = "{}1"
+            devName = k.strip("信息")
+            设备名称 = devName,
+            生产厂商 = "",
+            设备型号 = f"{devName}1"
     # class/methods might have distinct annotations inside.
