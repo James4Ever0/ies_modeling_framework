95365d85da4bcc53b941c02fdcc8c41c834abc25
jamesbrown <randomvoidmail@foxmail.com>
2023-06-30 09:16:01 +0800

    update

--- microgrid_base/test/dev_info_tmp_gen.py
+++ microgrid_base/test/dev_info_tmp_gen.py
@@ -9,7 +9,12 @@ from ies_optim import *
 for k, v in globals():
     # print(k)
     # print(k.__annotations__)
-    if k.endswith("信息") and (not k.startswith("设备")):
+    if k.endswith("设备名称") and (not k.startswith("设备")):
         if issubclass(v, 设备信息):
-            # 意味着需要填写一些内容
+            
+            # 意味着有公共内容
+            devName = k.strip(""
+            设备名称
+            生产厂商 = ""
+            设备型号 = "{}1"
     # class/methods might have distinct annotations inside.
