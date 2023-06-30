cb68bdd01ba1535a13344b3f6244647efb266a44
jamesbrown <randomvoidmail@foxmail.com>
2023-06-30 10:52:01 +0800

    update

--- microgrid_base/log_infeasible.py
+++ microgrid_base/log_infeasible.py
@@ -1,4 +1,12 @@
-from pyomo.environ import Param, ConcreteModel, Var, Objective, ConstraintList, value, minimize
+from pyomo.environ import (
+    Param,
+    ConcreteModel,
+    Var,
+    Objective,
+    ConstraintList,
+    value,
+    minimize,
+)
 from pyomo.opt import SolverFactory
 from pyomo.util.infeasible import log_infeasible_constraints
 import logging
@@ -8,8 +16,10 @@ m.LE = set([1, 2, 3])
 m.x = Var(m.LE, initialize=0)
 m.M = Param(initialize=1000000)
 
+
 def obj_rule(m):
-        return sum(m.x[i] * 1 for i in m.LE)
+    return sum(m.x[i] * 1 for i in m.LE)
+
 
 m.z = Objective(rule=obj_rule, sense=minimize)
 m.cons1 = ConstraintList()
@@ -18,8 +28,10 @@ for i in m.LE:
     m.cons1.add(10**2 * m.x[i] >= m.M)
     m.cons1.add(10**2 * m.x[i] <= -3)
 
-solver = SolverFactory('glpk')
+solver = SolverFactory("cplex")
+# solver = SolverFactory("glpk")
 solution = solver.solve(m, tee=False)
 log_infeasible_constraints(m, log_expression=True, log_variables=True)
-logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.INFO)
+logging.basicConfig(filename="example.log", encoding="utf-8", level=logging.INFO)
+# logging.basicConfig(filename="example.log", encoding="utf-8", level=logging.INFO)
 print(value(m.z))
