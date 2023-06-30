34023b14c4cd8b6576c64a5f7d3c86f6272019e0
jamesbrown <randomvoidmail@foxmail.com>
2023-06-30 11:36:00 +0800

    update

--- microgrid_base/log_infeasible.py
+++ microgrid_base/log_infeasible.py
@@ -28,15 +28,18 @@ for i in m.LE:
     m.cons1.add(10**2 * m.x[i] >= m.M)
     m.cons1.add(10**2 * m.x[i] <= -3)
 
-solver = SolverFactory("cplex")
-# solver = SolverFactory("glpk")
-solution = solver.solve(m, tee=False)
-log_infeasible_constraints(m, log_expression=True, log_variables=True)
 import io
 mstream = io.StringIO()
-import sys
-logging.basicConfig(stream=sys.stderr, level=logging.INFO)
-# logging.basicConfig(stream=mstream, level=logging.INFO)
+
+# import sys
+# logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+logging.basicConfig(stream=mstream, level=logging.INFO)
+
+solver = SolverFactory("cplex")
+# solver = SolverFactory("glpk")
+solution = solver.solve(m, tee=True)
+log_infeasible_constraints(m, log_expression=True, log_variables=True)
+
 # logging.basicConfig(filename="example.log", encoding="utf-8", level=logging.INFO)
 print(value(m.z))
 
