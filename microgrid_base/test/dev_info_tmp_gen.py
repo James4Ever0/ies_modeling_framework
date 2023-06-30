cf462b841662455d41bf822338025b34dbc54a76
jamesbrown <randomvoidmail@foxmail.com>
2023-06-30 09:14:00 +0800

    update

--- microgrid_base/test/dev_info_tmp_gen.py
+++ microgrid_base/test/dev_info_tmp_gen.py
@@ -10,5 +10,6 @@ for k, v in globals():
     # print(k)
     # print(k.__annotations__)
     if k.endswith("信息") and (not k.startswith("设备")):
-        ...
+        if issubclass(v, 设备信息):
+            # 意味着需要填写一些内容
     # class/methods might have distinct annotations inside.
