ac38e1cf195cf55417bf828ab20f2a0be82e8359
jamesbrown <randomvoidmail@foxmail.com>
2023-06-30 10:16:01 +0800

    update

--- microgrid_base/test/dev_info_tmp_gen.py
+++ microgrid_base/test/dev_info_tmp_gen.py
@@ -10,9 +10,11 @@ from ies_optim import *
 for k, v in globals():
     # print(k)
     # print(k.__annotations__)
-    if k.endswith("设备名称") and (not k.startswith("设备")):
-        if issubclass(v, 设备信息):
-            # 意味着有公共内容
+    if k.endswith("信息") and (not k.startswith("设备")):
+        if issubclass(v, 设备基础信息):
             devName = k.strip("信息")
-            commonParams = dict(设备名称=devName, 生产厂商="Any", 设备型号=f"{devName}1")
+            commonParams = dict(设备名称=devName)
+            if issubclass(v, 设备信息):
+                # 意味着有公共内容
+                commonParams.update(生产厂商="Any", 设备型号=f"{devName}1")
     # class/methods might have distinct annotations inside.
