40a958f3c5270b8d7b420a276079adc4349f7aff
jamesbrown <randomvoidmail@foxmail.com>
2023-07-01 23:38:01 +0800

    update

--- microgrid_base/arbitrary_topology_generation.py
+++ microgrid_base/arbitrary_topology_generation.py
@@ -1,38 +1,42 @@
-import networkx as nx
-from aco_tsp import ACO, Graph
+# import networkx as nx
+# from aco_tsp import ACO, Graph
 
-# define the set of nodes/devices
-devices = ['Diesel source', 'Diesel generator', 'Solar power generator', 'Wind power generator', 'Power converter', 'Power lines', 'Power load']
+# # define the set of nodes/devices
+# devices = ['Diesel source', 'Diesel generator', 'Solar power generator', 'Wind power generator', 'Power converter', 'Power lines', 'Power load']
 
-# define the set of rules
-rules = {
-    'Diesel source': ['Diesel generator'],
-    'Diesel generator': ['Power lines'],
-    'Solar power generator': ['Power lines'],
-    'Wind power generator': ['Power lines'],
-    'Power converter': ['Power lines', 'Power load'],
-    'Power lines': ['Diesel generator', 'Solar power generator', 'Wind power generator', 'Power converter'],
-    'Power load': ['Power converter']
-}
+# # define the set of rules
+# rules = {
+#     'Diesel source': ['Diesel generator'],
+#     'Diesel generator': ['Power lines'],
+#     'Solar power generator': ['Power lines'],
+#     'Wind power generator': ['Power lines'],
+#     'Power converter': ['Power lines', 'Power load'],
+#     'Power lines': ['Diesel generator', 'Solar power generator', 'Wind power generator', 'Power converter'],
+#     'Power load': ['Power converter']
+# }
 
-# define the graph
-G = Graph(len(devices), devices)
+# # define the graph
+# G = Graph(len(devices), devices)
 
-# add edges based on the rules
-for i in range(len(devices)):
-    for j in range(i+1, len(devices)):
-        if devices[j] in rules[devices[i]]:
-            G.add_edge(i, j, 1)  # add an edge with cost 1 if the nodes can be connected
+# # add edges based on the rules
+# for i in range(len(devices)):
+#     for j in range(i+1, len(devices)):
+#         if devices[j] in rules[devices[i]]:
+#             G.add_edge(i, j, 1)  # add an edge with cost 1 if the nodes can be connected
 
-# define the ACO parameters
-aco = ACO(ants=10, generations=100, alpha=1.0, beta=10.0, rho=0.5, q=1.0)
+# # define the ACO parameters
+# aco = ACO(ants=10, generations=100, alpha=1.0, beta=10.0, rho=0.5, q=1.0)
 
-# run the ACO algorithm to generate the graph
-path, cost = aco.solve(G)
+# # run the ACO algorithm to generate the graph
+# path, cost = aco.solve(G)
 
-# convert the path to a NetworkX graph
-edges = [(devices[path[i]], devices[path[i+1]]) for i in range(len(path)-1)]
-G_nx = nx.Graph(edges)
+# # convert the path to a NetworkX graph
+# edges = [(devices[path[i]], devices[path[i+1]]) for i in range(len(path)-1)]
+# G_nx = nx.Graph(edges)
 
-# visualize the graph
-nx.draw(G_nx, with_labels=True)
+# # visualize the graph
+# nx.draw(G_nx, with_labels=True)
+
+# above code does not seem to be right.
+# the library is fictional.
+# TSP is not of our type of problem.
