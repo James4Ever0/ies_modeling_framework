fd0fe22b689aac9271a6a10c75bc3216d5d9921f
jamesbrown <randomvoidmail@foxmail.com>
2023-07-03 17:26:01 +0800

    update

--- microgrid_base/test/generate_test_model.py
+++ microgrid_base/test/generate_test_model.py
@@ -1,7 +1,10 @@
 import sys
+
 sys.path.append("../")
 import jinja_utils
 
 code_path, template_path = jinja_utils.code_and_template_path("test_model")
 
-jinja_utils.load_render_and_format(template_path, code_path, render_params=dict(), banner = "TEST MODEL CODE")
+jinja_utils.load_render_and_format(
+    template_path, code_path, render_params=dict(), banner="TEST MODEL CODE"
+)
