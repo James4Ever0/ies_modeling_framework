704e83920cb0575ffafe0cdf052ee7480aeea09a
jamesbrown <randomvoidmail@foxmail.com>
2023-07-03 10:38:00 +0800

    update

--- microgrid_base/test/dev_info_tmp_gen.py
+++ microgrid_base/test/dev_info_tmp_gen.py
@@ -7,18 +7,21 @@ sys.path.append("../")
 
 import ies_optim
 import inspect
-import copy
-ies_optim.
-for k, v in mglobals.items():
+# import copy
+for k, v in ies_optim.__dict__.items():
+# for k, v in mglobals.items():
     # print(k)
     # print(k.__annotations__)
     if k.endswith("信息") and (not k.startswith("设备")):
-        if issubclass(v, 设备基础信息):
+        if issubclass(v, ies_optim.设备基础信息):
             devName = k.strip("信息")
             commonParams = dict(设备名称=devName)
-            if issubclass(v, 设备信息):
+            if issubclass(v, ies_optim.设备信息):
                 # 意味着有公共内容
                 commonParams.update(生产厂商="Any", 设备型号=f"{devName}1")
             sig = inspect.signature(v)
-            print(sig)
+            # print(sig)
+            # breakpoint()
+            for sigkey in sig.parameters.keys():
+                print(sigkey)
     # class/methods might have distinct annotations inside.
