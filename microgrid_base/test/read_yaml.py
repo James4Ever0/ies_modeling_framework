aff8fb3e6aabc7c2f96249c75f6822e7c0766cb1
jamesbrown <randomvoidmail@foxmail.com>
2023-07-03 10:52:00 +0800

    update

--- microgrid_base/test/read_yaml.py
+++ microgrid_base/test/read_yaml.py
@@ -4,6 +4,14 @@ fpath = "test_config.yaml"
 import rich
 
 with open(fpath, "r") as file:
-    data = yaml.safe_load(file)
-    rich.print(data)
+    # data = yaml.safe_load(file)
+    # rich.print(data)
     # {'db': {'abc': 'def', 'mykey': None, 'myotherkey': None}}
+    #################################
+    #     {
+    #     'db': {
+    #         'abc': 'def',
+    #         'mykey': None,
+    #         'myotherkey': [None, None]
+    #     }
+    # }
