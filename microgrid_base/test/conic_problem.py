db7b96e1d9b46382a606cb8b535a717a8bab3c3c
jamesbrown <randomvoidmail@foxmail.com>
2023-07-03 15:14:00 +0800

    update

--- microgrid_base/test/conic_problem.py
+++ microgrid_base/test/conic_problem.py
@@ -1,10 +1,10 @@
 from sympy.abc import x, y
 import numpy as np
 
-expr = x*y -2*(x+y)
+expr = x * y - 2 * (x + y)
 # help(expr.evalf)
-x_min, x_max = -1,3
-y_min, y_max = -2,4
+x_min, x_max = -1, 3
+y_min, y_max = -2, 4
 
 
 min_val = 1000
@@ -13,14 +13,14 @@ min_inputs = []
 max_inputs = []
 for _x in np.linspace(x_min, x_max, 100):
     for _y in np.linspace(y_min, y_max, 100):
-        val = expr.evalf(subs = {x: _x, y:_y})
+        val = expr.evalf(subs={x: _x, y: _y})
         if val < min_val:
             min_val = val
             min_inputs = [_x, _y]
         if val > max_val:
             max_val = val
             max_inputs = [_x, _y]
-        
+
 
 print(f"MIN: {min_val}, MAX: {max_val}")
 print(f"MIN_INPUT: {min_inputs}, MAX_INPUT: {max_inputs}")
