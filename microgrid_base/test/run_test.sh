d76e2960447ddcfabcb1edf2de1d49c1626f3e6d
jamesbrown <randomvoidmail@foxmail.com>
2023-07-05 12:34:00 +0800

    update

--- microgrid_base/test/run_test.sh
+++ microgrid_base/test/run_test.sh
@@ -1,6 +1,7 @@
 # env PATH="/Applications/CPLEX_Studio1210/cplex/bin/x86-64_osx:$PATH" conda run -n rosetta --live-stream --no-capture-output python -m pytest --lf --lfnf=all --capture=tee-sys .
 
-env PATH="/Applications/CPLEX_Studio1210/cplex/bin/x86-64_osx:$PATH" pytest --lf --lfnf=all --capture=tee-sys test_model.py
+env PATH="/Applications/CPLEX_Studio1210/cplex/bin/x86-64_osx:$PATH" conda run -n base --no-capture-output --live-stream pytest --lf --lfnf=all --capture=tee-sys test_model.py
+# env PATH="/Applications/CPLEX_Studio1210/cplex/bin/x86-64_osx:$PATH" pytest --lf --lfnf=all --capture=tee-sys test_model.py
 # env PATH="/Applications/CPLEX_Studio1210/cplex/bin/x86-64_osx:$PATH" pytest --lf --lfnf=all --capture=tee-sys .
 
 # env PATH="/Applications/CPLEX_Studio1210/cplex/bin/x86-64_osx:$PATH" pytest --lf --lfnf=all .
