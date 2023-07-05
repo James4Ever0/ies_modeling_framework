6bf89bbc2fa632a346e75e590295764855b7700a
jamesbrown <randomvoidmail@foxmail.com>
2023-07-05 10:08:00 +0800

    update

--- microgrid_base/test/runtime_override_stepwise.py
+++ microgrid_base/test/runtime_override_stepwise.py
@@ -1,4 +1,7 @@
-def dec(f): return f
+def dec(f):
+    return f
+
+
 class MyClass:
     @dec
     def myfunc(self):
@@ -7,7 +10,7 @@ class MyClass:
         # mycomment
         assert False, "you cannot pass"
         print("hjk")
-        yield "myflag" # you may yield flag.
+        yield "myflag"  # you may yield flag.
         return "abc"
 
 
@@ -18,13 +21,14 @@ import ast
 import astor
 import re
 
+
 def overwrite_func(func):
     # get definition and return a new func.
     # test: add "yield" after every line.
     # func_ast = astor.code_to_ast(func)
     # print(func_ast)
     # deprecated?
-    
+
     func_source = inspect.getsource(func)
     # return new_func
     find_def = r"^( +)def"
@@ -32,7 +36,7 @@ def overwrite_func(func):
     strip_blanks = FDRegex.findall(func_source)[0]
     blank_count = len(strip_blanks)
     # print("BLANK COUNT:", blank_count) # BLANK COUNT: 4
-    indent_replace = r"^ "+("{%d}" % blank_count)
+    indent_replace = r"^ " + ("{%d}" % blank_count)
     # print(repr(indent_replace))
     IRRegex = re.compile(indent_replace, flags=re.MULTILINE)
     func_source_cleaned = IRRegex.sub("", func_source)
@@ -40,25 +44,28 @@ def overwrite_func(func):
     print(func_source_cleaned)
     print()
     func_ast = ast.parse(func_source_cleaned, type_comments=True)
-    print(func_ast) # unexpected indent, if not cleaned.
-    print(func_ast.body) # [<_ast.FunctionDef object at 0x1048a1a00>]
+    print(func_ast)  # unexpected indent, if not cleaned.
+    print(func_ast.body)  # [<_ast.FunctionDef object at 0x1048a1a00>]
     # for cn in ast.iter_child_nodes(func_ast):
     #     print(cn)
     funcdef = func_ast.body[0]
-    print(funcdef.body) # no comment?
+    print(funcdef.body)  # no comment?
     print(dir(funcdef))
-    print(funcdef.decorator_list) # [<_ast.Name object at 0x103081b50>]
+    print(funcdef.decorator_list)  # [<_ast.Name object at 0x103081b50>]
     # [<_ast.Expr object at 0x105359550>, <_ast.Expr object at 0x105368100>, <_ast.Assert object at 0x105395790>, <_ast.Expr object at 0x105395a60>]
+
 
 # c.myfunc()
 
 # new_func = overwrite_func(c.myfunc)
 # c.__setattr__("new_func", new_func)
 from types import MethodType
-c.__setattr__("__locals__", lambda self: locals())
-c.__setattr__("__globals__", lambda self: globals())
-c_locals = c.__locals__()
-c_globals = c.__globals__()
+
+c.locals = MethodType(lambda self: locals(), c)
+# c.__setattr__("__locals__", lambda self: locals())
+# c.__setattr__("__globals__", lambda self: globals())
+c_locals = c.locals()
+# c_globals = c.__globals__()
 
 print(c_locals)
-print(c_globals)
+# print(c_globals)
