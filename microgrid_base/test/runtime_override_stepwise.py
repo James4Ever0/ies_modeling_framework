77100db31298ac51d89e1aff49250f0e5cdbe244
jamesbrown <randomvoidmail@foxmail.com>
2023-07-05 09:18:00 +0800

    update

--- microgrid_base/test/runtime_override_stepwise.py
+++ microgrid_base/test/runtime_override_stepwise.py
@@ -9,7 +9,7 @@ class MyClass:
 c = MyClass()
 
 import inspect
-
+import ast
 
 def overwrite_func(func):
     # get definition and return a new func.
@@ -18,10 +18,12 @@ def overwrite_func(func):
     print("SOURCE CODE".center(70, "="))
     print(func_source)
     print()
-    return new_func
+    # return new_func
+    func_ast = ast.parse(func_source)
+    print(func_ast) # unexpected indent.
 
 
-c.myfunc()
+# c.myfunc()
 
 new_func = overwrite_func(c.myfunc)
 c.myfunc = new_func
