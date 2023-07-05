cfee3f3f71f7e5b1a368df90f25c5e9f337b2902
jamesbrown <randomvoidmail@foxmail.com>
2023-07-05 09:14:00 +0800

    update

--- microgrid_base/test/runtime_override_stepwise.py
+++ microgrid_base/test/runtime_override_stepwise.py
@@ -1,1 +1,14 @@
-class 
+class MyClass:
+    def myfunc(self):
+        print('abc')
+        print('def')
+        assert False, "you cannot pass"
+        print("hjk")
+
+c = MyClass()
+
+def overwrite_func(func):
+    # get definition and return a new func.
+    return new_func
+
+c.myfunc()
