278a96c9acc94c0366877bfa6b08e4e3319eb042
jamesbrown <randomvoidmail@foxmail.com>
2023-07-05 09:16:01 +0800

    update

--- microgrid_base/test/runtime_override_stepwise.py
+++ microgrid_base/test/runtime_override_stepwise.py
@@ -1,14 +1,27 @@
 class MyClass:
     def myfunc(self):
-        print('abc')
-        print('def')
+        print("abc")
+        print("def")
         assert False, "you cannot pass"
         print("hjk")
 
+
 c = MyClass()
+
+import inspect
+
 
 def overwrite_func(func):
     # get definition and return a new func.
+    # test: add "yield" after every line.
+    func_source = inspect.getsource(func)
+    print("SOURCE CODE".center(70, "="))
+    print(func_source)
+    print()
     return new_func
 
+
 c.myfunc()
+
+new_func = overwrite_func(c.myfunc)
+c.myfunc = new_func
