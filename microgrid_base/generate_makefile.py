950d78c18e632b05966ccfc84384a5d17dd0a2cf
jamesbrown <randomvoidmail@foxmail.com>
2023-07-08 17:54:00 +0800

    update

--- microgrid_base/generate_makefile.py
+++ microgrid_base/generate_makefile.py
@@ -1,6 +1,13 @@
 import os
 
 generate_path = "Makefile.tmp"
+import ast
+
+def read_file(fname):
+    with open(fname, 'r') as f:
+        content = f.read()
+        return content
 
 for fname in os.listdir("."):
-    
+    if fname.endswith(".py"):
+        content = read_file(fname)
