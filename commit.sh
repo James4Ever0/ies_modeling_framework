e81f24af7996dcf15355ce35ba9079bf075ebaae
GitJournal <app@gitjournal.io>
2023-07-09 10:29:01 +0800

    update

--- commit.sh
+++ commit.sh
@@ -1,5 +1,5 @@
 git pull
 git add .
 git commit -m 'update'
-git push origin main
-env http_proxy="" https_proxy="" git push origin_devops main
+git push origin_devops main
+env http_proxy="" https_proxy="" git push origin main
