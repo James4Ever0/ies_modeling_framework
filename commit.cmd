722abf48fa0385355236489535fcee4aa01f72fe
you <you@example.com>
2023-07-10 10:51:57 +0800

    'update'

--- commit.cmd
+++ commit.cmd
@@ -1,8 +1,14 @@
-D:
-cd D:\project\xianxing
-date /T
-time /T
+@REM D:
+@REM cd D:\project\xianxing
+@REM date /T
+@REM time /T
+@REM git pull
+@REM git add .
+@REM git commit -m 'update'
+@REM git push
+git config --global --add safe.directory E:/works/jubilant-adventure2
 git pull
 git add .
 git commit -m 'update'
-git push
+git push origin_devops main
+git push origin main
