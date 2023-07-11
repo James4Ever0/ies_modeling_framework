d65bf7c2ccb52f8d112f1146291b5a617cbe0178
jamesbrown <randomvoidmail@foxmail.com>
2023-07-11 14:58:00 +0800

    update

--- microgrid_base/solve_model.py
+++ microgrid_base/solve_model.py
@@ -294,9 +294,10 @@ def solveModelFromCalcParamList(
             for index, devInstDict in enumerate(ret.devInstDictList):
                 graph_data = ret.graph_data_list[index]
                 典型日代表的日期 = graph_data["典型日代表的日期"]
-                timeParam = 24 * len(典型日代表的日期) if 典型日 else (8760 if 计算步长 == "小时" else 2)
-                # TODO: fix inconsistent timeParam.
-                timeParam /= 8760
+                timeParam = ret.timeParamList[index]
+                # timeParam = 24 * len(典型日代表的日期) if 典型日 else (8760 if 计算步长 == "小时" else 2)
+                # # TODO: fix inconsistent timeParam.
+                # timeParam /= 8760
                 for devId, devInst in devInstDict.items():
                     devClassName = devInst.__class__.__name__.strip("模型")
                     # where you convert the units.
