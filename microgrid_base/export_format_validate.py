447d871208ed0f92fc430a806b24240e3d826543
jamesbrown <randomvoidmail@foxmail.com>
2023-07-11 13:52:00 +0800

    update

--- microgrid_base/export_format_validate.py
+++ microgrid_base/export_format_validate.py
@@ -70,7 +70,6 @@ class 柴油仿真结果(BaseModel):
 
     @staticmethod
     def export(model: 柴油模型, timeParam: float):
-
         return 柴油仿真结果(
             元件名称=safeAbs(model.设备信息.设备名称),
             柴油消耗费用=safeAbs(((value(model.总成本年化)) * timeParam)),
@@ -93,7 +92,6 @@ class 电负荷仿真结果(BaseModel):
 
     @staticmethod
     def export(model: 电负荷模型, timeParam: float):
-
         return 电负荷仿真结果(
             元件名称=safeAbs(model.设备信息.设备名称),
             电负荷=safeAbs(((statistics.mean(model.设备信息.EnergyConsumption)) * timeParam)),
@@ -124,7 +122,6 @@ class 光伏发电仿真结果(BaseModel):
 
     @staticmethod
     def export(model: 光伏发电模型, timeParam: float):
-
         return 光伏发电仿真结果(
             元件名称=safeAbs(model.设备信息.设备名称),
             设备型号=safeAbs(model.设备信息.设备型号),
@@ -161,7 +158,6 @@ class 风力发电仿真结果(BaseModel):
 
     @staticmethod
     def export(model: 风力发电模型, timeParam: float):
-
         return 风力发电仿真结果(
             元件名称=safeAbs(model.设备信息.设备名称),
             设备型号=safeAbs(model.设备信息.设备型号),
@@ -212,7 +208,6 @@ class 柴油发电仿真结果(BaseModel):
 
     @staticmethod
     def export(model: 柴油发电模型, timeParam: float):
-
         return 柴油发电仿真结果(
             元件名称=safeAbs(model.设备信息.设备名称),
             设备型号=safeAbs(model.设备信息.设备型号),
@@ -267,7 +262,6 @@ class 锂电池仿真结果(BaseModel):
 
     @staticmethod
     def export(model: 锂电池模型, timeParam: float):
-
         return 锂电池仿真结果(
             元件名称=safeAbs(model.设备信息.设备名称),
             设备型号=safeAbs(model.设备信息.设备型号),
@@ -278,22 +272,13 @@ class 锂电池仿真结果(BaseModel):
             平均效率_平均COP=safeAbs(
                 safeDiv(
                     ReLU(
-                        (
-                            (
-                                (statistics.mean([ReLU(e) for e in model.电接口.values()]))
-                                * timeParam
-                            )
-                        )
+                        (((statistics.mean([ReLU(e) for e in model.电接口])) * timeParam))
                         - (model.InitSOC * model.TotalCapacity)
                     ),
                     (
                         -(
                             (
-                                (
-                                    statistics.mean(
-                                        [-ReLU(-e) for e in model.电接口.values()]
-                                    )
-                                )
+                                (statistics.mean([-ReLU(-e) for e in model.电接口]))
                                 * timeParam
                             )
                         )
@@ -326,7 +311,6 @@ class 变压器仿真结果(BaseModel):
 
     @staticmethod
     def export(model: 变压器模型, timeParam: float):
-
         return 变压器仿真结果(
             元件名称=safeAbs(model.设备信息.设备名称),
             设备型号=safeAbs(model.设备信息.设备型号),
@@ -366,7 +350,6 @@ class 变流器仿真结果(BaseModel):
 
     @staticmethod
     def export(model: 变流器模型, timeParam: float):
-
         return 变流器仿真结果(
             元件名称=safeAbs(model.设备信息.设备名称),
             设备型号=safeAbs(model.设备信息.设备型号),
@@ -406,7 +389,6 @@ class 双向变流器仿真结果(BaseModel):
 
     @staticmethod
     def export(model: 双向变流器模型, timeParam: float):
-
         return 双向变流器仿真结果(
             元件名称=safeAbs(model.设备信息.设备名称),
             设备型号=safeAbs(model.设备信息.设备型号),
@@ -457,7 +439,6 @@ class 传输线仿真结果(BaseModel):
 
     @staticmethod
     def export(model: 传输线模型, timeParam: float):
-
         return 传输线仿真结果(
             元件名称=safeAbs(model.设备信息.设备名称),
             设备型号=safeAbs(model.设备信息.设备型号),
@@ -495,7 +476,6 @@ class 光伏发电出力曲线(BaseModel):
 
     @staticmethod
     def export(model: 光伏发电模型, timeParam: float):
-
         return 光伏发电出力曲线(
             时间=list(range(model.计算参数.迭代步数)),
             元件名称=model.设备信息.设备名称,
@@ -519,7 +499,6 @@ class 风力发电出力曲线(BaseModel):
 
     @staticmethod
     def export(model: 风力发电模型, timeParam: float):
-
         return 风力发电出力曲线(
             时间=list(range(model.计算参数.迭代步数)),
             元件名称=model.设备信息.设备名称,
@@ -543,7 +522,6 @@ class 柴油发电出力曲线(BaseModel):
 
     @staticmethod
     def export(model: 柴油发电模型, timeParam: float):
-
         return 柴油发电出力曲线(
             时间=list(range(model.计算参数.迭代步数)),
             元件名称=model.设备信息.设备名称,
@@ -581,7 +559,6 @@ class 锂电池出力曲线(BaseModel):
 
     @staticmethod
     def export(model: 锂电池模型, timeParam: float):
-
         return 锂电池出力曲线(
             时间=list(range(model.计算参数.迭代步数)),
             元件名称=model.设备信息.设备名称,
@@ -610,7 +587,6 @@ class 变压器出力曲线(BaseModel):
 
     @staticmethod
     def export(model: 变压器模型, timeParam: float):
-
         return 变压器出力曲线(
             时间=list(range(model.计算参数.迭代步数)),
             元件名称=model.设备信息.设备名称,
@@ -634,7 +610,6 @@ class 变流器出力曲线(BaseModel):
 
     @staticmethod
     def export(model: 变流器模型, timeParam: float):
-
         return 变流器出力曲线(
             时间=list(range(model.计算参数.迭代步数)),
             元件名称=model.设备信息.设备名称,
@@ -658,7 +633,6 @@ class 双向变流器出力曲线(BaseModel):
 
     @staticmethod
     def export(model: 双向变流器模型, timeParam: float):
-
         return 双向变流器出力曲线(
             时间=list(range(model.计算参数.迭代步数)),
             元件名称=model.设备信息.设备名称,
