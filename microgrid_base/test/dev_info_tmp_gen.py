75462b25648a64d24dbaea3b21934968e1e1d338
jamesbrown <randomvoidmail@foxmail.com>
2023-07-15 00:08:01 +0800

    update

--- microgrid_base/test/dev_info_tmp_gen.py
+++ microgrid_base/test/dev_info_tmp_gen.py
@@ -78,3 +78,10 @@ jinja_utils.load_render_and_format(
     ),
     banner := "TEST FIXTURES",
 )
+
+
+code_path, template_path = jinja_utils.code_and_template_path("test_export")
+jinja_utils.load_render_and_format(
+    template_path,
+    code_path,
+    render_params = dict(devNames = render_params['data']), "TEST EXPORT")
