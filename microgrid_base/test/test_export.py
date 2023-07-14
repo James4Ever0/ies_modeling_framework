21120697ac403181d39671f77e58ccfe6b5660f1
jamesbrown <randomvoidmail@foxmail.com>
2023-07-15 00:16:01 +0800

    update

--- microgrid_base/test/test_export.py
+++ microgrid_base/test/test_export.py
@@ -4,9 +4,11 @@ from export_format_validate import *
 {% for devName in devNames %}
     {% set testModel = "测试{}模型".format(devName) %}
 def test_{{devName}}_export({{testModel}}):
+    timeParam = 10
     [% if devName == "柴油发电"%}
         {{testModel}}.热值 = 1
     {% endif %}
     {{testModel}}.constraints_register()
-    仿真结果
-    出力曲线
+    for attrName in ['{{devName}}仿真结果', '{{devName}}出力曲线']:
+        if obj:=globals().get(attrName, None):
+            obj.export(, timeParam)
