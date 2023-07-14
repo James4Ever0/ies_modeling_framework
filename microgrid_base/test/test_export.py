9f5a935d96ae8a0d99c2927bca4090d08e9fec59
jamesbrown <randomvoidmail@foxmail.com>
2023-07-15 00:12:00 +0800

    update

--- microgrid_base/test/test_export.py
+++ microgrid_base/test/test_export.py
@@ -2,4 +2,8 @@ from common_fixtures import *
 from export_format_validate import *
 
 {% for devName in devNames %}
-def test_{{devName}}_export({{}})
+    {% set testModel = "测试{}模型".format(devName) %}
+def test_{{devName}}_export({{testModel}}):
+    if {{testModel}}
+    {{testModel}}.constraints_register()
+    
