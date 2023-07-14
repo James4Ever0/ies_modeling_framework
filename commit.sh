c448aeeb16cd7c741df199ed628bb205c4bdbbff
jamesbrown <randomvoidmail@foxmail.com>
2023-07-14 21:28:00 +0800

    update

--- commit.sh
+++ commit.sh
@@ -5,7 +5,7 @@ git push origin_devops main
 env http_proxy="" https_proxy="" git push origin main
 # TODO: let chatgpt-like bots generate this commit message.
 
-cd microgrid\dsl_parser
+cd microgrid/dsl_parser
 git pull
 git add .
 git commit -m 'update'
