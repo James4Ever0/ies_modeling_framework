f9e96fc8d508e0aaf520a8c47d30082210f66d21
jamesbrown <randomvoidmail@foxmail.com>
2023-07-17 00:49:15 +0800

    update

--- commit.sh
+++ commit.sh
@@ -7,6 +7,7 @@ env http_proxy="" https_proxy="" git push origin main
 
 cd microgrid_base/dsl_parser
 git pull
+# shall you filter anything too big under this folder before commit, add them to .gitignore
 git add .
 git commit -m 'update'
 git push origin_devops main
