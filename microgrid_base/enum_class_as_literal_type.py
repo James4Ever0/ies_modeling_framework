1914ff09a66f508c320baf6912db8afca55d41af
you <you@example.com>
2023-07-18 16:45:05 +0800

    'update'

--- microgrid_base/enum_class_as_literal_type.py
+++ microgrid_base/enum_class_as_literal_type.py
@@ -2,7 +2,7 @@
 # a = StrEnum('Color', ['RED', 'GREEN', 'BLUE'])
 from enum import Enum
 # a = Enum('Color', ['RED', 'GREEN', 'BLUE'])
-
+from typing import Literal
 class a(Enum):
     RED='red'
     GREEN='green'
@@ -17,8 +17,7 @@ assert (b == 'blue') # true
 from pydantic import BaseModel
 class A(BaseModel):
     a0: a
-
-data = A(a0 = "blue") # though static type error, still working
+data = A(a0 = 'blue') # though static type error, still working
 # data = A(a0 = "blue2") # validation error.
 # data = A(a0 = a['BLUE']) # working
 print('data',data, data.a0)
