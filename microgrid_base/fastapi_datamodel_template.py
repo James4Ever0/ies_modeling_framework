408bf068072f54b43a47e894754a95b03714bc78
you <you@example.com>
2023-07-18 16:15:14 +0800

    'update'

--- microgrid_base/fastapi_datamodel_template.py
+++ microgrid_base/fastapi_datamodel_template.py
@@ -71,9 +71,11 @@ class 单次计算结果(BaseModel):
         description="存放例如经济目标值、环保目标值的计算结果",
         example={"financialObjective": 2000, "environmentalObjective": 3000},
     )
-    planningResultTable: List[规划结果] = Field()
+    planningResultTable: List[规划结果详情] = Field(title='规划结果详情列表', description = '方案内各机组信息') 
+    planningSummary: List[规划方案] = Field(title= '规划方案概览', description = '包括方案类型，方案总参数等等')
     performanceDataList: List[设备出力曲线] = Field(
         title="设备出力曲线列表",
+        description= "，设计规划和仿真模拟都有该项",
         example=[
             {
                 "name": "Any",
@@ -118,6 +120,9 @@ class 单次计算结果(BaseModel):
             }
         ],
     )
+    calculationType: CalculationType = Field(title="", description='')
+    @validator('calculationType')
+    def validate_calculationType()
 
 
 class ParetoCurve(BaseModel):
