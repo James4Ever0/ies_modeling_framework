f5a677130f40854466af55b0511bd9d2b1094958
you <you@example.com>
2023-07-18 16:35:06 +0800

    'update'

--- microgrid_base/enum_class_as_literal_type.py
+++ microgrid_base/enum_class_as_literal_type.py
@@ -5,4 +5,14 @@ a = StrEnum('Color', ['RED', 'GREEN', 'BLUE'])
 
 b: a = a.BLUE # Color.BLUE
 print(b, type(b))
-print(b == 'blue') # true
+# assert (b == 'blue2') # false
+assert (b == 'blue') # true
+
+from pydantic import BaseModel
+class A(BaseModel):
+    a0: a
+
+# data = A(a0 = "blue") # though type error, still working
+data = A(a0 = "blue2")
+# data = A(a0 = a['BLUE']) # nothing received.
+print('data',data, data.a0)
