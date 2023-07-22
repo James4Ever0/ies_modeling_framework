6987273a7f373e3499c1a764d0eec3528800170d
jamesbrown <randomvoidmail@foxmail.com>
2023-07-22 10:00:01 +0800

    update

--- microgrid_base/pyright_utils.py
+++ microgrid_base/pyright_utils.py
@@ -1,2 +1,32 @@
 MIN_PYRIGHT_VERSION='1.1.317' # if lower than this version then raise exception.
 
+import pyright
+from typing import Any, Union
+import subprocess
+
+# monkey patch start
+def run(                                      *args: str, **kwargs: Any
+) -> Union['subprocess.CompletedProcess[bytes]', 'subprocess.CompletedProcess[str]']:
+    ROOT_CACHE_DIR=pyright.utils.get_cache_dir() / 'pyright-python'
+    version=pyright.__pyright_version__
+    #current_version = pyright.node.get_pkg_version(pkg_dir / 'package.json')
+    #cache_dir = ROOT_CACHE_DIR / current_version
+    cache_dir = ROOT_CACHE_DIR / version
+    cache_dir.mkdir(exist_ok=True, parents=True)
+    pkg_dir = cache_dir / 'node_modules' / 'pyright'
+
+    script = pkg_dir / 'index.js'
+    if not script.exists():
+        raise RuntimeError(f'Expected CLI entrypoint: {script} to exist')
+    result=pyright.node.run('node', str(script), *args, **kwargs)
+    return result
+
+pyright.cli.run=run
+# monkey patch end
+
+args=['watchdog_macos.py']
+kwargs=dict(capture_output=True)
+result = pyright.cli.run(*args,capture_output=True)
+
+import rich
+rich.print(result)
