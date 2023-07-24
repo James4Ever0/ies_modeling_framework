bd6fdb9bc61b758060cbf6e2e2499d94798e054d
you <you@example.com>
2023-07-24 15:25:04 +0800

    'update'

--- microgrid_base/solve_model.py
+++ microgrid_base/solve_model.py
@@ -47,7 +47,9 @@ import rich
 with open("export_format.json", "r") as f:
     dt = json.load(f)
     simulationResultColumns = dt["仿真结果"]["ALL"]
-    simulationResultColumns = [e if type(e) == str else e[0] for e in simulationResultColumns]
+    simulationResultColumns = [
+        e if type(e) == str else e[0] for e in simulationResultColumns
+    ]
 
 with open("frontend_sim_param_translation.json", "r") as f:
     FSPT = json.load(f)
@@ -58,15 +60,15 @@ from topo_check import 拓扑图
 
 ###
 def 导出结果表_格式化(
-    结果表: DataFrame, 字符串表头: List[str], 翻译表: Dict[str, str], columns:List[str]
-) -> Tuple[DataFrame, DataFrame, List[Dict[str, Union[float, int, str]]]]:
+    结果表: DataFrame, 字符串表头: List[str], 翻译表: Dict[str, str], columns: List[str]
+) -> Tuple[List[Dict[str, Union[float, int, str]]], DataFrame, List[Dict[str, Union[float, int, str]]]]:
     结果表_导出 = pd.DataFrame([v for _, v in 结果表.items()], columns=columns)
     # use "inplace" otherwise you have to manually assign return values.
     结果表_导出.fillna({elem: "" for elem in 字符串表头}, inplace=True)
     结果表_导出.fillna(
         cmath.nan, inplace=True
     )  # default "nan" or "null" replacement, compatible with type "float"
-    结果表_未翻译 = 结果表_导出
+    结果表_未翻译 = 结果表_导出.to_dict(orient="records")
     结果表_导出 = translateDataframeHeaders(结果表_导出, 翻译表)
 
     结果表_导出.head()
@@ -443,14 +445,19 @@ def solveModelFromCalcParamList(
             rich.print(出力曲线字典)
             print()
             # breakpoint()
-            仿真结果表_未翻译, _, 仿真结果表_格式化 = 导出结果表_格式化(仿真结果表, 仿真结果字符串表头, FSPT, simulationResultColumns)
+            仿真结果表_未翻译, _, 仿真结果表_格式化 = 导出结果表_格式化(
+                仿真结果表, 仿真结果字符串表头, FSPT, simulationResultColumns
+            )
             # 仿真结果表_未翻译, _, 仿真结果表_格式化 = 导出结果表_格式化(仿真结果表, 仿真结果字符串表头, FSPT, 仿真结果.schema()['required'])
             # breakpoint()
             规划结果详情表_未翻译, _, 规划结果详情表_格式化 = 导出结果表_格式化(
-                规划结果详情表, 规划结果详情字符串表头, 规划结果详情.get_translation_table()
-            , 规划结果详情.schema()['required'])
+                规划结果详情表,
+                规划结果详情字符串表头,
+                规划结果详情.get_translation_table(),
+                规划结果详情.schema()["required"],
+            )
 
-            simulationResultList = [仿真结果.parse_obj(e) for e in 仿真结果表_未翻译]
+            simulationResultList = [仿真结果.parse_obj(e) for e in 仿真结果表_格式化]
             planningResultList = [规划结果详情.parse_obj(e) for e in 规划结果详情表_未翻译]
             # return 出力曲线字典, 仿真结果表_格式化
             出力曲线列表 = []
