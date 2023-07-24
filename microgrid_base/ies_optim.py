ad6f5f406b7d137e716ec9fc4a4b0c740196cb69
you <you@example.com>
2023-07-24 17:15:05 +0800

    'update'

--- microgrid_base/ies_optim.py
+++ microgrid_base/ies_optim.py
@@ -318,13 +318,13 @@ class 锂电池ID(设备ID):
 
 
 class 变压器ID(设备ID):
-    电输出: conint(ge=0) = Field(title="电输出ID", description="接口类型: 变压器输出")
-    """
-    类型: 变压器输出
-    """
     电输入: conint(ge=0) = Field(title="电输入ID", description="接口类型: 电母线输入")
     """
     类型: 电母线输入
+    """
+    电输出: conint(ge=0) = Field(title="电输出ID", description="接口类型: 变压器输出")
+    """
+    类型: 变压器输出
     """
 
 
@@ -340,13 +340,13 @@ class 变流器ID(设备ID):
 
 
 class 双向变流器ID(设备ID):
-    储能端: conint(ge=0) = Field(title="储能端ID", description="接口类型: 双向变流器储能端输入输出")
-    """
-    类型: 双向变流器储能端输入输出
-    """
     线路端: conint(ge=0) = Field(title="线路端ID", description="接口类型: 双向变流器线路端输入输出")
     """
     类型: 双向变流器线路端输入输出
+    """
+    储能端: conint(ge=0) = Field(title="储能端ID", description="接口类型: 双向变流器储能端输入输出")
+    """
+    类型: 双向变流器储能端输入输出
     """
 
 
@@ -3026,18 +3026,18 @@ class 变压器模型(设备模型):
 
         self.ports = {}
 
-        self.PD[self.设备ID.电输出] = self.ports["电输出"] = self.电输出 = self.变量列表(
-            "电输出", within=NonNegativeReals
-        )
-        """
-        类型: 变压器输出
-        """
-
         self.PD[self.设备ID.电输入] = self.ports["电输入"] = self.电输入 = self.变量列表(
             "电输入", within=NonPositiveReals
         )
         """
         类型: 电母线输入
+        """
+
+        self.PD[self.设备ID.电输出] = self.ports["电输出"] = self.电输出 = self.变量列表(
+            "电输出", within=NonNegativeReals
+        )
+        """
+        类型: 变压器输出
         """
 
         # 设备特有约束（变量）
@@ -3357,18 +3357,18 @@ class 双向变流器模型(设备模型):
 
         self.ports = {}
 
-        self.PD[self.设备ID.储能端] = self.ports["储能端"] = self.储能端 = self.变量列表(
-            "储能端", within=Reals
-        )
-        """
-        类型: 双向变流器储能端输入输出
-        """
-
         self.PD[self.设备ID.线路端] = self.ports["线路端"] = self.线路端 = self.变量列表(
             "线路端", within=Reals
         )
         """
         类型: 双向变流器线路端输入输出
+        """
+
+        self.PD[self.设备ID.储能端] = self.ports["储能端"] = self.储能端 = self.变量列表(
+            "储能端", within=Reals
+        )
+        """
+        类型: 双向变流器储能端输入输出
         """
 
         # 设备特有约束（变量）
@@ -4149,13 +4149,16 @@ class 规划方案概览(BaseModel):
 
         for simulationResult in simulationResultList:
             for remainedKey in remainedKeys:  # '年热负荷', '年电负荷', '年冷负荷', '年蒸汽负荷', '年氢气负荷'
-                keyBase = remainedKey.strip("年")
-                if (
-                    val := getattr(
-                        simulationResult, FSPT.get(keyBase, "NO_TRANSLATION"), ...
-                    )
-                ) is not ...:
-                    updateParam(remainedKey, val)
+                keyBase = remainedKey.strip("年").strip("负荷").strip("消耗量")
+                for keySuffix in ["负荷", "消耗量"]:
+                    attemptKey = f"{keyBase}{keySuffix}"
+                    if (
+                        val := getattr(
+                            simulationResult, FSPT.get(keyBase, "NO_TRANSLATION"), ...
+                        )
+                    ) is not ...:
+                        updateParam(remainedKey, val)
+                    break
 
         return cls(**params)
 
