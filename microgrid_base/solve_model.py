7370cbb0b4d929871c162b9273680f56e5f20d4f
you <you@example.com>
2023-07-24 15:05:04 +0800

    'update'

--- microgrid_base/solve_model.py
+++ microgrid_base/solve_model.py
@@ -443,6 +443,7 @@ def solveModelFromCalcParamList(
             print()
             breakpoint()
             仿真结果表_未翻译, _, 仿真结果表_格式化 = 导出结果表_格式化(仿真结果表, 仿真结果字符串表头, FSPT)
+            breakpoint()
             规划结果详情表_未翻译, _, 规划结果详情表_格式化 = 导出结果表_格式化(
                 规划结果详情表, 规划结果详情字符串表头, 规划结果详情.get_translation_table()
             )
