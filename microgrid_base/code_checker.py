369d3b048419d4cd02e7c59c27d8d12cd1506055
you <you@example.com>
2023-07-26 10:05:04 +0800

    'update'

--- microgrid_base/code_checker.py
+++ microgrid_base/code_checker.py
@@ -6,13 +6,16 @@ import re
 IMPORT_LOGGER_PRINT = "from log_utils import logger_print"
 IMPORT_LOGGER_PRINT_REGEX = r"^from[ ]+?log_utils[ ]+?import[ ]+?logger_print(?:| .+)$"
 # check pydantic field issues.
+fix_content = lambda old_content: "\n\n".join([IMPORT_LOGGER_PRINT, content])
 
 stripped_source = lambda el: astor.to_source(el).strip()
 
 # files = os.listdir(".")
-files = ["test_replace_logger.py", "test_replace_logger_no_template.py"] # files for test!
+files = ["test_replace_logger.py", "test_replace_logger_no_template.py", "test_replace_logger.py.j2"] # files for test!
 for fpath in files:
     if fpath.endswith(".py"):
+        with_template = (template_path:=f"{fpath}.j2") in files
+
         found_import_log_utils = False if fpath != "log_utils.py" else True
         # read this file.
         with open(fpath, "r") as f:
@@ -42,17 +45,16 @@ for fpath in files:
                     found_import_log_utils = True
         if not found_import_log_utils: # just import, do not change the print logic.
             # if no template was found, fix just one. if template found, fix both.
-            with_template = (template_path:=f"{fpath}.j2") in files
 
             print(f"fixing logging issue in file: {fpath}")
-            fix_content = lambda old_content: "\n\n".join([IMPORT_LOGGER_PRINT, content])
             with open(fpath, 'w+') as f:
                 f.write(fix_content(content))
-            if with_template:
+        if with_template:
+            with open(template_path, 'r') as f:
+                template_content = f.read()
+            has_import_on_root = re.findall(IMPORT_LOGGER_PRINT_REGEX, template_content, re.MULTILINE)
+            if len(has_import_on_root) == 0:
                 print(f"fixing logging issue in template: {template_path}")
-                with open(template_path, 'r') as f:
-                    template_content = f.read()
-                has_import_on_root = re.findall(IMPORT_LOGGER_PRINT_REGEX, template_content, re.MULTILINE)
-                if len(has_import_on_root) == 0:
-                    with open(template_path, 'w+') as f:
-                        f.write(fix_content(template_content))
+
+                with open(template_path, 'w+') as f:
+                    f.write(fix_content(template_content))
---
+++ microgrid_base/test_replace_logger.py.j2
@@ -0,0 +1,6 @@
+from log_utils import logger_print
+
+# there is no logger used
+a = 1
+b = 2
+def abc(): return 1
