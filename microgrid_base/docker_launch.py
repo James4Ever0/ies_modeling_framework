2b92413acbde1e2666d8c9ecad4ce2e116672a86
you <you@example.com>
2023-07-26 16:15:05 +0800

    'update'

--- microgrid_base/docker_launch.py
+++ microgrid_base/docker_launch.py
@@ -7,4 +7,10 @@ import docker
 image_name = "microgrid_server"
 
 
-if no_image
+if no_imported_image:
+    if no_exported_image:
+        # first build the image, then export.
+        ...
+    # load the exported image.
+
+# run the command to launch server within image from here.
