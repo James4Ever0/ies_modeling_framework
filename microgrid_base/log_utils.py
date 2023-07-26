3fda2ba1072467be17ad462074049a82f172d96f
you <you@example.com>
2023-07-26 13:50:04 +0800

    'update'

--- microgrid_base/log_utils.py
+++ microgrid_base/log_utils.py
@@ -79,8 +79,9 @@ myHandler = RotatingFileHandler(
 )
 myHandler.setLevel(logging.DEBUG)
 # myHandler.setLevel(logging.INFO) # will it log less things? yes.
-FORMAT = (
-    "<%(name)s:%(levelname)s> [%(pathname)s:%(lineno)s - %(funcName)s()]\n%(message)s"
+FORMAT = ( # add timestamp.
+    "%(asctime)s.%(msecs)03d <%(name)s:%(levelname)s> [%(pathname)s:%(lineno)s - %(funcName)s()]\n%(message)s"
+    # "<%(name)s:%(levelname)s> [%(pathname)s:%(lineno)s - %(funcName)s()]\n%(message)s"
 )
 # FORMAT = "<%(name)s:%(levelname)s> [%(filename)s:%(lineno)s - %(funcName)s() ] %(message)s"
 myFormatter = logging.Formatter(fmt=FORMAT)
