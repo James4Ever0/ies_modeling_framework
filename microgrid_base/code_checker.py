67ea32a4561fcaf40318b13647f6ec5e9b090ef4
you <you@example.com>
2023-07-26 11:15:06 +0800

    'update'

--- microgrid_base/code_checker.py
+++ microgrid_base/code_checker.py
@@ -5,32 +5,47 @@ import os
 import astor
 import re
 from typing import Callable
+
 # import traceback
 
 IMPORT_LOGGER_PRINT = "from log_utils import logger_print"
 IMPORT_LOGGER_PRINT_REGEX = r"^from[ ]+?log_utils[ ]+?import[ ]+?logger_print(?:| .+)$"
 
-def open_file_and_modify_content(fpath:str, func: Callable[[str], str]):
-    with open(fpath, 'r') as f:
+
+def open_file_and_modify_content(
+    fpath: str, func: Callable[[str], str], modify_msg: str
+):
+    with open(fpath, "r") as f:
         cnt = f.read()
     fixed_cnt = func(cnt)
-    with open(fpath, 'w+') as f:
+    if fixed_cnt != cnt:
+        print(f"{modify_msg}: {fpath}")
+    with open(fpath, "w+") as f:
         f.write(fixed_cnt)
     return fixed_cnt
 
+
 def fix_import_logger_in_content(fpath):
     # fixed_cnt = "\n\n".join([IMPORT_LOGGER_PRINT, cnt])
-    fixed_cnt = open_file_and_modify_content(fpath, lambda cnt: "\n\n".join([IMPORT_LOGGER_PRINT, cnt]))
+    fixed_cnt = open_file_and_modify_content(
+        fpath, lambda cnt: "\n\n".join([IMPORT_LOGGER_PRINT, cnt]), ""
+    )
     return fixed_cnt
+
 
 FIND_PRINT_REGEX = r"(?<!logger_)((rich.|)(?P<print_statement>print\(.+\)))"
 REPLACE_PRINT_REGEX = "logger_\g<print_statement>"
 
-def fix_print_statement_in_content(fpath:str):
+
+def fix_print_statement_in_content(fpath: str):
     # with open(fpath, 'r') as f:
     #     cnt = f.read()
     # fixed_cnt = re.sub(FIND_PRINT_REGEX, REPLACE_PRINT_REGEX, cnt, re.MULTILINE)
-    fixed_cnt = open_file_and_modify_content(fpath, lambda cnt: re.sub(FIND_PRINT_REGEX, REPLACE_PRINT_REGEX, cnt, re.MULTILINE))
+    fixed_cnt = open_file_and_modify_content(
+        fpath,
+        lambda cnt: re.sub(FIND_PRINT_REGEX, REPLACE_PRINT_REGEX, cnt, re.MULTILINE),
+        "",
+    )
     # with open(fpath, 'w+') as f:
     #     f.write(fixed_cnt)
     return fixed_cnt
@@ -39,11 +54,15 @@ def fix_print_statement_in_content(fpath:str):
 stripped_source = lambda el: astor.to_source(el).strip()
 
 # files = os.listdir(".")
-files = ["test_replace_logger.py", "test_replace_logger_no_template.py", "test_replace_logger.py.j2"] # files for test!
+files = [
+    "test_replace_logger.py",
+    "test_replace_logger_no_template.py",
+    "test_replace_logger.py.j2",
+]  # files for test!
 for fpath in files:
     if fpath.endswith(".py"):
-        with_template = (template_path:=f"{fpath}.j2") in files
-    
+        with_template = (template_path := f"{fpath}.j2") in files
+
         # with open(template_path, 'r') as f:
         #     template_content = f.read()
 
@@ -79,23 +98,25 @@ for fpath in files:
                         )
             elif isinstance(el, ast.ImportFrom):
                 # check if really imported.
-                if el.level == 0: # root level.
+                if el.level == 0:  # root level.
                     el_source = stripped_source(el)
                     if el_source == IMPORT_LOGGER_PRINT:
                         found_import_log_utils = True
-        if not found_import_log_utils: # just import, do not change the print logic.
+        if not found_import_log_utils:  # just import, do not change the print logic.
             # if no template was found, fix just one. if template found, fix both.
 
-            print(f"fixing logging issue in file: {fpath}")
+            # print(f"fixing logging issue in file: {fpath}")
             fix_import_logger_in_content(fpath)
             # with open(fpath, 'w+') as f:
             #     f.write(fix_import_logger_in_content(content))
         if with_template:
             template_content = fix_print_statement_in_content(template_path)
-    
-            has_import_on_root = re.findall(IMPORT_LOGGER_PRINT_REGEX, template_content, re.MULTILINE)
+
+            has_import_on_root = re.findall(
+                IMPORT_LOGGER_PRINT_REGEX, template_content, re.MULTILINE
+            )
             if len(has_import_on_root) == 0:
-                print(f"fixing logging issue in template: {template_path}")
+                # print(f"fixing logging issue in template: {template_path}")
                 fix_import_logger_in_content(template_path)
                 # with open(template_path, 'w+') as f:
                 #     f.write(fix_import_logger_in_content(template_content))
--- microgrid_base/test_replace_logger_no_template.py
+++ microgrid_base/test_replace_logger_no_template.py
@@ -4,3 +4,12 @@ from log_utils import logger_print
 a = 1
 b = 2
 def abc(): return 1
+
+logger_print("abc")
+logger_print('def')
+
+# import rich
+
+def myfunc():
+    logger_print('hjk')
+    logger_print("hjk")
