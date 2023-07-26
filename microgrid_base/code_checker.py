ac2e3c16ae2c6f6cb36ecc268eb9624b337a9919
you <you@example.com>
2023-07-26 09:45:04 +0800

    'update'

--- microgrid_base/code_checker.py
+++ microgrid_base/code_checker.py
@@ -1,7 +1,7 @@
 import ast
 import os
 import astor
-
+import re
 # import traceback
 
 # check pydantic field issues.
@@ -35,6 +35,14 @@ for fpath in files:
                 # check if really imported.
                 found_import_log_utils = True
         if not found_import_log_utils: # just import, do not change the print logic.
+            # if no template was found, fix just one. if template found, fix both.
+            with_template = (template_path:=f"{fpath}.j2") in files
+
             print("fixing logging issue on file:")
             with open(fpath, 'w+') as f:
-                f.writelines(["from log_utils import "])
+                f.write("\n\n".join(["from log_utils import logger_print", content]))
+            if with_template:
+                print("fixing logging issue from template:")
+                with open(template_path, 'r') as f:
+                    template_content = f.read()
+                    re.findall(r"[ ]+?from[ ]+?", , re.MULTILINE)
