b5b58f6796f48be757a450408348b460dbe8adb5
you <you@example.com>
2023-07-26 16:55:04 +0800

    'update'

--- microgrid_base/modify_bashrc_for_mamba.py
+++ microgrid_base/modify_bashrc_for_mamba.py
@@ -1,2 +1,18 @@
-with open(BASHRC,'r') as f:
-    content = f.read
+with open(BASHRC:= "/root/.bashrc",'r') as f:
+    lines = f.read().split("\n")
+
+uncomment = lambda l: l.split("#")[-1]
+flag = False
+
+new_lines = []
+for line in lines:
+    if not flag:
+        if "mamba initialize" in line:
+            flag = True
+        
+    if flag:
+        line = uncomment(line)
+    new_lines.append(line)
+
+with open(BASHRC, 'w+') as f:
+    f.write("\n".join(new_lines))
