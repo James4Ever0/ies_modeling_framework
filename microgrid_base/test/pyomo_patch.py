3b543e482dc7725c344362d622e1ed5defdbb959
you <you@example.com>
2023-07-27 17:20:05 +0800

    'update'

--- microgrid_base/test/pyomo_patch.py
+++ microgrid_base/test/pyomo_patch.py
@@ -1,4 +1,8 @@
 import pyomo
 
 pyomo_version = pyomo.__version__
-assert pyomo_version == (expected_pyomo_version:= ""), "Expected Pyomo version: {}\nActual: {}"
+assert pyomo_version == (
+    expected_pyomo_version := "6.5.0"
+), f"Expected Pyomo version: {expected_pyomo_version}\nActual: {pyomo_version}"
+
+pyomo.environ
