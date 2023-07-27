154dd30fc7d716e7f5d3f39a25090d8c65f3d51b
you <you@example.com>
2023-07-27 09:14:25 +0800

    'update'

--- microgrid_base/docker_launch.py
+++ microgrid_base/docker_launch.py
@@ -10,11 +10,11 @@ client = docker.from_env()
 #     base_url="//./pipe/docker_engine" if os.name == "nt" else "unix://var/run/docker.sock"
 # )
 
-image_name = "microgrid_server"
+image_tag = "microgrid_server:latest"
 dockerfile_path = "."
 
 image_storage_dir = "images"
-image_path = os.path.join(image_storage_dir, f"{image_name}.tar")
+image_path = os.path.join(image_storage_dir, f"{image_tag.replace(':','_')}.tar")
 import os
 
 image_storage_gitignore = os.path.join(image_storage_dir, ".gitignore")
@@ -29,11 +29,11 @@ with open(image_storage_gitignore, "w+") as f:
     f.write("*\n")
 
 images = client.images.list()
-image_tags = [tag.split(":")[0] for image in images for tag in image.tags]
-if image_name not in image_tags:
+image_tags = [tag for image in images for tag in image.tags]
+if image_tag not in image_tags:
     if not os.path.exists(image_path):
         # first build the image, then export.
-        client.images.build(path = dockerfile_path)
+        client.images.build(path=dockerfile_path, tag = image_tag)
     # load the exported image.
 
 # run the command to launch server within image from here.
