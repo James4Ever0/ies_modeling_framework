8cb208e62cdd21b880f945b80bdf5185ae0a09c5
you <you@example.com>
2023-07-31 17:55:07 +0800

    'update'

--- microgrid_base/chatgpt_convert_latex_to_python/test_code.py
+++ microgrid_base/chatgpt_convert_latex_to_python/test_code.py
@@ -6,16 +6,17 @@ import sympy
 
 # a, b= sympy.symbols("a b")
 # a, b= sympy.symbols("a_b b")
-a, b= sympy.symbols("a^b b")
+# a, b= sympy.symbols("a^b b")
 # r = sympy.Range(1,10)
-
-summation = sympy.Sum(a, (b, 1, 10), )
+b = sympy.symbols('b')
+a = sympy.IndexedBase('a')
+summation = sympy.Sum(a[b], (b, 1, 10), )
 # summation = sympy.Sum(a, (a, 1, b), r)
 # summation = sympy.Sum(c[a], (a, 1, b), r)
 # summation
-
-sympy.pretty_print(summation)
-sympy.print_latex(summation)
+with sympy.assuming(sympy.Q.integer(a)):
+    sympy.pretty_print(summation)
+    sympy.print_latex(summation)
 
 sympy.pretty_print(sympy.Derivative(b,b))
 sympy.print_latex(sympy.Derivative(b,b))
