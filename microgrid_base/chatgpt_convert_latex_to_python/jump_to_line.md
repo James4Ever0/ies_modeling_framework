da7930bd08d03b47c7a411af4290072b3d30ae23
you <you@example.com>
2023-07-31 17:25:06 +0800

    'update'

--- microgrid_base/chatgpt_convert_latex_to_python/jump_to_line.md
+++ microgrid_base/chatgpt_convert_latex_to_python/jump_to_line.md
@@ -1,2 +1,8 @@
 [link to line one]()
 [link to line two](./test_code.py)
+
+formula:
+
+$$
+\sum_{a=1}^{b} a
+$$
--- microgrid_base/chatgpt_convert_latex_to_python/test_code.py
+++ microgrid_base/chatgpt_convert_latex_to_python/test_code.py
@@ -5,4 +5,9 @@ import sympy
 
 a,b = sympy.symbols("a b")
 
-summation = sympy.Sum()
+summation = sympy.Sum(a, (a, 1, b))
+
+# summation
+
+sympy.pretty_print(summation)
+sympy.print_latex(summation)
