b1b37d2f0aa0a2cf43954e737f6f8c4d58ff6e83
you <you@example.com>
2023-08-01 16:00:05 +0800

    'update'

--- microgrid_base/chatgpt_convert_latex_to_python/render_latex.js
+++ microgrid_base/chatgpt_convert_latex_to_python/render_latex.js
@@ -6,7 +6,7 @@ md.use(mathjax3);
 // double backslash is required for javascript strings, but not html input
 const {writeFileSync, readFileSync} = require("fs");
 
-var input = readFileSync("jump_to_line.md")
+var input = readFileSync("jump_to_line.md", {'encoding': 'utf8','mode':'r'});
 var result = md.render(input);
 
 writeFileSync("jump_to_line.html", result)
