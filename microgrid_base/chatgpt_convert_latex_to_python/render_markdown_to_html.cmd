ccf5a539568875e39e080b42ae773913f72a5a0f
you <you@example.com>
2023-08-01 15:40:14 +0800

    'update'

--- microgrid_base/chatgpt_convert_latex_to_python/render_markdown_to_html.cmd
+++ microgrid_base/chatgpt_convert_latex_to_python/render_markdown_to_html.cmd
@@ -1,2 +1,3 @@
-python -m markdown -f jump_to_line.html -o html jump_to_line.md
+@REM python -m markdown -f jump_to_line.html -o html jump_to_line.md
 @REM pandoc -o jump_to_line.html jump_to_line.md
+markdown-it jump_to_line.md > jump_to_line.md
