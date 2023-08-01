35c2aee9df1b72f823dc95af7128b64335bc9109
you <you@example.com>
2023-08-01 15:50:04 +0800

    'update'

--- microgrid_base/chatgpt_convert_latex_to_python/render_markdown_with_latex.py
+++ microgrid_base/chatgpt_convert_latex_to_python/render_markdown_with_latex.py
@@ -1,2 +1,6 @@
 import markdown
-md = markdown.Markdown(extensions=['pymdownx.arithmatex'])
+
+md = markdown.Markdown(extensions=["pymdownx.arithmatex"], output_format="html")
+
+
+md.convertFile(input="jump_to_line.md", output="jump_to_line.html", encoding="utf-8")
