a9124d03fad3c13e4ab6e94260de8e3fe9a1cdb8
you <you@example.com>
2023-08-01 14:55:12 +0800

    'update'

--- microgrid_base/chatgpt_convert_latex_to_python/extract_line_range_from_html.py
+++ microgrid_base/chatgpt_convert_latex_to_python/extract_line_range_from_html.py
@@ -5,10 +5,17 @@ soup = BeautifulSoup(open(input_file, 'r').read())
 
 import parse
 line_range = range(10, 20+1)
+# import rich
 
 for a in soup.find_all('a'):
     print(a)
+    a_id = a.attrs['id']
+    id_no = int(a_id.split("-")[-1])
+    if id_no not in line_range:
+        print('removing line:', id_no)
+        # for elem in a.
+    # rich.print(a.__dict__)
     print(dir(a))
-    span = a.next_sibling()
-    print(span)
+    # span = a.next_sibling()
+    # print(span)
     # find next sibling of "span".
