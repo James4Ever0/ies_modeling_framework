3d5f8b8feba320f76428e6bccab08e2bcb2f97e8
you <you@example.com>
2023-08-01 16:10:04 +0800

    'update'

--- microgrid_base/chatgpt_convert_latex_to_python/render_markdown_to_html.cmd
+++ microgrid_base/chatgpt_convert_latex_to_python/render_markdown_to_html.cmd
@@ -1,7 +1,10 @@
 @REM python -m markdown -f jump_to_line.html -o html jump_to_line.md
 @REM https://facelessuser.github.io/pymdown-extensions/extensions/arithmatex/
-python render_markdown_with_latex.py
+@REM python render_markdown_with_latex.py
 
 @REM pandoc -o jump_to_line.html jump_to_line.md
 @REM markdown-it jump_to_line.md > jump_to_line.html
 @REM npm install markdown-it markdown-it-mathjax3
+node render_latex.js
+@REM then pdf
+pandoc -o jump_to_line.pdf jump_to_line.html
