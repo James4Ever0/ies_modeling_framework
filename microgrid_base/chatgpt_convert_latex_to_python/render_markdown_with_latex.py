b463d4250c7059b719e4d7b51f476508a1838625
you <you@example.com>
2023-08-01 17:55:04 +0800

    'update'

--- microgrid_base/chatgpt_convert_latex_to_python/render_markdown_with_latex.py
+++ microgrid_base/chatgpt_convert_latex_to_python/render_markdown_with_latex.py
@@ -4,5 +4,17 @@
 
 # md.convertFile(input="jump_to_line.md", output="jump_to_line.html", encoding="utf-8")
 
-from 
+from markdown_it import MarkdownIt
 from mdit_py_plugins import dollarmath
+
+md = MarkdownIt().use(dollarmath)
+
+sample_markdown=r"""
+# hello latex
+$$
+\alpha = \beta
+$$
+"""
+
+html_string = md.render(sample_markdown)
+print(html_string)
