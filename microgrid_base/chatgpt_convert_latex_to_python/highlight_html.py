604bc572ea8c057969d1d2db4f15dbe0e8375732
you <you@example.com>
2023-08-02 12:05:04 +0800

    'update'

--- microgrid_base/chatgpt_convert_latex_to_python/highlight_html.py
+++ microgrid_base/chatgpt_convert_latex_to_python/highlight_html.py
@@ -8,21 +8,22 @@ soup = BeautifulSoup(open(input_file, "r").read())
 # line_range = range(10, 20 + 1)
 # import rich
 
-elements_to_delete = []
+elements_to_wrap = {}
 for a in soup.find_all("a"):
     # print(a)
     a_id = a.attrs["id"]
     id_no = int(a_id.split("-")[-1])
-    if id_no not in line_range:
-        print("removing line:", id_no)
-        elements_to_delete.append(a)
-        for elem in a.next_siblings:
-            # print(elem, elem.name)
-            if elem.name == "a":
-                # if elem.name !='span':
-                break
-            else:
-                elements_to_delete.append(elem)
+    elements_to_wrap[id_no] = []
+    # if id_no not in line_range:
+    print("changing line:", id_no)
+    elements_to_wrap[id_no].append(a)
+    for elem in a.next_siblings:
+        # print(elem, elem.name)
+        if elem.name == "a":
+            # if elem.name !='span':
+            break
+        else:
+            elements_to_wrap[id_no].append(elem)
     # span = a.next_sibling()
     # print(span)
     # print(dir(a))
