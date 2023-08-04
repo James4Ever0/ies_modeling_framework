086daa0ea8bbce44c400cbfa8e47095eac719af0
you <you@example.com>
2023-08-04 16:50:05 +0800

    'update'

--- microgrid_base/test/common_fixtures.py
+++ microgrid_base/test/common_fixtures.py
@@ -154,14 +154,14 @@ def 测试光伏发电模型(
 # =================================风力发电================================= #
 
 
-@fixture
-def 测试风力发电信息():
+@fixture(params=["定桨",变桨",标幺值"])
+def 测试风力发电信息(request:Request):
     val = 风力发电信息(
         设备名称="风力发电",
         生产厂商="Any",
         设备型号="风力发电1",
-        machineType=None,
-        normalizedPower=None,
+        machineType=request.param,
+        normalizedPower=[],
         CutoutPower= 90,
         RatedPower=100,
         RatedWindSpeed=100,
