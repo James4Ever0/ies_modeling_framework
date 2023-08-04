318622806eac21f32ae5d2ea7aeacc5bc16433fa
you <you@example.com>
2023-08-04 16:10:05 +0800

    'update'

--- microgrid_base/fastapi_server_template.py
+++ microgrid_base/fastapi_server_template.py
@@ -37,6 +37,22 @@ from fastapi_datamodel_template import (
     RevokeResult,
     CalculationStateResult,
 )
+from fastapi.exceptions import RequestValidationError
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+# from fastapi.utils import is_body_allowed_for_status_code
+# from starlette.exceptions import HTTPException
+from starlette.requests import Request
+from starlette.responses import JSONResponse #, Response
+from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
+async def request_validation_exception_handler(
+    request: Request, exc: RequestValidationError
+) -> JSONResponse:
+    
+    return JSONResponse(
+        status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+        content={"detail": jsonable_encoder(exc.errors())},
+    )
 
 # define the input structure here.
 from pydantic import BaseModel
