0e7c4bb3e5fd1ae6a73228ba48787ab31a82cbf4
you <you@example.com>
2023-08-04 16:30:08 +0800

    'update'

--- microgrid_base/reload.sh
+++ microgrid_base/reload.sh
@@ -1,5 +1,5 @@
-mkdir history_logs
-mv microgrid_server_release/server/logs history_logs/
+# mkdir history_logs
+mv microgrid_server_release/server/logs history_logs
 rm -rf microgrid_server_release
 7z x release.7z
 cd microgrid_server_release
@@ -7,5 +7,6 @@ cd init
 bash init.sh
 cd ..
 cd server
-mkdir logs
+# mkdir logs
+mv /root/history_logs logs
 bash fastapi_tmuxp.sh windows
