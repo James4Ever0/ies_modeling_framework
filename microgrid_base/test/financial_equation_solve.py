d9ab263b90b06b9cbd310299167abaaf989e0f09
you <you@example.com>
2023-08-08 17:55:04 +0800

    'update'

--- microgrid_base/test/financial_equation_solve.py
+++ microgrid_base/test/financial_equation_solve.py
@@ -1,14 +1,34 @@
 import sympy
 
-def solve_eq(a_arr:list, build_time:int, business_time:int):
+
+def solve_eq(a_arr: list, build_time: int, business_time: int):
+    assert len(a_arr) == build_time+business_time
     i = sympy.symbols("i")
     expr = 0
     for n in range(build_time):
-        expr += a_arr[n] / ((1+i)**n)
+        expr += a_arr[n] / ((1 + i) ** n)
     for n in range(business_time):
-        arr_index = n+build_time
-        expr += a_arr[arr_index] /((i+i)**(arr_index+1))
-    sol = sympy.nsolve(sympy.Eq(expr,0), (0,1))
+        arr_index = n + build_time
+        expr += a_arr[arr_index] / ((i + i) ** (arr_index + 1))
+    sol = sympy.nsolve(sympy.Eq(expr, 0), (0, 1))
     print(sol)
     return sol
 
+
+a_arr = [
+    -6511.07,
+    1283.97,
+    5714.22,
+    7854.22,
+    -14725.82,
+    1841.11,
+    10792.05,
+    10792.05,
+    10792.05,
+    10792.05,
+    11190.56,
+    2343.6,
+]
+
+build_time = 2
+business_time = 10
