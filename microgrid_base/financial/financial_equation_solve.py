ae70b4a72f5a82f8b4874af691317cf66b006389
jamesbrown <randomvoidmail@foxmail.com>
2023-08-09 15:50:01 +0800

    update

--- microgrid_base/financial/financial_equation_solve.py
+++ microgrid_base/financial/financial_equation_solve.py
@@ -1,21 +1,24 @@
 import sympy
 import numpy as np
 from typing import cast
+
+
 def solve_eq(a_arr: list, build_time: int, business_time: int):
     assert len(a_arr) == build_time + business_time
     i = sympy.symbols("i")
-    expr= cast(sympy.Expr,0)
-    for n in range(build_time+business_time):
+    expr = cast(sympy.Expr, 0)
+    for n in range(build_time + business_time):
         expr += a_arr[n] / ((1 + i) ** (n + (0 if n < build_time else 1)))
-    print('[expr]',expr)
+    print("[expr]", expr)
     try:
         sol = sympy.nsolve(
             sympy.Eq(expr, 0), i, (0, 1), solver="bisect", verify=True
         )  # will raise exception if no solution exists.
         return sol
     except:
-        vals = [expr.evalf(subs={i:v}) for v in np.linspace(0, 1, 100)]
-        print('possible vals:',vals)
+        vals = [expr.evalf(subs={i: v}) for v in np.linspace(0, 1, 100)]
+        print("possible vals:", vals)
+
 
 # a_arr = [
 #     -6511.07,
@@ -48,18 +51,18 @@ def solve_eq(a_arr: list, build_time: int, business_time: int):
 # ]
 
 a_arr = [
--5947.12
-1462.36
-5714.22
-2077.78
-25721.82
-9883.89
-1920.25
-1920.25
-1920.25
-1920.25
-1920.25
-511.45
+    -5947.12,
+    1462.36,
+    5714.22,
+    2077.78,
+    25721.82,
+    9883.89,
+    1920.25,
+    1920.25,
+    1920.25,
+    1920.25,
+    1920.25,
+    511.45,
 ]
 
 build_time = 2
@@ -67,6 +70,6 @@ business_time = 10
 
 sol = solve_eq(a_arr, build_time, business_time)
 if sol:
-    print('[sol]',sol)  # float
+    print("[sol]", sol)  # float
 else:
     print("no solution.")
