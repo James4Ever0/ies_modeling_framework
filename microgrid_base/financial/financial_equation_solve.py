401adb288f11d5403cd31f683af3cadfad19e47f
jamesbrown <randomvoidmail@foxmail.com>
2023-08-09 15:40:01 +0800

    update

--- microgrid_base/financial/financial_equation_solve.py
+++ microgrid_base/financial/financial_equation_solve.py
@@ -5,8 +5,9 @@ def solve_eq(a_arr: list, build_time: int, business_time: int):
     assert len(a_arr) == build_time + business_time
     i = sympy.symbols("i")
     expr= cast(sympy.Expr,0)
-    for n in range(build_time):
+    for n in range(build_time+business_time):
         expr += a_arr[n] / ((1 + i) ** (n + (0 if n < build_time else 1)))
+    print('[expr]',expr)
     try:
         sol = sympy.nsolve(
             sympy.Eq(expr, 0), i, (0, 1), solver="bisect", verify=True
@@ -51,6 +52,6 @@ business_time = 10
 
 sol = solve_eq(a_arr, build_time, business_time)
 if sol:
-    print(sol)  # float
+    print('[sol]',sol)  # float
 else:
     print("no solution.")
