0dd383aac54f6ee1101f33f7483ba7e5bae0d6de
you <you@example.com>
2023-08-22 13:15:44 +0800

    'update'

--- commit.py
+++ commit.py
@@ -4,7 +4,10 @@ import datetime
 import os
 import sys
 import tkinter
+import subprocess
 # on windows nt, alert us (using tkinter or native api?) if commit has failed.
 # on other platforms, please improvise.
+
+CHECK_GPTCOMMIT_KEYS="gptcommit config keys"
 
 # setup timezone as Shanghai
