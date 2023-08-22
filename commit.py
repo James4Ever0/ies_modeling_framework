16725027bd69130b6f3b0faff7dc9b20b0897890
you <you@example.com>
2023-08-22 12:00:03 +0800

    'update'

--- commit.py
+++ commit.py
@@ -1,7 +1,10 @@
+# this file shall be identical anywhere else.
 import pytz
 import datetime
 import os
 import sys
-import tkinter # on windows nt, alert us if commit has failed.
+import tkinter
+# on windows nt, alert us (using tkinter or native api?) if commit has failed.
+# on other platforms, please improvise.
 
 # setup timezone as Shanghai
