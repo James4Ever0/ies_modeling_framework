53a1ba44fb6a1048a8de5a4ba0e85b0f6a0c5d7a
you <you@example.com>
2023-08-22 11:55:03 +0800

    'update'

---
+++ setup_gptcommit.cmd
@@ -0,0 +1,3 @@
+gptcommit config set --local openai.apt_base "https://api.chatanywhere.com.cn"
+gptcommit config set --local openai.api_key "sk-CXMgNasrAvxNDYDTVE1LZtJvTWEF5scC6OvHhbTe1i016urg"
+@REM ensure that we do this in regular interval. do not do this too fast.
