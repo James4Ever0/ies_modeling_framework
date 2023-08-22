8eb862b406dc5f88719014be69189230ef3169d3
you <you@example.com>
2023-08-22 16:25:23 +0800

    'update'

--- setup_gptcommit.cmd
+++ setup_gptcommit.cmd
@@ -1,4 +1,5 @@
 gptcommit config set --local openai.apt_base "https://api.chatanywhere.com.cn/v1"
 gptcommit config set --local openai.api_key "sk-CXMgNasrAvxNDYDTVE1LZtJvTWEF5scC6OvHhbTe1i016urg"
+gptcommit config set --local openai.model "gpt-3.5-turbo"
 gptcommit install
 @REM ensure that we do this in regular interval. do not do this too fast.
