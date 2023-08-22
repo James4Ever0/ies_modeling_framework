4b03e5ce37784830423a34b44df3e2c2ee6a1359
you <you@example.com>
2023-08-22 16:10:23 +0800

    'update'

--- commit.cmd
+++ commit.cmd
@@ -7,6 +7,7 @@
 @REM git commit -m 'update'
 @REM git push
 @REM install gptcommit: cargo install --locked gptcommit
+
 echo %USERNAME% executed commit script at %DATE% %TIME%
 git config --global --add safe.directory E:/works/jubilant-adventure2
 git pull origin_devops main
@@ -14,7 +15,6 @@ git add .
 git commit -m 'update'
 git push origin_devops main
 git push origin main
-
 
 
 cd microgrid_base/dsl_parser
