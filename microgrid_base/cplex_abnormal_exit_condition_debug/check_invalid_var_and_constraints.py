3fffefc341f968fb61b9b702a5dea4fc63ab71b9
you <you@example.com>
2023-08-23 16:10:50 +0800

    feat: Implement advanced linearization and constraint checking.
    - Implement advanced logical expression linearization using pyomo.GDP
    - Define `ConstraintInfo` and `VarInfo` classes
    - Add functions to get violation of infeasible constraints and bounds
    - Modify `check_invalid_var_and_constraints.py` to use new functions and classes
    - Update timestamp in `.last_commit_time` file

--- microgrid_base/cplex_abnormal_exit_condition_debug/check_invalid_var_and_constraints.py
+++ microgrid_base/cplex_abnormal_exit_condition_debug/check_invalid_var_and_constraints.py
@@ -3,6 +3,8 @@
 
 from pyomo.environ import *
 
+# advanced logical expression linearization using pyomo.GDP
+# ref: https://pyomo.readthedocs.io/en/latest/modeling_extensions/gdp/modeling.html
 model = ConcreteModel()
 
 model.a = Var(within=Binary)
@@ -19,5 +21,53 @@ model.pw = Piecewise(
     model.c, model.e, pw_pts=[-100, 0, 100], pw_repn="MC", f_rule=[100, 0, -100]
 )
 
-from pyomo.util.infeasible import log_infeasible_constraints
+# you might need to sort it out. check how much further it goes.
+from pyomo.util.infeasible import log_infeasible_constraints, log_infeasible_bounds
+from pydantic import BaseModel
+from typing import Union, Literal
+
+
+class ConstraintInfo(BaseModel):
+    variables: List[VarInfo]
+    violation: float
+
+
+def get_violation_of_infeasible_constraints(model: ConcreteModel, tol=1e-6):
+    ...
+
+
+class VarViolation(BaseModel):
+    bound_violation: float
+    vartype_violation: float
+
+
+class VarInfo(BaseModel):
+    varName: str
+    val: float
+    varType: Literal[""]
+    lower_bound: Union[float, None]
+    upper_bound: Union[float, None]
+    violation: VarViolation
+
+
+def get_violation_of_infeasible_bounds_and_vartype(model: ConcreteModel, tol=1e-6):
+    ...
+
+
+def get_violation_of_infeasible_bounds_and_vartype(model: ConcreteModel, tol=1e-6):
+    checkers = {}
+    results = []
+    for var in model.component_data_objects(ctype=Var, descend_into=True):
+        vartype = ...
+        varname = ...
+        val = value(var)
+        if vartype in checkers.keys():
+            checker = checkers[vartype]
+            violation = checker(
+                val, tol
+            )  # violation shall be positive when actual violation is greater than tolerance, otherwise zero.
+            if violation:
+                results.append()
+
+
 log_infeasible_constraints(model)
