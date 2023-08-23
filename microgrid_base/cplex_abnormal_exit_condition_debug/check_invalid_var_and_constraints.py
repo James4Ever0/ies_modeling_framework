0c3d2b1346ba0bfff15eec440ff571515715e779
you <you@example.com>
2023-08-23 16:45:22 +0800

    refactor: Add functions and fields to improve constraint and variable analysis.
    - Add new functions and fields to `check_invalid_var_and_constraints.py`
    - Improve variable naming conventions in `check_invalid_var_and_constraints.py`
    - Update timestamp in `.last_commit_time` file.

--- microgrid_base/cplex_abnormal_exit_condition_debug/check_invalid_var_and_constraints.py
+++ microgrid_base/cplex_abnormal_exit_condition_debug/check_invalid_var_and_constraints.py
@@ -24,16 +24,7 @@ model.pw = Piecewise(
 # you might need to sort it out. check how much further it goes.
 from pyomo.util.infeasible import log_infeasible_constraints, log_infeasible_bounds
 from pydantic import BaseModel
-from typing import Union, Literal
-
-
-class ConstraintInfo(BaseModel):
-    variables: List[VarInfo]
-    violation: float
-
-
-def get_violation_of_infeasible_constraints(model: ConcreteModel, tol=1e-6):
-    ...
+from typing import Union, Literal, List
 
 
 class VarViolation(BaseModel):
@@ -41,13 +32,62 @@ class VarViolation(BaseModel):
     vartype_violation: float
 
 
+# import math
+
+def moderate_violation(violation,tol):
+    violation = abs(violation)
+    if violation <= tol:
+        violation = 0
+    return violation
+
+def get_bounds_violation(val, 
+    lower_bound: Union[float, None],
+    upper_bound: Union[float, None], tol):
+    if all([bound is not None for bound in [lower_bound, upper_bound]]):
+        assert lower_bound <= upper_bound, "invalid bound ({lower_bound}, {upper_bound})"
+    violation = 0
+    if lower_bound:
+        if val<
+    violation = moderate_violation(violation)
+
+
+def get_boolean_or_integer_violation(val, tol):
+    violation = val % 1
+    if violation != 0:
+        violation = min([violation, 1 - violation])
+
+
+
 class VarInfo(BaseModel):
     varName: str
     val: float
-    varType: Literal[""]
+    varDomain: Literal[  # usually, just need to check if it is boolean/binary/integer.
+        "Reals",
+        "PositiveReals",
+        "NonPositiveReals",
+        "NegativeReals",
+        "NonNegativeReals",
+        "Integers",
+        "PositiveIntegers",
+        "NonPositiveIntegers",
+        "NegativeIntegers",
+        "NonNegativeIntegers",
+        "Boolean",
+        "Binary",
+    ]
     lower_bound: Union[float, None]
     upper_bound: Union[float, None]
     violation: VarViolation
+
+
+class ConstraintInfo(BaseModel):
+    variables: List[VarInfo]
+    violation: float
+    representation: str
+
+
+def get_violation_of_infeasible_constraints(model: ConcreteModel, tol=1e-6):
+    ...
 
 
 def get_violation_of_infeasible_bounds_and_vartype(model: ConcreteModel, tol=1e-6):
@@ -58,16 +98,18 @@ def get_violation_of_infeasible_bounds_and_vartype(model: ConcreteModel, tol=1e-
     checkers = {}
     results = []
     for var in model.component_data_objects(ctype=Var, descend_into=True):
-        vartype = ...
-        varname = ...
+        varDomain = ...
+        varName = var.name
         val = value(var)
-        if vartype in checkers.keys():
-            checker = checkers[vartype]
+        if varDomain in checkers.keys():
+            checker = checkers[varDomain]
             violation = checker(
                 val, tol
             )  # violation shall be positive when actual violation is greater than tolerance, otherwise zero.
             if violation:
                 results.append()
+        else:
+            raise Exception("unknown vartype: %s" % varDomain)
 
 
 log_infeasible_constraints(model)
