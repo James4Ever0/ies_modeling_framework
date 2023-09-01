5d6b0549ee6af2bd5babe09a62bf1cd557881ac9
you <you@example.com>
2023-09-01 10:25:09 +0800

    feat: Relax integrality and add solver functions for CPLEX solver.
    - Add getModelSolution and solver_solve functions to solve a model using cplex solver
    - Use solver_solve instead of solver.solve to solve the model
    - Relax integrality of the model using TransformationFactory
    - Print the result name and result using rich.print
    - Update the timestamp in .last_commit_time file

--- microgrid_base/cplex_abnormal_exit_condition_debug/solve_as_simplex.py
+++ microgrid_base/cplex_abnormal_exit_condition_debug/solve_as_simplex.py
@@ -3,6 +3,18 @@
 from pyomo.environ import *
 
 
+def getModelSolution(model: ConcreteModel):
+    solution = {}
+    for v in model.component_data_objects(ctype=Var, active=True, descend_into=True):
+        varName = v.name
+        val = value(v, exception=None)
+        if val is not None:
+            solution[varName] = val
+        else:
+            return None
+    return solution
+
+
 domainDelegationTable = {
     "NonNegativeIntegers": NonNegativeReals,
     "NonPositiveIntegers": NonPositiveReals,
@@ -48,35 +60,48 @@ def simplexDelegationContext(model: ConcreteModel):
         for v in model.component_data_objects(Var, active=True):
             varName = v.name
             if varName in delegationRestoreTable.keys():
-                v.setlb()
-                v.setub()
-                v.domain = ...
-        
+                restoreInfo = delegationRestoreTable[varName]
+                v.setlb(restoreInfo["lb"])
+                v.setub(restoreInfo["ub"])
+                v.domain = restoreInfo["domain"]
+        del delegationRestoreTable
+
+
+solver = SolverFactory("cplex")
+
+
+def solver_solve(model):
+    ret = solver.solve(model, tee=True)
+    sol = getModelSolution(model)
+    return ret, sol
 
 
 model = ConcreteModel()
 model.a = Var(domain=Integers, bounds=(-0.5, 5.5))
 model.b = Var(domain=Boolean, bounds=(0.3, 1.1))  # feasible, if value(model.b) == 1
 
-
-# breakpoint()
 model.o = Objective(expr=model.a + model.b, sense=minimize)
 
-solver = SolverFactory("cplex")
 
-ret = solver.solve(model, tee=True)
+ret = solver_solve(model)
 
 model_clone = model.clone()
 
-TransformationFactory('core.relax_integrality').apply_to(model_clone)
-
-ret_clone = solver.solve(model_clone, tee=True)
+TransformationFactory("core.relax_integrality").apply_to(model_clone)
+# working
+ret_clone = solver_solve(model_clone)
 
 with simplexDelegationContext(model):
-    ret_delegated = solver.solve(model, tee=True)
+    # not working
+    ret_delegated = solver_solve(model)
 
 
 results = {"ret": ret, "ret_clone": ret_clone, "ret_delegated": ret_delegated}
 
+import rich
+
 for res_name, res in results.items():
-    ...
+    ret, sol = res
+    print("result name:", res_name)
+    rich.print(res)
+    print(sol)
