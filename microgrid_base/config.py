8c1952a61619c15e6fed0ea982119069b91b0134
you <you@example.com>
2023-09-04 11:00:08 +0800

    refactor: Refactor configuration handling in microgrid_base.
    - Update `get_import_fpaths` to handle multiple file paths
    - Add case-insensitivity to property names in `EnvConfig`
    - Remove commented out code and unused variable in `EnvConfig`
    - Update timestamp in `.last_commit_time`

--- microgrid_base/config.py
+++ microgrid_base/config.py
@@ -5,22 +5,25 @@ from pydantic import BaseModel
 class EnvBaseModel(BaseModel):
     IMPORT:str = ""
     def get_import_fpaths(self):
-        self.IMPORT
+        imp_fpaths = self.IMPORT.strip().split()
+        imp_fpaths = [(new_fp:=fp.strip()) for fp in imp_fpaths if len(new_fp)>0]
+        return imp_fpaths
 
 class EnvConfig:
-
     """
     This class is used to parse and store the environment variables from file or environment variables.
+
+    Property names are case-insensitive.
     """
     suspicous_threshold = 3
     min_envname_length_threshold = 6
+    
     def __init__(self, cls):
         if issubclass(cls, EnvBaseModel):
             ...
         else:
             raise Exception(f"{cls} is not a valid EnvBaseModel")
 
-    def __
     # VAR_INIT_AS_ZERO = False
     # UNIT_WARNING_AS_ERROR = False
     # PERCENT_WARNING_THRESHOLD = (
