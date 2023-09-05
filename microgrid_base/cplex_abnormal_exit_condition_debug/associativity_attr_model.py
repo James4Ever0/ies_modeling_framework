c8b274d28a65ea6e41686b6583a6433174db1f30
you <you@example.com>
2023-09-05 16:00:07 +0800

    feat: Implement Pyomo model for abnormal exit condition.
    - Add new Pyomo model for associativity attribute in microgrid_base/cplex_abnormal_exit_condition_debug
    - Create Piecewise object with specific parameters
    - Update timestamp in .last_commit_time file

---
+++ microgrid_base/cplex_abnormal_exit_condition_debug/associativity_attr_model.py
@@ -0,0 +1,37 @@
+from pyomo.environ import *
+
+model = ConcreteModel()
+model.a = Var()
+model.b = Var([1, 2])
+model.pw = Piecewise(
+    model.b[1],
+    model.a,
+    pw_pts=[1, 2, 3],
+    pw_repn="MC",
+    f_rule=[3, 2, 1],
+    pw_constr_type="EQ",
+    unbounded_domain_var=True,
+)
+
+model.cons = Constraint(expr=model.a >= model.b[2])
+model.obj = Objective(expr=0, sense=minimize)
+# print(dir(model.pw))
+# breakpoint()
+for obj in model.component_data_objects():
+    print(obj.name)
+
+model.write("associativity_output.lp")
+print("_" * 60)
+import rich
+
+for it in model.solutions.symbol_map.values():
+    for k, v in it.bySymbol.items():
+        print(k, v().name)
+for c in [model.b, model.pw]:
+# for c in [model.a, model.b,model.pw, model.cons]:
+    print("_" * 60)
+    # breakpoint()
+    # for k,v in c.items():
+    #     print(k,v)
+    for obj in c.root_block.component_data_objects():
+        print(obj.name)
