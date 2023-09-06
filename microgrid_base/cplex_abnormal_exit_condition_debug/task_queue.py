44f399dc336707ebebc8f111017c422dfa536531
you <you@example.com>
2023-09-06 13:50:09 +0800

    chore: Refactor task queue with Redis and RQ integration
    - Update timestamp in `.last_commit_time` file
    - Add Redis and RQ imports to `task_queue.py`
    - Create Redis queue and define functions for successful and failed tasks with sleep time in `task_queue.py`

--- microgrid_base/cplex_abnormal_exit_condition_debug/task_queue.py
+++ microgrid_base/cplex_abnormal_exit_condition_debug/task_queue.py
@@ -1,1 +1,18 @@
-import rq
+import time
+
+from redis import Redis
+from rq import Queue, Retry
+
+port = 6380
+print("Connecting to Redis at port " + port)
+q = Queue(connection=Redis(port=port))
+
+def task_success():
+    print("running task success")
+    time.sleep(1)
+    print("end running task success")
+
+def task_fail():
+    print("running task fail")
+    time.sleep(2)
+    print("end running task fail")
