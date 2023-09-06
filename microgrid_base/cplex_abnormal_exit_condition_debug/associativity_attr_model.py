cd8910058b0daf8da242cba016615350503a1f2d
you <you@example.com>
2023-09-06 09:45:07 +0800

    chore: Refactor commit time and object name extraction.
    - Update last commit time to September 6th in [.last_commit_time]
    - Add function `getBaseAndOriginalObjectName` to extract object names in associativity_attr_model.py
    - Modify loop to print base and original object names in associativity_attr_model.py
    - Add import statement for `re` module in associativity_attr_model.py

--- microgrid_base/cplex_abnormal_exit_condition_debug/associativity_attr_model.py
+++ microgrid_base/cplex_abnormal_exit_condition_debug/associativity_attr_model.py
@@ -12,27 +12,39 @@ model.pw = Piecewise(
     pw_constr_type="EQ",
     unbounded_domain_var=True,
 )
-model.g = Var([1,2,3], [4,5,6])
+model.g = Var([1, 2, 3], [4, 5, 6])
 
 model.subm = model.clone()
-model.cons = Constraint(expr=model.a >= model.b[2]+model.g[1,4]-model.subm.b[1])
+model.cons = Constraint(expr=model.a >= model.b[2] + model.g[1, 4] - model.subm.b[1])
 model.obj = Objective(expr=0, sense=minimize)
-# print(dir(model.pw))
-# breakpoint()
+
+
+import re
+
+BRACKETS_RE = re.compile(r"\[.*?\]")
+
+
+def getBaseAndOriginalObjectName(obj) -> tuple[str, str]:
+    objname = obj.name
+    base_objname, _ = BRACKETS_RE.subn("", objname)
+    return objname, base_objname
+
+
 for obj in model.component_data_objects():
-    print(obj.name)
+    print("\t".join(getBaseAndOriginalObjectName(obj)))
 
 model.write("associativity_output.lp")
 print("_" * 60)
-# import rich
 
 for it in model.solutions.symbol_map.values():
     for k, v in it.bySymbol.items():
-        print(k, v().name) # strip away rectangular brackets
+        print(k, v().name)  # strip away rectangular brackets
 # if it is not block, just keep the base name
 # if it is, keep its names of components
 # for c in [model.b, model.pw]:
-for c in [model.a, model.b,model.pw, model.cons, model.subm]:
-if model
-    for obj in c.parent_component().component_data_objects():
-        print(obj.name)
+for c in [model.a, model.b, model.pw, model.cons, model.subm]:
+    if (func := getattr(c, "component_data_objects", None)) is not None:
+        for obj in func():
+            print("\t".join(getBaseAndOriginalObjectName(obj)))
+    else:
+        print(c.name)
