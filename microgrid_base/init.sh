37ab2f9387b26cf817c7eb0cef9dba670b2ab79b
you <you@example.com>
2023-09-07 12:45:07 +0800

    chore: Add htop to installed packages and update commit timestamp
    - Add `htop` to the list of installed packages in `init.sh`
    - Update the timestamp in `.last_commit_time` file.

--- microgrid_base/init.sh
+++ microgrid_base/init.sh
@@ -1,5 +1,5 @@
 # source ~/.bashrc
-apt install -y redis rabbitmq-server tmux tmuxp openjdk-8-jdk # htop
+apt install -y redis rabbitmq-server tmux tmuxp openjdk-8-jdk htop
 conda run -n cplex --live-stream --no-capture-output pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 conda run -n cplex --live-stream --no-capture-output pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
 conda run -n docplex --live-stream --no-capture-output pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements_docplex.txt
