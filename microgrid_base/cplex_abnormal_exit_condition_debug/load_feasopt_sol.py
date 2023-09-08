e79594b1bb7dbf48b1801ef383e5acdbbc01cff4
you <you@example.com>
2023-09-08 14:45:09 +0800

    refactor: Refactor XML parsing and file writing in load_feasopt_sol.py
    - Update timestamp in `.last_commit_time` file
    - Refactor `load_feasopt_sol.py` to use `feasopt.xml` instead of `feasopt.sol`
    - Parse `feasopt.xml` using BeautifulSoup and store data in a dictionary
    - Write the dictionary to a JSON file
    - Print variable names and values while parsing the XML file

--- microgrid_base/cplex_abnormal_exit_condition_debug/load_feasopt_sol.py
+++ microgrid_base/cplex_abnormal_exit_condition_debug/load_feasopt_sol.py
@@ -2,10 +2,24 @@
 # or use pyomo?
 # import lxml
 from bs4 import BeautifulSoup
-sol_file = "feasopt.sol"
 
-with open(sol_file, 'r') as f:
-	file = f.read()
+sol_file = "feasopt.xml"
+# sol_file = "feasopt.sol"
+
+with open(sol_file, "r") as f:
+    file = f.read()
 
 # 'xml' is the parser used. For html files, which BeautifulSoup is typically used for, it would be 'html.parser'.
-soup = BeautifulSoup(file, 'xml')
+soup = BeautifulSoup(file, "xml")
+# breakpoint()
+data = {}
+for var in soup.find_all("variable"):
+    name = var["name"]
+    value = float(var["value"])
+    data[name] = value
+    print(f"%s: %s" % (name, value))
+import json
+
+with open(output_path:="feasopt.json", "w+") as f:
+    f.write(json.dumps(data, indent=4, ensure_ascii=False))
+print("write to: %s" % output_path)
