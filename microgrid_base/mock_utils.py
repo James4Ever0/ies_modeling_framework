2185de4ef108f442f10b584d667bc501c5c018e6
you <you@example.com>
2023-09-11 11:00:10 +0800

    feat: Refactor mock utilities for algorithm testing.
    - Add new functionality to `mock_utils.py`
    - Implement hash input parameters for random seeds
    - Introduce `Person` class and `PersonFactory` for mocking algorithm response
    - Include `result` variable and print statement for testing purposes

--- microgrid_base/mock_utils.py
+++ microgrid_base/mock_utils.py
@@ -1,3 +1,30 @@
 """
 This library mocks algorithm response.
+
+Hash input parameters for random seeds.
 """
+
+# determinism?
+
+from datetime import date, datetime
+from typing import List, Union
+
+from pydantic import BaseModel, UUID4
+
+from pydantic_factories import ModelFactory
+
+
+class Person(BaseModel):
+    id: UUID4
+    name: str
+    hobbies: List[str]
+    age: Union[float, int]
+    birthday: Union[datetime, date]
+
+
+class PersonFactory(ModelFactory):
+    __model__ = Person
+
+
+result = PersonFactory.build()
+print(result)
